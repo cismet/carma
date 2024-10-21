de2c8712db10e0d49a856316b1334e4f531186a9
David Glogaza <david.glogaza40@googlemail.com>
2024-10-21 18:53:01 +0200

    add forwardrefs to buttons

--- libraries/mapping/engines/cesium/src/lib/components/controls/Compass.tsx
+++ libraries/mapping/engines/cesium/src/lib/components/controls/Compass.tsx
@@ -1,4 +1,4 @@
-import { MouseEvent, ReactNode } from "react";
+import { MouseEvent, ReactNode, forwardRef } from "react";
 import { useSelector } from "react-redux";
 
 import { faCompass } from "@fortawesome/free-solid-svg-icons";
@@ -22,78 +22,90 @@ type CompassProps = {
   disabled?: boolean;
 };
 
-export const Compass = (props: CompassProps) => {
-  const { viewer } = useCesiumContext();
-  const minZoomDistance = useSelector(
-    selectScreenSpaceCameraControllerMinimumZoomDistance,
-  );
+type Ref = HTMLButtonElement;
 
-  const handleFlyToCenter = (e: MouseEvent) => {
-    e.preventDefault();
+export const Compass = forwardRef<Ref, CompassProps>(
+  ({ children, disabled }, ref) => {
+    const { viewer } = useCesiumContext();
+    const minZoomDistance = useSelector(
+      selectScreenSpaceCameraControllerMinimumZoomDistance,
+    );
 
-    if (viewer) {
-      const windowPosition = new Cartesian2(
-        viewer.canvas.clientWidth / 2,
-        viewer.canvas.clientHeight / 2,
-      );
-      const horizonTest = viewer.camera.pickEllipsoid(windowPosition);
-      let destination = viewer.camera.position;
-      if (defined(horizonTest)) {
-        console.log("scene center below horizon");
-        //const pos = getCanvasCenter(viewer);
-        const { scenePosition, coordinates } = pickViewerCanvasCenter(viewer, {
-          getCoordinates: true,
-        });
-        console.log("pick compass", coordinates, scenePosition);
-        if (scenePosition && coordinates) {
-          const distance = Cartesian3.distance(
-            scenePosition,
+    const handleFlyToCenter = (e: MouseEvent) => {
+      e.preventDefault();
+
+      if (viewer) {
+        const windowPosition = new Cartesian2(
+          viewer.canvas.clientWidth / 2,
+          viewer.canvas.clientHeight / 2,
+        );
+        const horizonTest = viewer.camera.pickEllipsoid(windowPosition);
+        let destination = viewer.camera.position;
+        if (defined(horizonTest)) {
+          console.log("scene center below horizon");
+          //const pos = getCanvasCenter(viewer);
+          const { scenePosition, coordinates } = pickViewerCanvasCenter(
+            viewer,
+            {
+              getCoordinates: true,
+            },
+          );
+          console.log("pick compass", coordinates, scenePosition);
+          if (scenePosition && coordinates) {
+            const distance = Cartesian3.distance(
+              scenePosition,
+              viewer.camera.position,
+            );
+            const cartographic = coordinates;
+            const longitude = CesiumMath.toDegrees(cartographic.longitude);
+            const latitude = CesiumMath.toDegrees(cartographic.latitude);
+            destination = Cartesian3.fromDegrees(
+              longitude,
+              latitude,
+              cartographic.height + Math.max(distance, minZoomDistance),
+            );
+          }
+        } else {
+          console.info(
+            "scene above horizon, using camera position as reference",
+          );
+          // use camera position if horizon is not visible
+          // bump up the camera a bit if too close too ground
+          const cartographic = Cartographic.fromCartesian(
             viewer.camera.position,
           );
-          const cartographic = coordinates;
           const longitude = CesiumMath.toDegrees(cartographic.longitude);
           const latitude = CesiumMath.toDegrees(cartographic.latitude);
           destination = Cartesian3.fromDegrees(
             longitude,
             latitude,
-            cartographic.height + Math.max(distance, minZoomDistance),
+            cartographic.height + minZoomDistance,
           );
         }
-      } else {
-        console.info("scene above horizon, using camera position as reference");
-        // use camera position if horizon is not visible
-        // bump up the camera a bit if too close too ground
-        const cartographic = Cartographic.fromCartesian(viewer.camera.position);
-        const longitude = CesiumMath.toDegrees(cartographic.longitude);
-        const latitude = CesiumMath.toDegrees(cartographic.latitude);
-        destination = Cartesian3.fromDegrees(
-          longitude,
-          latitude,
-          cartographic.height + minZoomDistance,
-        );
+
+        console.log("HOOK: [2D3D|CESIUM|CAMERA] Compass FlyTo");
+        viewer.camera.flyTo({
+          destination,
+          orientation: {
+            heading: CesiumMath.toRadians(0), // facing north
+            pitch: CesiumMath.toRadians(-90), // looking straight down
+            roll: 0.0,
+          },
+        });
       }
+    };
 
-      console.log("HOOK: [2D3D|CESIUM|CAMERA] Compass FlyTo");
-      viewer.camera.flyTo({
-        destination,
-        orientation: {
-          heading: CesiumMath.toRadians(0), // facing north
-          pitch: CesiumMath.toRadians(-90), // looking straight down
-          roll: 0.0,
-        },
-      });
-    }
-  };
-
-  return (
-    <ControlButtonStyler
-      title="Einnorden"
-      onClick={handleFlyToCenter}
-      disabled={props.disabled}
-    >
-      <FontAwesomeIcon icon={faCompass}></FontAwesomeIcon>
-    </ControlButtonStyler>
-  );
-};
+    return (
+      <ControlButtonStyler
+        title="Einnorden"
+        onClick={handleFlyToCenter}
+        disabled={disabled}
+        ref={ref}
+      >
+        <FontAwesomeIcon icon={faCompass}></FontAwesomeIcon>
+      </ControlButtonStyler>
+    );
+  },
+);
 
 export default Compass;
