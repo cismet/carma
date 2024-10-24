8f54cedcc98598f148eda897049c80474d406098
David Glogaza <david.glogaza40@googlemail.com>
2024-10-24 15:05:35 +0200

    change position of icons

--- apps/geoportal/src/app/components/TopNavbar.tsx
+++ apps/geoportal/src/app/components/TopNavbar.tsx
@@ -63,84 +63,75 @@ const TopNavbar = () => {
   console.info("RENDER: TopNavbar");
 
   return (
-    <div className="h-16 w-full flex items-center relative justify-between py-2 px-[12px]">
+    <div className="h-16 w-full flex items-center gap-6 relative justify-between py-2 px-[12px]">
       <ResourceModal />
 
-      <div className="flex items-center gap-6">
-        <div className="flex items-center gap-2">
-          <p className="mb-0 font-semibold text-lg">
-            DigiTal Zwilling / Geoportal
-          </p>
-        </div>
-      </div>
+      <p className="mb-0 font-semibold text-lg w-1/4">
+        DigiTal Zwilling / Geoportal
+      </p>
 
-      <div
-        ref={menuTourRef}
-        className="flex items-center justify-between w-[57%] absolute left-1/2 -ml-[140px]"
+      <ActionButtons />
+      <Tooltip
+        title={`Hilfe ${uiMode === UIMode.TOUR ? "ausblenden" : "anzeigen"}`}
       >
-        <ActionButtons />
-        <Tooltip
-          title={`Hilfe ${uiMode === UIMode.TOUR ? "ausblenden" : "anzeigen"}`}
+        <button
+          className="hover:text-gray-600 text-xl"
+          onClick={handleToggleTour}
         >
-          <button
-            className="hover:text-gray-600 text-xl"
-            onClick={handleToggleTour}
-          >
-            <FontAwesomeIcon icon={faCircleQuestion} />
-          </button>
-        </Tooltip>
-        <div className="flex items-center gap-6">
-          <div className="lg:flex hidden" ref={hintergrundTourRef}>
-            {backgroundLayer && (
-              <Radio.Group
-                value={backgroundLayer.id}
-                onChange={(e) => {
-                  if (e.target.value === "karte") {
-                    dispatch(
-                      setBackgroundLayer({
-                        ...selectedMapLayer,
-                        id: "karte",
-                        visible: isMode2d,
-                      })
-                    );
-                    toggleSceneStyle("secondary");
-                  } else {
-                    dispatch(
-                      setBackgroundLayer({
-                        id: e.target.value,
-                        title: layerMap[e.target.value].title,
-                        opacity: 1.0,
-                        description: layerMap[e.target.value].description,
-                        inhalt: layerMap[e.target.value].inhalt,
-                        eignung: layerMap[e.target.value].eignung,
-                        layerType: "wmts",
-                        visible: isMode2d,
-                        props: {
-                          name: "",
-                          url: layerMap[e.target.value].url,
-                        },
-                        layers: layerMap[e.target.value].layers,
-                      })
-                    );
-                    toggleSceneStyle("primary");
-                  }
-                }}
-              >
-                <Radio.Button value="karte">Karte</Radio.Button>
-                <Radio.Button value="luftbild">Luftbild</Radio.Button>
-              </Radio.Group>
-            )}
-          </div>
-
-          <Button
-            onClick={() => {
-              setAppMenuVisible(true);
-            }}
-            ref={modalMenuTourRef}
-          >
-            <FontAwesomeIcon icon={faBars} />
-          </Button>
+          <FontAwesomeIcon icon={faCircleQuestion} />
+        </button>
+      </Tooltip>
+      <div className="flex items-center gap-6">
+        <div className="lg:flex hidden" ref={hintergrundTourRef}>
+          {backgroundLayer && (
+            <Radio.Group
+              value={backgroundLayer.id}
+              onChange={(e) => {
+                if (e.target.value === "karte") {
+                  dispatch(
+                    setBackgroundLayer({
+                      ...selectedMapLayer,
+                      id: "karte",
+                      visible: isMode2d,
+                    })
+                  );
+                  toggleSceneStyle("secondary");
+                } else {
+                  dispatch(
+                    setBackgroundLayer({
+                      id: e.target.value,
+                      title: layerMap[e.target.value].title,
+                      opacity: 1.0,
+                      description: layerMap[e.target.value].description,
+                      inhalt: layerMap[e.target.value].inhalt,
+                      eignung: layerMap[e.target.value].eignung,
+                      layerType: "wmts",
+                      visible: isMode2d,
+                      props: {
+                        name: "",
+                        url: layerMap[e.target.value].url,
+                      },
+                      layers: layerMap[e.target.value].layers,
+                    })
+                  );
+                  toggleSceneStyle("primary");
+                }
+              }}
+            >
+              <Radio.Button value="karte">Karte</Radio.Button>
+              <Radio.Button value="luftbild">Luftbild</Radio.Button>
+            </Radio.Group>
+          )}
         </div>
+
+        <Button
+          onClick={() => {
+            setAppMenuVisible(true);
+          }}
+          ref={modalMenuTourRef}
+        >
+          <FontAwesomeIcon icon={faBars} />
+        </Button>
       </div>
     </div>
   );
--- apps/geoportal/src/app/components/nav-items/ActionButtons.tsx
+++ apps/geoportal/src/app/components/nav-items/ActionButtons.tsx
@@ -39,7 +39,7 @@ const ActionButtons = () => {
   const baseUrl = window.location.origin + window.location.pathname;
 
   return (
-    <div className="flex items-center gap-6">
+    <div className="flex items-center gap-6 lg:ml-12">
       <Tooltip title="Aktualisieren">
         <button
           onClick={() => {
