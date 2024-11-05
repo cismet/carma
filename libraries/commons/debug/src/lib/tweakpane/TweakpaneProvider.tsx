f1253789899b6deb72c961d7147fb0c07f78c26e
Friedrich Hartmann <690920+mxfh@users.noreply.github.com>
2024-11-05 01:09:30 +0100

    remove location hash functionality from debug ui, change button

--- libraries/commons/debug/src/lib/tweakpane/TweakpaneProvider.tsx
+++ libraries/commons/debug/src/lib/tweakpane/TweakpaneProvider.tsx
@@ -1,72 +1,59 @@
-import React, { useRef, useEffect, useState, useMemo } from "react";
+import React, { useRef, useEffect, useState, useCallback } from "react";
 import type { ReactNode } from "react";
 import { Pane } from "tweakpane";
 import localForage from "localforage";
-import { hasHashParam, removeHashParam } from "../utils";
 import { TweakpaneContext } from "./TweakpaneContext";
 
-const eventKeys = ["~", "F12"];
-const DEFAULT_DEBUG_PARAM = "dev";
+const eventKeys = ["~", "F1"];
 const localForageKey = "tweakpaneEnabled";
 
 export const TweakpaneProvider: React.FC<{
   children: ReactNode;
-  hashparam?: string;
   position?: {
     top?: number;
     left?: number;
     right?: number;
   };
-}> = ({
-  children,
-  hashparam = DEFAULT_DEBUG_PARAM,
-  position = { top: 64, left: 64 },
-}) => {
-  const [hidden, setHidden] = useState(true);
+}> = ({ children, position = { top: 64, left: 64 } }) => {
+  const [isHidden, setIsHidden] = useState(true);
   const containerRef = useRef<HTMLDivElement>(null);
   const paneRef = useRef<Pane | null>(null);
 
+  const toggleTweakpane = useCallback(() => {
+    setIsHidden((prevState) => {
+      const newState = !prevState;
+      localForage.setItem(localForageKey, newState);
+      return newState;
+    });
+  }, []);
+
   useEffect(() => {
-    const checkHashAndStoredState = async () => {
-      const isEnabledFromHash = hasHashParam(hashparam);
-      if (isEnabledFromHash) {
-        // If 'dev' is in the URL, enable debug view immediately
-        console.log("Dev mode enabled from hash");
-        setHidden(true);
-        await localForage.setItem(localForageKey, true);
-      } else {
-        // If 'dev' is not in the URL, check localForage
-        const storedIsEnabled = await localForage.getItem(localForageKey);
-        setHidden(storedIsEnabled === true);
+    const checkStoredState = async () => {
+      const storedIsHidden = await localForage.getItem<boolean>(localForageKey);
+      if (storedIsHidden !== null && storedIsHidden !== undefined) {
+        setIsHidden(storedIsHidden);
       }
     };
+    checkStoredState();
+  }, []);
 
-    checkHashAndStoredState();
-
-    const toggleTweakpane = (event: KeyboardEvent) => {
+  const toggleOnKeypress = useCallback(
+    (event: KeyboardEvent) => {
       if (eventKeys.includes(event.key)) {
-        // dev state should not be persited in URL when sharing
-        setHidden((prevState) => {
-          const newState = !prevState;
-          localForage.setItem(localForageKey, newState);
-          newState === false && removeHashParam(hashparam);
-          return newState;
-        });
+        toggleTweakpane();
       }
-    };
-    window.addEventListener("keydown", toggleTweakpane);
-    return () => {
-      window.removeEventListener("keydown", toggleTweakpane);
-    };
-  }, [hashparam]);
+    },
+    [toggleTweakpane]
+  );
 
   useEffect(() => {
-    const disableTweakpane = () => {
-      setHidden(true);
-      localForage.removeItem(localForageKey);
-      removeHashParam(hashparam);
+    window.addEventListener("keydown", toggleOnKeypress);
+    return () => {
+      window.removeEventListener("keydown", toggleOnKeypress);
     };
+  }, [toggleOnKeypress]);
 
+  useEffect(() => {
     if (!paneRef.current && containerRef.current) {
       const pane = new Pane({
         title: "Developer Options",
@@ -76,36 +63,25 @@ export const TweakpaneProvider: React.FC<{
 
       const closeButton = pane.addButton({
         title: "Close This Dev GUI",
-        label: "Toggle with F12 or ~",
+        label: "Toggle with F1 or ~",
       });
-      closeButton.on("click", () => {
-        disableTweakpane();
-      });
+      closeButton.on("click", toggleTweakpane);
     }
 
     return () => {
-      if (paneRef.current) {
-        paneRef.current.dispose();
-        paneRef.current = null;
-      }
+      paneRef.current?.dispose();
+      paneRef.current = null;
     };
-  }, [containerRef, hashparam]);
+  }, [toggleTweakpane]);
 
-  const { top, left, right } = position ?? {};
-
-  console.debug("RENDER: [DEBUG] TweakpaneProvider", hidden);
-
-  // prevent re-rendering of children on change of isEnabled
-  const memoizedChildren = useMemo(() => {
-    return children;
-  }, [children]);
+  const { top, left, right } = position;
 
   return (
     <TweakpaneContext.Provider value={{ paneRef }}>
       <div
         ref={containerRef}
         id="tweakpane-container"
-        hidden={hidden}
+        hidden={isHidden}
         style={{
           position: "absolute",
           top,
@@ -114,7 +90,7 @@ export const TweakpaneProvider: React.FC<{
           zIndex: 10000,
         }}
       ></div>
-      {memoizedChildren}
+      {children}
     </TweakpaneContext.Provider>
   );
 };
