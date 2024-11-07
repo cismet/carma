f873ae8ef6d31fa180ce7f9175aaf91fc465c108
PavelOlkhovoi <p-olhovoi@bk.ru>
2024-11-07 09:33:19 +0100

    intercept addresses data

--- e2e/geoportal/geoportal/src/e2e/fuzzysearch.cy.ts
+++ e2e/geoportal/geoportal/src/e2e/fuzzysearch.cy.ts
@@ -7,11 +7,52 @@ describe("Fuzzy search should show search results and move map to the selected i
     cy.get(".ant-select-item.ant-select-item-option").should("not.exist");
     cy.get(".leaflet-marker-icon").should("not.exist");
 
-    cy.get("[data-test-id=fuzzy-search]")
-      .should("be.visible")
-      .wait(500)
-      .find("input")
-      .type("gabel");
+    // cy.intercept(
+    //   "GET",
+    //   "https://wupp-topicmaps-data.cismet.de/data/3857/adressen.json.md5"
+    // ).as("adressen");
+
+    // cy.intercept(
+    //   "GET",
+    //   "https://wupp-topicmaps-data.cismet.de/data/3857/kitas.json.md5"
+    // ).as("kitas");
+
+    cy.intercept("GET", "https://wupp-topicmaps-data.cismet.de/**", {
+      statusCode: 200,
+      body: [
+        {
+          s: "Achenbachstr.",
+          nr: 1,
+          z: "",
+          g: "home",
+          x: 793007.83,
+          y: 6668501.93,
+          m: { zl: 18 },
+        },
+        {
+          s: "Achenbachstr.",
+          nr: 9,
+          z: "",
+          g: "home",
+          x: 793053.3,
+          y: 6668415.06,
+          m: { zl: 18 },
+        },
+        {
+          s: "Achenbachtreppe",
+          nr: 0,
+          z: "",
+          g: "road",
+          x: 793022.68,
+          y: 6668515.97,
+          m: { zl: 18 },
+        },
+      ],
+    }).as("data");
+
+    cy.wait("@data").then((response) => {
+      console.log("xxx data", response);
+    });
 
     cy.location("hash").then((hash) => {
       const queryString = hash.slice(2);
@@ -24,10 +65,26 @@ describe("Fuzzy search should show search results and move map to the selected i
       cy.wrap(lng).as("lng");
     });
 
-    cy.get(".ant-select-item.ant-select-item-option")
-      .should("have.length.greaterThan", 5)
-      .first()
-      .click();
+    cy.get("[data-test-id=fuzzy-search]")
+      .should("be.visible")
+      .find("input")
+      // .wait("@data")
+      .type("ach");
+
+    // cy.pause();
+    cy.wait("@data");
+
+    // cy.get(".ant-select-item.ant-select-item-option").should("be.visible");
+    // cy.get(".ant-select-item.ant-select-item-option").first().click();
+
+    // cy.contains(".ant-select-item.ant-select-item-option", "Achenbachstr.")
+    //   .should("be.visible")
+    //   .click();
+
+    // cy.get(".ant-select-item.ant-select-item-option")
+    //   .should("have.length.greaterThan", 5)
+    //   .first()
+    //   .click();
 
     cy.get(".leaflet-marker-icon").should("be.visible");
 
