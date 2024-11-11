2e1bd9dc9e68528d520de6533832b03bb35da4c9
PavelOlkhovoi <p-olhovoi@bk.ru>
2024-11-11 11:05:10 +0100

    test more accordions

--- e2e/geoportal/geoportal/src/e2e/modalmenu.cy.ts
+++ e2e/geoportal/geoportal/src/e2e/modalmenu.cy.ts
@@ -21,21 +21,8 @@ describe("Fuzzy search should show search results and move map to the selected i
     );
     cy.get(".accordion").should("have.length.greaterThan", 3);
 
-    // cy.get(".collapse.show").should("not.exist");
-    // cy.get("[name=datengrundlage]").find("button").should("exist");
-    // cy.get("[name=datengrundlage]").find("button").click({ force: true });
-    // cy.get(".collapse.show").should("exist");
-    // cy.get("[name=datengrundlage]").find("button").click({ force: true });
-
     toggleAccordion("datengrundlage");
     toggleAccordion("zwilling");
-
-    // cy.get(".collapse.show").should("not.exist");
-    // cy.get("[name=zwilling]").find("button").should("exist");
-    // cy.get("[name=zwilling]").find("button").click({ force: true });
-    // cy.get(".collapse.show").should("exist");
-    // cy.get("[name=zwilling]").find("button").click({ force: true });
-    // cy.get(".collapse.show").should("not.exist");
 
     cy.get(".modal-footer").should("be.visible");
     cy.get("#cmdCloseModalApplicationMenu").should("be.visible");
