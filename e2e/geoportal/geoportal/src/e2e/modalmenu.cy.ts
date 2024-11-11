529e4166e38ce1f9879eba7ae68d24cd09d34953
PavelOlkhovoi <p-olhovoi@bk.ru>
2024-11-11 11:02:41 +0100

    refactoring of accordion

--- e2e/geoportal/geoportal/src/e2e/modalmenu.cy.ts
+++ e2e/geoportal/geoportal/src/e2e/modalmenu.cy.ts
@@ -3,6 +3,15 @@ describe("Fuzzy search should show search results and move map to the selected i
     cy.visit("/");
   });
 
+  const toggleAccordion = (sectionName: string) => {
+    cy.get(".collapse.show").should("not.exist");
+    cy.get(`[name=${sectionName}]`).find("button").should("exist");
+    cy.get(`[name=${sectionName}]`).find("button").click({ force: true });
+    cy.get(".collapse.show").should("exist");
+    cy.get(`[name=${sectionName}]`).find("button").click({ force: true });
+    cy.get(".collapse.show").should("not.exist");
+  };
+
   it("Modal menu opens and contains header, introduction, sections, footer.", () => {
     cy.get("[data-test-id=modal-menu-btn]").click();
     cy.get(".modal-title").should("be.visible");
@@ -12,18 +21,21 @@ describe("Fuzzy search should show search results and move map to the selected i
     );
     cy.get(".accordion").should("have.length.greaterThan", 3);
 
-    cy.get(".collapse.show").should("not.exist");
-    cy.get("[name=datengrundlage]").find("button").should("exist");
-    cy.get("[name=datengrundlage]").find("button").click({ force: true });
-    cy.get(".collapse.show").should("exist");
-    cy.get("[name=datengrundlage]").find("button").click({ force: true });
+    // cy.get(".collapse.show").should("not.exist");
+    // cy.get("[name=datengrundlage]").find("button").should("exist");
+    // cy.get("[name=datengrundlage]").find("button").click({ force: true });
+    // cy.get(".collapse.show").should("exist");
+    // cy.get("[name=datengrundlage]").find("button").click({ force: true });
 
-    cy.get(".collapse.show").should("not.exist");
-    cy.get("[name=zwilling]").find("button").should("exist");
-    cy.get("[name=zwilling]").find("button").click({ force: true });
-    cy.get(".collapse.show").should("exist");
-    cy.get("[name=zwilling]").find("button").click({ force: true });
-    cy.get(".collapse.show").should("not.exist");
+    toggleAccordion("datengrundlage");
+    toggleAccordion("zwilling");
+
+    // cy.get(".collapse.show").should("not.exist");
+    // cy.get("[name=zwilling]").find("button").should("exist");
+    // cy.get("[name=zwilling]").find("button").click({ force: true });
+    // cy.get(".collapse.show").should("exist");
+    // cy.get("[name=zwilling]").find("button").click({ force: true });
+    // cy.get(".collapse.show").should("not.exist");
 
     cy.get(".modal-footer").should("be.visible");
     cy.get("#cmdCloseModalApplicationMenu").should("be.visible");
