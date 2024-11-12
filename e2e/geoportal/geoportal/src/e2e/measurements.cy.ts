56104710ba1b91871c6d2f3ae9c4fe2938611b8f
PavelOlkhovoi <p-olhovoi@bk.ru>
2024-11-12 15:00:15 +0100

    check zoom and deletion

--- e2e/geoportal/geoportal/src/e2e/measurements.cy.ts
+++ e2e/geoportal/geoportal/src/e2e/measurements.cy.ts
@@ -7,11 +7,12 @@ describe("Geoportal measurements", () => {
     cy.get("[data-test-id=measurement-control]").should("be.visible");
     cy.get("[data-test-id=measurement-control]").click();
     cy.get("[data-test-id=empty-measurement-info]").should("be.visible");
+
     cy.get("#routedMap").should("be.visible");
-    // cy.contains("Aktuell sind keine Messungen").should("be.visible");
     cy.get("#routedMap").click(300, 300);
     cy.get("#routedMap").click(403, 300);
     cy.get("#routedMap").click(403, 300);
+
     cy.contains("Linienzug").should("be.visible");
     cy.get('[title="Total length"]').should("be.visible");
     cy.get('[title="Total length"]')
@@ -55,7 +56,29 @@ describe("Geoportal measurements", () => {
     cy.contains("Polygon").should("be.visible");
     cy.contains("Fläche").should("be.visible");
 
-    // cy.get(".fa-trash-can").click();
-    // cy.contains("Aktuell sind keine Messungen").should("be.visible");
+    cy.get("[data-test-id=switch-measurement-left]").click();
+    cy.contains("Polygon").should("not.exist");
+    cy.contains("Fläche").should("not.exist");
+    cy.contains("Linienzug").should("be.visible");
+
+    cy.get("[data-test-id=switch-measurement-right]").click();
+    cy.contains("Polygon").should("be.visible");
+    cy.contains("Fläche").should("be.visible");
+    cy.contains("Linienzug").should("not.exist");
+
+    cy.contains("2 Messungen angezeigt").should("be.visible");
+    cy.get("[data-test-id=zoom-measurement-btn]").click();
+    cy.contains("1 Messungen angezeigt").should("be.visible");
+
+    cy.contains("2 Messungen verfügbar").should("be.visible");
+    cy.contains("2 Messungen verfügbar").click();
+    cy.contains("2 Messungen angezeigt").should("be.visible");
+
+    cy.get("[data-test-id=delete-measurement-btn]").click();
+    cy.contains("1 Messungen verfügbar").should("be.visible");
+    cy.contains("1 Messungen angezeigt").should("be.visible");
+
+    cy.get("[data-test-id=delete-measurement-btn]").click();
+    cy.contains("0 Messungen verfügbar").should("be.visible");
   });
 });
