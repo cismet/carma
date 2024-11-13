e55f980977a5353a1a69d248fe165205f866c95b
PavelOlkhovoi <p-olhovoi@bk.ru>
2024-11-13 19:12:23 +0100

    fix problem with click

--- e2e/geoportal/geoportal/src/e2e/overlay-helper.cy.ts
+++ e2e/geoportal/geoportal/src/e2e/overlay-helper.cy.ts
@@ -10,9 +10,13 @@ describe("geoportal smoke test", () => {
       5
     );
     cy.get(".ant-popover-content").should("not.exist");
-    cy.get("[data-test-id=primary-with-secondary]").first().click();
+    cy.get("[data-test-id=primary-with-secondary]")
+      .first()
+      .click({ force: true });
     cy.get(".ant-popover-content").should("be.visible");
-    cy.get("[data-test-id=primary-with-secondary]").first().click();
+    cy.get("[data-test-id=primary-with-secondary]")
+      .first()
+      .click({ force: true });
     cy.get(".ant-popover-content").should("not.be.visible");
     cy.get("[data-test-id=overlay-helper-bg]").click();
     cy.get("[data-test-id=overlay-helper-bg]").should("not.exist");
