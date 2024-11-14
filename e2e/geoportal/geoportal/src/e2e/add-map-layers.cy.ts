7f8c6ee7e0b360da6abd53f9bf66deb1ece3ad90
PavelOlkhovoi <p-olhovoi@bk.ru>
2024-11-14 15:02:02 +0100

    improve check favorite layer

--- e2e/geoportal/geoportal/src/e2e/add-map-layers.cy.ts
+++ e2e/geoportal/geoportal/src/e2e/add-map-layers.cy.ts
@@ -3,7 +3,7 @@ describe("Geoportal add map layers", () => {
     cy.visit("/");
   });
 
-  it("Search shows only related layer, layers are added to map and the favorite section", () => {
+  it("Search shows only related layer, layers are added to map and to the favorite section", () => {
     cy.get("[data-test-id=kartenebenen-hinzufügen-btn]").should("be.visible");
     cy.get("[data-test-id=kartenebenen-hinzufügen-btn]").click();
     cy.get(".ant-modal-content").should("be.visible");
@@ -12,18 +12,42 @@ describe("Geoportal add map layers", () => {
       "have.length.greaterThan",
       8
     );
-    cy.get(".ant-modal-content")
-      .find("input")
-      .should("be.visible")
-      .type("Expresskarte");
+
+    // cy.get(".ant-modal-content")
+    //   .find("input")
+    //   .should("be.visible")
+    //   .type("Expresskarte");
+    // cy.get(".anticon.anticon-close-circle").click();
 
     cy.get("[data-test-id=card-layer-prev]").should("be.visible");
 
-    cy.get(".anticon.anticon-close-circle").click();
+    cy.get(".Favoriten").should("not.exist");
+
     cy.get("[data-test-id=card-layer-prev]")
-      .first()
+      .contains("Expresskarte (Strichkarte gelb)")
+      .should("exist")
+      .parents('[data-test-id="card-layer-prev"]')
+      .as("flayer-gelp");
+
+    cy.get("@flayer-gelp")
       .find("[data-test-id=add-layer-favorite]")
-      .should("be.visible")
+      .should("exist")
       .click();
+
+    cy.get("#Favoriten").should("exist");
+    cy.get("#Favoriten")
+      .find('[data-test-id="card-layer-prev"]')
+      .should("exist");
+
+    cy.get("@flayer-gelp")
+      .find("[data-test-id=remove-layer-favorite]")
+      .should("exist")
+      .click();
+
+    cy.get("#Favoriten").should("not.exist");
+
+    // cy.get("@first-prev-layer")
+    //   .find("[data-test-id=remove-layer-favorite]")
+    //   .should("be.visible ");
   });
 });
