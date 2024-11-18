5552ed01ff893ade26dbfb657c4a294838bc9266
PavelOlkhovoi <p-olhovoi@bk.ru>
2024-11-18 10:27:50 +0100

    expand ui print settings

--- apps/geoportal/src/app/components/map-print/Print.tsx
+++ apps/geoportal/src/app/components/map-print/Print.tsx
@@ -1,11 +1,10 @@
-import { faShareNodes } from "@fortawesome/free-solid-svg-icons";
+import { faPrint, faShareNodes } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Button, Radio } from "antd";
+import { Button, Radio, Input } from "antd";
 import { useEffect, useState } from "react";
 // import "./popover.css";
 import type { RadioChangeEvent } from "antd";
 import { useDrawRectangle } from "../../hooks/useDrawRectangle";
-import { getUIMode, setUIMode } from "../../store/slices/ui";
 import { getOrientation, changeOrientation } from "../../store/slices/print";
 import { useSelector, useDispatch } from "react-redux";
 
@@ -13,24 +12,22 @@ const Print = () => {
   const dispatch = useDispatch();
   const currentOrient = useSelector(getOrientation);
   const [orientation, setOrientation] = useState(currentOrient);
-
-  const rectangle = useDrawRectangle();
+  const [scale, setScale] = useState("4000");
+  const [dpi, setDpi] = useState("100");
+  useDrawRectangle();
 
   const onChange = (e: RadioChangeEvent) => {
-    console.log("xxx radio checked", e.target.value);
     setOrientation(e.target.value);
     dispatch(changeOrientation(e.target.value));
   };
 
-  //   useEffect(() => {
-  //     dispatch(setUIMode("print"));
-  //   }, []);
   return (
     <div className="p-2 flex flex-col gap-3">
       <div className="flex items-center gap-2">
-        <FontAwesomeIcon icon={faShareNodes} className="text-xl" />
-        <h4 className="mb-0">Map print</h4>
+        <FontAwesomeIcon icon={faPrint} className="text-xl" />
+        <h4 className="mb-0">Drucken</h4>
       </div>
+      <h5 className="mb-0">Vorlage</h5>
       <Radio.Group onChange={onChange} value={orientation}>
         <div className="flex items-center gap-1">
           <Radio value={"portrait"}>Hochkant</Radio>
@@ -39,7 +36,18 @@ const Print = () => {
       </Radio.Group>
       <hr className="my-0" />
 
-      <Button>Print</Button>
+      <h5 className="mb-0">Maßstab</h5>
+      <Input placeholder="4000" onChange={(e) => setScale(e.target.value)} />
+      <hr className="my-0" />
+      <h5 className="mb-0">DPI</h5>
+      <Radio.Group onChange={(e) => setDpi(e.target.value)} value={dpi}>
+        <div className="flex items-center gap-1">
+          <Radio value={"100"}>100</Radio>
+          <Radio value={"200"}>200</Radio>
+          <Radio value={"300"}>300</Radio>
+        </div>
+      </Radio.Group>
+      <Button>Starten</Button>
     </div>
   );
 };
