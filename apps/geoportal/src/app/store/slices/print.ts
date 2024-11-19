1946bd5a83f7581341530663482e74729eac1b24
PavelOlkhovoi <p-olhovoi@bk.ru>
2024-11-19 11:51:48 +0100

    add settings to print slice

--- apps/geoportal/src/app/store/slices/print.ts
+++ apps/geoportal/src/app/store/slices/print.ts
@@ -1,12 +1,17 @@
 import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
 import { RootState } from "..";
 type PrintOrientation = "landscape" | "portrait";
+type DPI = "100" | "200" | "300";
 export type PrintState = {
   orientation: PrintOrientation;
+  dpi: DPI;
+  scale: string;
 };
 
 const initialState: PrintState = {
   orientation: "portrait",
+  dpi: "100",
+  scale: "4000",
 };
 
 const slice = createSlice({
@@ -16,10 +21,18 @@ const slice = createSlice({
     changeOrientation(state, action: PayloadAction<PrintOrientation>) {
       state.orientation = action.payload;
     },
+    changeDPI(state, action: PayloadAction<DPI>) {
+      state.dpi = action.payload;
+    },
+    changeScale(state, action: PayloadAction<string>) {
+      state.scale = action.payload;
+    },
   },
 });
 
 export const { changeOrientation } = slice.actions;
 export const getOrientation = (state: RootState) => state.print.orientation;
+export const getDPI = (state: RootState) => state.print.dpi;
+export const getScale = (state: RootState) => state.print.scale;
 
 export default slice.reducer;
