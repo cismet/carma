fad865ad238af6a1b7796174671b22971a300afa
David Glogaza <david.glogaza40@googlemail.com>
2024-11-22 21:13:25 +0100

    show position when no layer is active

--- apps/geoportal/src/app/components/GeoportalMap/topicmap.utils.ts
+++ apps/geoportal/src/app/components/GeoportalMap/topicmap.utils.ts
@@ -213,6 +213,18 @@ export const onClickTopicMap = async (
         dispatch(clearVectorInfos());
       }
     }
+  } else {
+    dispatch(
+      setSelectedFeature({
+        properties: {
+          header: "Position",
+          headerColor: "#0078a8",
+          title: `${e.latlng.lat.toFixed(5)}, ${e.latlng.lng.toFixed(5)}`,
+          subtitle: "(Geogr. Breite und Länge in Dezimalgrad, ETRS89)",
+        },
+        id: "information",
+      })
+    );
   }
 };
 
