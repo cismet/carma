b2d3089b1158bd7273c75995f92fe2489b402f19
PavelOlkhovoi <p-olhovoi@bk.ru>
2024-11-27 09:23:34 +0100

    make overlay test for each item

--- e2e/geoportal/geoportal/src/e2e/overlay-helper.cy.ts
+++ e2e/geoportal/geoportal/src/e2e/overlay-helper.cy.ts
@@ -10,14 +10,14 @@ describe("geoportal smoke test", () => {
       5
     );
     cy.get(".ant-popover-content").should("not.exist");
-    cy.get("[data-test-id=primary-with-secondary]")
-      .first()
-      .click({ force: true });
-    cy.get(".ant-popover-content").should("be.visible");
-    cy.get("[data-test-id=primary-with-secondary]")
-      .first()
-      .click({ force: true });
-    cy.get(".ant-popover-content").should("not.be.visible");
+    cy.get("[data-test-id=primary-with-secondary]").each(($el) => {
+      cy.wrap($el).click({ force: true });
+      cy.get(".ant-popover-content").should("be.visible");
+
+      cy.wrap($el).click({ force: true });
+
+      cy.get(".ant-popover-content").should("not.be.visible");
+    });
     cy.get("[data-test-id=overlay-helper-bg]").click();
     cy.get("[data-test-id=overlay-helper-bg]").should("not.exist");
     cy.get("[data-test-id=primary-with-secondary]").should("not.exist");
