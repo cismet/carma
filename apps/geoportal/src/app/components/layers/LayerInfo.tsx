181ccdb4563bebd78e3873de324a2c871fe548f2
David Glogaza <david.glogaza40@googlemail.com>
2024-11-28 14:23:12 +0100

    change width of description

--- apps/geoportal/src/app/components/layers/LayerInfo.tsx
+++ apps/geoportal/src/app/components/layers/LayerInfo.tsx
@@ -79,7 +79,7 @@ const LayerInfo = ({ description, legend, zoomLevels }: LayerInfoProps) => {
   return (
     <div className="flex flex-col gap-1 overflow-y-hidden h-full">
       <div className="flex gap-2 w-full h-[94%]">
-        <div className="h-full flex flex-col gap-2 w-2/3">
+        <div className="h-full flex flex-col gap-2 w-[80%]">
           {parsedDescription && (
             <div>
               <h5 className="font-semibold">Inhalt</h5>
