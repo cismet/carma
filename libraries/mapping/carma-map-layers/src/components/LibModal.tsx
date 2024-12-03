6cac45dd3862ad5c916f0580a54be71b9d52dd5d
David Glogaza <david.glogaza40@googlemail.com>
2024-12-03 09:57:51 +0100

    show new styling with url params

--- libraries/mapping/carma-map-layers/src/components/LibModal.tsx
+++ libraries/mapping/carma-map-layers/src/components/LibModal.tsx
@@ -1,5 +1,12 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { faX } from "@fortawesome/free-solid-svg-icons";
+import {
+  faBook,
+  faList,
+  faMap,
+  faMapPin,
+  faStar,
+  faX,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useDebounce } from "@uidotdev/usehooks";
 import { Button, Input, Modal, Spin } from "antd";
@@ -23,6 +30,7 @@ import "./modal.css";
 import type { Item, Layer, SavedLayerConfig } from "../helper/types";
 import { isEqual } from "lodash";
 import { utils } from "@carma-apps/portals";
+import { SidebarItem } from "./SidebarItems";
 const { Search } = Input;
 
 // @ts-expect-error tbd
@@ -47,6 +55,14 @@ export interface LibModalProps {
   updateActiveLayer: (layer: Layer) => void;
   removeLastLayer?: () => void;
 }
+
+const sidebarElements = [
+  { icon: faStar, text: "Favoriten" },
+  { icon: faList, text: "Entdecken" },
+  { icon: faBook, text: "Teilzwillinge" },
+  { icon: faMap, text: "Kartenebenen" },
+  { icon: faMapPin, text: "Sensoren" },
+];
 
 export const LibModal = ({
   open,
@@ -76,6 +92,9 @@ export const LibModal = ({
     any[] | undefined
   >([]);
   const debouncedSearchTerm = useDebounce(searchValue, 300);
+
+  const urlParams = new URLSearchParams(window.location.hash);
+  const showNewStyleParam = urlParams.get("alternativeModalStyle");
 
   // const checkDifferences = (url, configName) => {
   //   fetch(`${url}?service=WMS&request=GetCapabilities&version=1.1.1`)
@@ -310,6 +329,202 @@ export const LibModal = ({
 
     return () => clearTimeout(timer);
   }, [open]);
+
+  if (showNewStyleParam !== null) {
+    return (
+      <Modal
+        open={open}
+        onCancel={() => {
+          if (preview) {
+            setPreview(false);
+            if (removeLastLayer) {
+              removeLastLayer();
+            }
+          } else {
+            setOpen(false);
+          }
+        }}
+        style={{
+          top: preview ? "84%" : undefined,
+          transition: "top 400ms linear",
+        }}
+        mask={!preview}
+        footer={<></>}
+        width={"100%"}
+        closeIcon={false}
+        wrapClassName="h-full !overflow-y-hidden"
+        className="h-[88%]"
+        styles={{
+          content: {
+            backgroundColor: "#f2f2f2",
+          },
+        }}
+      >
+        <div
+          className="w-full h-full flex bg-[#f2f2f2]"
+          style={{
+            maxHeight: "calc(100vh - 200px)",
+            minHeight: "calc(100vh - 200px)",
+          }}
+        >
+          <div
+            className={`w-36 h-full flex justify-between items-center flex-col pb-3 bg-gray-600`}
+            style={{ height: "calc(100vh - 188px)" }}
+          >
+            <div className="flex flex-col w-full items-center gap-2">
+              <div className="h-24"></div>
+              {sidebarElements.map((element, i) => {
+                return (
+                  <SidebarItem
+                    icon={element.icon}
+                    text={element.text}
+                    active={i === 1}
+                  />
+                );
+              })}
+            </div>
+          </div>
+
+          <div
+            className="w-full h-full flex flex-col bg-[#f2f2f2]"
+            style={{
+              maxHeight: "calc(100vh - 200px)",
+              minHeight: "calc(100vh - 200px)",
+            }}
+          >
+            <div className="sticky top-0 px-6 pt-6">
+              <div className="flex justify-between items-center">
+                <h1 className="mb-0 text-2xl font-semibold">Karteninhalte</h1>
+                <Search
+                  placeholder="Suchbegriff eingeben"
+                  className="w-[76%]"
+                  allowClear
+                  onChange={(e) => {
+                    setIsSearching(true);
+                    setSearchValue(e.target.value);
+                  }}
+                  loading={isSearching}
+                  onSearch={(value) => {
+                    search(value);
+                  }}
+                />
+                <Button
+                  type="text"
+                  onClick={() => {
+                    setOpen(false);
+                    setPreview(false);
+                    setSelectedLayerId(null);
+                  }}
+                >
+                  <FontAwesomeIcon icon={faX} />
+                </Button>
+              </div>
+              <div className="flex w-full gap-2">
+                {layers && layers.length > 0 && (
+                  <>
+                    <LayerTabs
+                      layers={layers}
+                      activeId={inViewCategory}
+                      numberOfItems={getNumberOfLayers(layers)}
+                    />
+                    <hr className="h-px bg-gray-300 border-0 mt-0 mb-2" />
+                  </>
+                )}
+              </div>
+            </div>
+            <div className="flex w-full gap-4 h-full overflow-auto pt-0.5 px-6">
+              {!showItems && open && (
+                <div className="h-full w-full flex items-center justify-center">
+                  <div className="grid xl:grid-cols-5 lg:grid-cols-4 sm:grid-cols-2 w-full gap-8 mb-4 px-6 pt-4">
+                    {[...Array(10)].map((_, i) => (
+                      <div
+                        key={i}
+                        className="bg-white rounded-lg shadow-sm h-80 w-full flex flex-col gap-2 animate-pulse"
+                      >
+                        <div className="h-40 p-2 w-full bg-slate-200 rounded-t-lg"></div>
+                        <div className="h-2 bg-slate-200 rounded mx-8 w-1/3"></div>
+                        <div className="h-20 bg-slate-200 rounded mx-8"></div>
+                        <div className="mx-8 flex items-center gap-2">
+                          <div className="h-2 bg-slate-200 rounded w-full"></div>
+                          <span className="text-slate-200"> · </span>
+                          <div className="h-2 bg-slate-200 rounded w-full"></div>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              )}
+
+              <div>
+                {showItems &&
+                  layers.map((category, i) => (
+                    <div key={category.Title}>
+                      {category.layers.length > 0 && (
+                        <InView
+                          rootMargin="20px 0px 20px 0px"
+                          as="div"
+                          onChange={(inView, entry) => {
+                            if (inView) {
+                              setInViewCategory(entry.target.id);
+
+                              setAllCategoriesInView((prev) => {
+                                return [...prev, entry.target.id];
+                              });
+                            } else {
+                              const updatedCategoriesInView =
+                                allCategoriesInView.filter(
+                                  (item) => item !== entry.target.id
+                                );
+                              setAllCategoriesInView(updatedCategoriesInView);
+                              if (inViewCategory === entry.target.id && i > 0) {
+                                for (let j = i - 1; j >= 0; j--) {
+                                  if (layers[j].layers.length > 0) {
+                                    setInViewCategory(layers[j].Title);
+                                  }
+                                }
+                              }
+                            }
+                          }}
+                          id={category?.Title}
+                          key={category?.Title}
+                        >
+                          <p className="mb-4 text-2xl font-semibold">
+                            {category?.Title}
+                          </p>
+                          <div className="grid xl:grid-cols-7 grid-flow-dense lg:grid-cols-5 sm:grid-cols-4 gap-8 mb-4">
+                            {category?.layers?.map((layer: any, i: number) => (
+                              <LibItem
+                                setAdditionalLayers={setAdditionalLayers}
+                                layer={layer}
+                                thumbnails={thumbnails}
+                                setThumbnail={setThumbnail}
+                                activeLayers={activeLayers}
+                                favorites={favorites}
+                                addFavorite={addFavorite}
+                                removeFavorite={removeFavorite}
+                                selectedLayerId={selectedLayerId}
+                                setSelectedLayerId={setSelectedLayerId}
+                                setPreview={setPreview}
+                                key={`${category.Title}_layer_${i}_${layer.id}`}
+                              />
+                            ))}
+                          </div>
+                        </InView>
+                      )}
+                    </div>
+                  ))}
+                {layers && getNumberOfLayers(layers) === 0 && (
+                  <h1 className="text-2xl font-normal">
+                    Keine Ressourcen gefunden
+                  </h1>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+      </Modal>
+    );
+  }
 
   return (
     <Modal
