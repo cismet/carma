2c006a6e561b5fbd25efc9cc7359777d8fbe02e4
David Glogaza <david.glogaza40@googlemail.com>
2024-12-04 09:54:23 +0100

    move new component outside of old one

--- libraries/mapping/carma-map-layers/src/components/NewLibModal.tsx
+++ libraries/mapping/carma-map-layers/src/components/NewLibModal.tsx
@@ -66,7 +66,7 @@ const sidebarElements = [
   { icon: faSearch, text: "Suchergebnisse" },
 ];
 
-export const LibModal = ({
+export const NewLibModal = ({
   open,
   setOpen,
   setAdditionalLayers,
@@ -360,150 +360,172 @@ export const LibModal = ({
       }}
     >
       <div
-        className="w-full h-full flex flex-col bg-[#f2f2f2]"
+        className="w-full h-full flex bg-[#f2f2f2]"
         style={{
           maxHeight: "calc(100vh - 200px)",
           minHeight: "calc(100vh - 200px)",
         }}
       >
-        <div className="sticky top-0 px-6 pt-6">
-          <div className="flex justify-between items-center">
-            <h1 className="mb-0 text-2xl font-semibold">Karteninhalte</h1>
-            {/* <Button
-              onClick={() => {
-                createBaseConfig(layers);
-              }}
-            >
-              Create Base Config
-            </Button> */}
-            {/* <Button
-              onClick={() =>
-                checkDifferences('https://maps.wuppertal.de/umwelt', 'Umwelt')
-              }
-            >
-              Get Config Differences
-            </Button> */}
-            <Search
-              placeholder="Suchbegriff eingeben"
-              className="w-[76%]"
-              allowClear
-              onChange={(e) => {
-                setIsSearching(true);
-                setSearchValue(e.target.value);
-              }}
-              loading={isSearching}
-              onSearch={(value) => {
-                search(value);
-              }}
-            />
-            <Button
-              type="text"
-              onClick={() => {
-                setOpen(false);
-                setPreview(false);
-                setSelectedLayerId(null);
-              }}
-              data-test-id="add-layer-to-map-close-btn"
-            >
-              <FontAwesomeIcon icon={faX} />
-            </Button>
+        <div
+          className={`w-40 h-full flex justify-between items-center flex-col pb-3 bg-gray-600`}
+          style={{ height: "calc(100vh - 188px)" }}
+        >
+          <div className="flex flex-col w-full items-center gap-2">
+            <div className="h-24"></div>
+            {sidebarElements.map((element, i) => {
+              return (
+                <SidebarItem
+                  icon={element.icon}
+                  text={element.text}
+                  active={i === selectedNavItemIndex}
+                  onClick={() => {
+                    setSelectedNavItemIndex(i);
+                  }}
+                />
+              );
+            })}
           </div>
-          {layers && layers.length > 0 && (
-            <>
-              <LayerTabs
-                layers={layers}
-                activeId={inViewCategory}
-                numberOfItems={getNumberOfLayers(layers)}
-              />
-              <hr className="h-px bg-gray-300 border-0 mt-0 mb-2" />
-            </>
-          )}
         </div>
-        {!showItems && open && (
-          <div className="h-full w-full flex items-center justify-center">
-            <div className="grid xl:grid-cols-5 lg:grid-cols-4 sm:grid-cols-2 w-full gap-8 mb-4 px-6 pt-4">
-              {[...Array(10)].map((_, i) => (
-                <div
-                  key={i}
-                  className="bg-white rounded-lg shadow-sm h-80 w-full flex flex-col gap-2 animate-pulse"
-                >
-                  <div className="h-40 p-2 w-full bg-slate-200 rounded-t-lg"></div>
-                  <div className="h-2 bg-slate-200 rounded mx-8 w-1/3"></div>
-                  <div className="h-20 bg-slate-200 rounded mx-8"></div>
-                  <div className="mx-8 flex items-center gap-2">
-                    <div className="h-2 bg-slate-200 rounded w-full"></div>
-                    <span className="text-slate-200"> · </span>
-                    <div className="h-2 bg-slate-200 rounded w-full"></div>
-                  </div>
-                </div>
-              ))}
+
+        <div
+          className="w-full h-full flex flex-col bg-[#f2f2f2]"
+          style={{
+            maxHeight: "calc(100vh - 200px)",
+            minHeight: "calc(100vh - 200px)",
+          }}
+        >
+          <div className="sticky top-0 px-6 pt-6">
+            <div className="flex justify-between items-center">
+              <h1 className="mb-0 text-2xl font-semibold">Karteninhalte</h1>
+              <Search
+                placeholder="Suchbegriff eingeben"
+                className="w-[76%]"
+                allowClear
+                onChange={(e) => {
+                  setIsSearching(true);
+                  setSearchValue(e.target.value);
+                }}
+                loading={isSearching}
+                onSearch={(value) => {
+                  search(value);
+                }}
+              />
+              <Button
+                type="text"
+                onClick={() => {
+                  setOpen(false);
+                  setPreview(false);
+                  setSelectedLayerId(null);
+                }}
+              >
+                <FontAwesomeIcon icon={faX} />
+              </Button>
+            </div>
+            <div className="flex w-full gap-2">
+              {layers && layers.length > 0 && (
+                <>
+                  <LayerTabs
+                    layers={layers.filter(
+                      (layer) =>
+                        layer.Title !== "Favoriten" &&
+                        layer.Title !== "Topic Maps" &&
+                        layer.Title !== "Meine Zusammenstellungen"
+                    )}
+                    activeId={inViewCategory}
+                    numberOfItems={getNumberOfLayers(layers)}
+                  />
+                  <hr className="h-px bg-gray-300 border-0 mt-0 mb-2" />
+                </>
+              )}
             </div>
           </div>
-        )}
-        <div className="overflow-auto pt-0.5">
-          <div className="px-6">
-            {showItems &&
-              layers.map((category, i) => (
-                <div key={category.Title}>
-                  {category.layers.length > 0 && (
-                    <InView
-                      rootMargin="20px 0px 20px 0px"
-                      as="div"
-                      onChange={(inView, entry) => {
-                        if (inView) {
-                          setInViewCategory(entry.target.id);
+          <div className="flex w-full gap-4 h-full overflow-auto pt-0.5 px-6">
+            {!showItems && open && (
+              <div className="h-full w-full flex items-center justify-center">
+                <div className="grid xl:grid-cols-5 lg:grid-cols-4 sm:grid-cols-2 w-full gap-8 mb-4 px-6 pt-4">
+                  {[...Array(10)].map((_, i) => (
+                    <div
+                      key={i}
+                      className="bg-white rounded-lg shadow-sm h-80 w-full flex flex-col gap-2 animate-pulse"
+                    >
+                      <div className="h-40 p-2 w-full bg-slate-200 rounded-t-lg"></div>
+                      <div className="h-2 bg-slate-200 rounded mx-8 w-1/3"></div>
+                      <div className="h-20 bg-slate-200 rounded mx-8"></div>
+                      <div className="mx-8 flex items-center gap-2">
+                        <div className="h-2 bg-slate-200 rounded w-full"></div>
+                        <span className="text-slate-200"> · </span>
+                        <div className="h-2 bg-slate-200 rounded w-full"></div>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            )}
 
-                          setAllCategoriesInView((prev) => {
-                            return [...prev, entry.target.id];
-                          });
-                        } else {
-                          const updatedCategoriesInView =
-                            allCategoriesInView.filter(
-                              (item) => item !== entry.target.id
-                            );
-                          setAllCategoriesInView(updatedCategoriesInView);
-                          if (inViewCategory === entry.target.id && i > 0) {
-                            for (let j = i - 1; j >= 0; j--) {
-                              if (layers[j].layers.length > 0) {
-                                setInViewCategory(layers[j].Title);
+            <div>
+              {showItems &&
+                layers.map((category, i) => (
+                  <div key={category.Title}>
+                    {category.layers.length > 0 && (
+                      <InView
+                        rootMargin="20px 0px 20px 0px"
+                        as="div"
+                        onChange={(inView, entry) => {
+                          if (inView) {
+                            setInViewCategory(entry.target.id);
+
+                            setAllCategoriesInView((prev) => {
+                              return [...prev, entry.target.id];
+                            });
+                          } else {
+                            const updatedCategoriesInView =
+                              allCategoriesInView.filter(
+                                (item) => item !== entry.target.id
+                              );
+                            setAllCategoriesInView(updatedCategoriesInView);
+                            if (inViewCategory === entry.target.id && i > 0) {
+                              for (let j = i - 1; j >= 0; j--) {
+                                if (layers[j].layers.length > 0) {
+                                  setInViewCategory(layers[j].Title);
+                                }
                               }
                             }
                           }
-                        }
-                      }}
-                      id={category?.Title}
-                      key={category?.Title}
-                    >
-                      <p className="mb-4 text-2xl font-semibold">
-                        {category?.Title}
-                      </p>
-                      <div className="grid xl:grid-cols-7 grid-flow-dense lg:grid-cols-5 sm:grid-cols-4 gap-8 mb-4">
-                        {category?.layers?.map((layer: any, i: number) => (
-                          <LibItem
-                            setAdditionalLayers={setAdditionalLayers}
-                            layer={layer}
-                            thumbnails={thumbnails}
-                            setThumbnail={setThumbnail}
-                            activeLayers={activeLayers}
-                            favorites={favorites}
-                            addFavorite={addFavorite}
-                            removeFavorite={removeFavorite}
-                            selectedLayerId={selectedLayerId}
-                            setSelectedLayerId={setSelectedLayerId}
-                            setPreview={setPreview}
-                            key={`${category.Title}_layer_${i}_${layer.id}`}
-                          />
-                        ))}
-                      </div>
-                    </InView>
-                  )}
-                </div>
-              ))}
-            {layers && getNumberOfLayers(layers) === 0 && (
-              <h1 className="text-2xl font-normal">
-                Keine Ressourcen gefunden
-              </h1>
-            )}
+                        }}
+                        id={category?.Title}
+                        key={category?.Title}
+                      >
+                        <p className="mb-4 text-2xl font-semibold">
+                          {category?.Title}
+                        </p>
+                        <div className="grid xl:grid-cols-7 grid-flow-dense lg:grid-cols-5 sm:grid-cols-4 gap-8 mb-4">
+                          {category?.layers?.map((layer: any, i: number) => (
+                            <LibItem
+                              setAdditionalLayers={setAdditionalLayers}
+                              layer={layer}
+                              thumbnails={thumbnails}
+                              setThumbnail={setThumbnail}
+                              activeLayers={activeLayers}
+                              favorites={favorites}
+                              addFavorite={addFavorite}
+                              removeFavorite={removeFavorite}
+                              selectedLayerId={selectedLayerId}
+                              setSelectedLayerId={setSelectedLayerId}
+                              setPreview={setPreview}
+                              key={`${category.Title}_layer_${i}_${layer.id}`}
+                            />
+                          ))}
+                        </div>
+                      </InView>
+                    )}
+                  </div>
+                ))}
+              {layers && getNumberOfLayers(layers) === 0 && (
+                <h1 className="text-2xl font-normal">
+                  Keine Ressourcen gefunden
+                </h1>
+              )}
+            </div>
           </div>
         </div>
       </div>
@@ -511,4 +533,4 @@ export const LibModal = ({
   );
 };
 
-export default LibModal;
+export default NewLibModal;
