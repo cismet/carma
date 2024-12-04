510679af3521a1a9d3bb55ff9358f99207b6388a
PavelOlkhovoi <p-olhovoi@bk.ru>
2024-12-04 18:56:01 +0100

    add print component to prev

--- apps/geoportal/src/app/components/map-print/PrintButton.tsx
+++ apps/geoportal/src/app/components/map-print/PrintButton.tsx
@@ -1,23 +1,33 @@
-import { LoadingOutlined } from "@ant-design/icons";
-import { Spin } from "antd";
-const PrintButton = ({ hadlerStartPrint, loading }) => {
+// import { LoadingOutlined } from "@ant-design/icons";
+// import { Spin } from "antd";
+// const PrintButton = ({ hadlerStartPrint, loading }) => {
+//   return (
+//     <div>
+//       <button
+//         className="rectangle-button ml-auto"
+//         onClick={hadlerStartPrint}
+//         disabled={loading}
+//       >
+//         {loading ? (
+//           <Spin
+//             indicator={<LoadingOutlined spin />}
+//             className="mr-2 "
+//             size="small"
+//           />
+//         ) : (
+//           "Print"
+//         )}
+//       </button>
+//     </div>
+//   );
+// };
+
+// export default PrintButton;
+
+const PrintButton = () => {
   return (
     <div>
-      <button
-        className="rectangle-button ml-auto"
-        onClick={hadlerStartPrint}
-        disabled={loading}
-      >
-        {loading ? (
-          <Spin
-            indicator={<LoadingOutlined spin />}
-            className="mr-2 "
-            size="small"
-          />
-        ) : (
-          "Print"
-        )}
-      </button>
+      <button className="rectangle-button ml-auto">Print</button>
     </div>
   );
 };
