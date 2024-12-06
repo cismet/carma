73fc4d830203fdb79a88b5c7935a4b9099211678
PavelOlkhovoi <p-olhovoi@bk.ru>
2024-12-06 13:41:28 +0100

    fix redrawing with components

--- apps/geoportal/src/app/components/map-print/PrintButton.tsx
+++ apps/geoportal/src/app/components/map-print/PrintButton.tsx
@@ -7,34 +7,37 @@ const PrintButton = ({
   width = "72px",
   height = "34px",
   fontSize = "14px",
+  hide = false,
 }) => {
   // useEffect(() => {
   // }, [width, height]);
   return (
     <>
-      <button
-        className="rectangle-button"
-        onClick={hadlerStartPrint}
-        disabled={loading}
-        style={{
-          fontSize,
-          width,
-          height,
-          opacity: width !== "0px" ? "1" : "0",
-        }}
-      >
-        {/* <FontAwesomeIcon icon={faPrint} className="text-xl cursor-pointer" /> */}
+      {!hide && (
+        <button
+          className="rectangle-button"
+          onClick={hadlerStartPrint}
+          disabled={loading}
+          style={{
+            fontSize,
+            width,
+            height,
+            opacity: width !== "0px" ? "1" : "0",
+          }}
+        >
+          {/* <FontAwesomeIcon icon={faPrint} className="text-xl cursor-pointer" /> */}
 
-        {loading ? (
-          <Spin
-            indicator={<LoadingOutlined spin />}
-            className="mr-2 text-white"
-            size="small"
-          />
-        ) : (
-          "Drucken"
-        )}
-      </button>
+          {loading ? (
+            <Spin
+              indicator={<LoadingOutlined spin />}
+              className="mr-2 text-white"
+              size="small"
+            />
+          ) : (
+            "Drucken"
+          )}
+        </button>
+      )}
     </>
   );
 };
