f01af2101623194875e0bb3fa769b4fc9d19e677
David Glogaza <david.glogaza40@googlemail.com>
2024-12-10 13:11:39 +0100

    add functionality to disable sidebar elements

--- libraries/mapping/carma-map-layers/src/components/SidebarItems.tsx
+++ libraries/mapping/carma-map-layers/src/components/SidebarItems.tsx
@@ -5,18 +5,20 @@ export const SidebarItem = ({
   icon,
   active,
   onClick,
+  disabled,
 }: {
   text: string;
   icon: any;
   active?: boolean;
   onClick?: () => void;
+  disabled?: boolean;
 }) => {
   return (
     <div
-      className={`w-full ${
-        active && "bg-[#f2f2f2]"
+      className={`w-full ${active && "bg-[#f2f2f2]"} ${
+        disabled && "cursor-not-allowed"
       } ml-6 rounded-l-md py-3 flex flex-col gap-1 cursor-pointer items-center`}
-      onClick={onClick}
+      onClick={!disabled ? onClick : undefined}
     >
       <FontAwesomeIcon className="w-9 h-9 mr-3 text-gray-400" icon={icon} />
 
