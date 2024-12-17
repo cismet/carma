3debe03f63b10807d9cd417567fc0b9e0daf204b
Friedrich Hartmann <690920+mxfh@users.noreply.github.com>
2024-12-17 11:54:18 +0100

    feature/3331 fix typo

--- playgrounds/floodingmap/src/App.tsx
+++ playgrounds/floodingmap/src/App.tsx
@@ -203,7 +203,7 @@ function App() {
       (viewer as any)._cesiumWidget._creditContainer.style.display = "none";
       setTimeout(() => {
         console.debug("3d setup for HGK terrain style");
-        viewer.scene.backgroundColnpor = Color.DIMGREY;
+        viewer.scene.backgroundColor = Color.DIMGREY;
         viewer.scene.globe.baseColor = new Color(0.3, 0.2, 0.8, 0.7);
         viewer.scene.globe.show = true;
         viewer.scene.globe.translucency.enabled = true;
