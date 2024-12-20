c803a46f678b3e72b0f5b916d6b9b4c6ff08fec9
David Glogaza <david.glogaza40@googlemail.com>
2024-12-20 18:07:58 +0100

    fetch config from url and apply it

--- apps/geoportal/src/app/App.tsx
+++ apps/geoportal/src/app/App.tsx
@@ -57,6 +57,8 @@ import "react-bootstrap-typeahead/css/Typeahead.css";
 import "react-cismap/topicMaps.css";
 import "./index.css";
 import { changeIfPopupOpend, getIfPopupOpend } from "./store/slices/print";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 
 if (typeof global === "undefined") {
   window.global = window;
@@ -78,7 +80,10 @@ function App({ published }: { published?: boolean }) {
   const selectedMapLayer = useSelector(getSelectedMapLayer);
 
   const [syncToken, setSyncToken] = useState(null);
+  const [loadingConfig, setLoadingConfig] = useState(false);
   const ifPopupPrintOpened = useSelector(getIfPopupOpend);
+
+  const configBaseUrl = "https://gist.githubusercontent.com/d4v3000/";
 
   useEffect(() => {
     console.debug(
@@ -90,6 +95,25 @@ function App({ published }: { published?: boolean }) {
   useEffect(() => {
     if (searchParams.get("sync")) {
       setSyncToken(searchParams.get("sync"));
+    }
+
+    if (searchParams.get("config")) {
+      setLoadingConfig(true);
+      const config = searchParams.get("config");
+
+      fetch(configBaseUrl + config)
+        .then((response) => {
+          return response.json();
+        })
+        .then((newConfig: Config) => {
+          dispatch(setLayers(newConfig.layers));
+          dispatch(setBackgroundLayer(newConfig.backgroundLayer));
+          searchParams.delete("config");
+          setSearchParams(searchParams);
+        })
+        .finally(() => {
+          setLoadingConfig(false);
+        });
     }
 
     if (searchParams.get("data")) {
@@ -205,6 +229,15 @@ function App({ published }: { published?: boolean }) {
     >
       <ErrorBoundary FallbackComponent={AppErrorFallback}>
         <div className="flex flex-col w-full " style={{ height: "100dvh" }}>
+          {loadingConfig && (
+            <div
+              id="loading"
+              className="absolute flex flex-col items-center text-white justify-center h-screen w-full bg-black/50 z-[9999999999999]"
+            >
+              <h2>Lade Konfiguration</h2>
+              <FontAwesomeIcon size="2x" icon={faSpinner} spin />
+            </div>
+          )}
           {!published && <TopNavbar />}
           <MapMeasurement />
           <GeoportalMap />
