86af97f16871b9b084fb14eed20030ec93a38852
PavelOlkhovoi <p-olhovoi@bk.ru>
2024-12-20 17:43:48 +0100

    create full url from point

--- apps/verdis/desktop/src/components/ui/PointSearchButton.jsx
+++ apps/verdis/desktop/src/components/ui/PointSearchButton.jsx
@@ -10,27 +10,13 @@ import { storeShapeMode } from "../../store/slices/searchMode";
 
 const PointSearchButton = () => {
   const dispatch = useDispatch();
-  // const ifDisable = useSelector(getIfShapeModeAvailable);
-  const jwt = useSelector(getJWT);
-  // http://localhost:3033/renderer/?domain=WUNDA_BLAU&jwt={jwt}&table=alkis_landparcel&id={landparcel-id}
-  const handleClick = () => {
-    const landparcelId = "10";
-    const url = `http://localhost:3033/renderer/?domain=WUNDA_BLAU&jwt=${jwt}&table=alkis_landparcel&id=${landparcelId}`;
-    window.open(url, "_blank");
-  };
   return (
     <Tooltip title="Kassenzeichen-Suche">
       <div
         className="relative flex cursor-pointer items-center justify-center"
         onClick={() => dispatch(storeShapeMode("point"))}
       >
-        <FontAwesomeIcon
-          icon={faMagnifyingGlassLocation}
-          className={`h-6`}
-          // style={{
-          //   color: ifDisable ? "black" : "#d3d3d3",
-          // }}
-        />
+        <FontAwesomeIcon icon={faMagnifyingGlassLocation} className={`h-6`} />
       </div>
     </Tooltip>
   );
