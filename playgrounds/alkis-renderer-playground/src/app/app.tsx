28462f1375df99fdd0f8d582df4292ff6335c275
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-01-06 10:57:01 +0100

    add auth functions

--- playgrounds/alkis-renderer-playground/src/app/app.tsx
+++ playgrounds/alkis-renderer-playground/src/app/app.tsx
@@ -1,4 +1,6 @@
-import { Input } from "antd";
+import { faLock, faUser } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Input, Form, Button } from "antd";
 const { Search } = Input;
 
 export function App() {
@@ -7,7 +9,56 @@ export function App() {
   };
   return (
     <div>
-      <Search placeholder="input search text" onSearch={onSearch} enterButton />
+      <Form className="w-full">
+        <div className="flex flex-col gap-6 w-full">
+          <h3 className="text-primary border-b-2 border-0 w-fit border-solid">
+            Anmeldung
+          </h3>
+          <Form.Item
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: "Bitte füge deinen Nutzernamen hinzu",
+              },
+            ]}
+          >
+            <Input
+              placeholder="Nutzername"
+              prefix={<FontAwesomeIcon icon={faUser} color="#E67843" />}
+            />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: "Bitte füge deinen Passwort hinzu",
+              },
+            ]}
+          >
+            <Input.Password
+              placeholder="Passwort"
+              prefix={<FontAwesomeIcon icon={faLock} color="#E67843" />}
+            />
+          </Form.Item>
+          <Button
+            type="primary"
+            size="large"
+            className="w-fit"
+            htmlType="submit"
+          >
+            Anmelden
+          </Button>
+        </div>
+      </Form>
+      <div style={{ marginTop: "40px" }}>
+        <Search
+          placeholder="type alkis id input"
+          onSearch={onSearch}
+          enterButton
+        />
+      </div>
     </div>
   );
 }
