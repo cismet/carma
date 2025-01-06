0019f219ff4ae802f2b50e7f13b3c6b6d83741eb
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-01-06 10:34:08 +0100

    type alkis id

--- playgrounds/alkis-renderer-playground/src/app/app.tsx
+++ playgrounds/alkis-renderer-playground/src/app/app.tsx
@@ -1,12 +1,13 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from "./app.module.css";
-
-import NxWelcome from "./nx-welcome";
+import { Input } from "antd";
+const { Search } = Input;
 
 export function App() {
+  const onSearch = (value: string) => {
+    console.log("xxx Search text:", value.trim());
+  };
   return (
     <div>
-      <NxWelcome title="alkis-renderer-playground" />
+      <Search placeholder="input search text" onSearch={onSearch} enterButton />
     </div>
   );
 }
