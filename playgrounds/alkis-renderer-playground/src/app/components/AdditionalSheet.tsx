7edcdc6abd0abf5bfe988e6abd71b9ee44e9d054
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-01-16 17:40:21 +0100

    fix gaps in owners array

--- playgrounds/alkis-renderer-playground/src/app/components/AdditionalSheet.tsx
+++ playgrounds/alkis-renderer-playground/src/app/components/AdditionalSheet.tsx
@@ -29,7 +29,6 @@ const AdditionalSheet = ({ owners, namesArr, legalDesc }: Props) => {
   const typeOfTitle = namesArr[0];
   const ifLegalDesc = !typeOfTitle.nenner && !typeOfTitle.zaehler;
   const ifWithoutNumber = !typeOfTitle.artRechtsgemeinschaft;
-  console.log("xxx namesArr", namesArr);
   const uuidList = namesArr.map((n) => n.uuid);
 
   const uuidGroupsArr = namesArr
@@ -37,13 +36,9 @@ const AdditionalSheet = ({ owners, namesArr, legalDesc }: Props) => {
     .map((n) => n.namensnummernUUIds)
     .flat();
 
-  console.log("xxx uuidGroupsArr", uuidGroupsArr);
-
   const removedDoubles = uuidList.filter(
     (uuid) => !uuidGroupsArr.includes(uuid)
   );
-
-  console.log("xxx removedDoubles", removedDoubles);
 
   const existingsUids = namesArr
     .filter((n) => removedDoubles.includes(n.uuid))
@@ -90,67 +85,84 @@ const AdditionalSheet = ({ owners, namesArr, legalDesc }: Props) => {
     ownerRes.push(res);
   });
 
-  console.log("xxx ownerRes", ownerRes);
-
-  // eigentuemerUUId = ownerId
-
   return (
-    <div style={{ display: "flex", gap: "2rem" }}>
-      {!ifWithoutNumber && <div>ohne Nr.</div>}
-      <div style={{ width: "70%" }}>
-        {legalDesc && (
+    <div>
+      {ownerRes.map((ownerItem, idx) => {
+        return (
           <div
             style={{
-              paddingBottom: "1.4rem",
-              ...(!ifLegalDesc && {
-                display: "flex",
-                justifyContent: "space-between",
-              }),
+              display: "flex",
+              gap: "2rem",
+              marginBottom: idx !== ownerRes.length - 1 ? "1.7rem" : "0px",
             }}
           >
-            <b>{ifLegalDesc ? "Rechtsgemeinschaft:" : "Erbengemeinschaft:"}</b>{" "}
-            <span>{ifLegalDesc ? legalDesc : "zu 1/2"}</span>
-          </div>
-        )}
-        {owners.map((owner, idx: number) => {
-          const { salutation, firstName, surName, dateOfBirth, nameNumber } =
-            owner;
-          const date = new Date(dateOfBirth);
-          const day = String(date.getDate()).padStart(2, "0");
-          const month = String(date.getMonth() + 1).padStart(2, "0");
-          const year = date.getFullYear();
-          const formattedDate = `${day}.${month}.${year}`;
+            {!ifWithoutNumber && <div>ohne Nr.</div>}
+            <div style={{ width: "70%" }}>
+              {legalDesc && (
+                <div
+                  style={{
+                    paddingBottom: "1.4rem",
+                    ...(!ifLegalDesc && {
+                      display: "flex",
+                      justifyContent: "space-between",
+                    }),
+                  }}
+                >
+                  <b>
+                    {ifLegalDesc ? "Rechtsgemeinschaft:" : "Erbengemeinschaft:"}
+                  </b>{" "}
+                  <span>{ifLegalDesc ? legalDesc : "zu 1/2"}</span>
+                </div>
+              )}
+              {ownerItem.map((owner, idx: number) => {
+                const {
+                  salutation,
+                  firstName,
+                  surName,
+                  dateOfBirth,
+                  nameNumber,
+                } = owner;
+                const date = new Date(dateOfBirth);
+                const day = String(date.getDate()).padStart(2, "0");
+                const month = String(date.getMonth() + 1).padStart(2, "0");
+                const year = date.getFullYear();
+                const formattedDate = `${day}.${month}.${year}`;
 
-          const { houseNumber, postalCode, city, street } = owner.addresses[0];
-          return (
-            <div
-              key={idx}
-              style={{
-                display: "flex",
-                gap: "3rem",
-                paddingBottom: "1.4rem",
-                borderLeft: owners.length > 1 ? "1px solid #d9d9d9" : "0px",
-                paddingLeft: "10px",
-              }}
-            >
-              <div>{nameNumber}</div>
-              <div>
-                <div style={{ paddingBottom: "0.6rem" }}>
-                  {salutation} {firstName || ""} {surName},{" "}
-                  {salutation !== "Firma" ? "*" + formattedDate : ""}
-                </div>
-                <div>
-                  {street} {houseNumber}
-                </div>
-                <div>
-                  {postalCode}, {city}
-                </div>
-                <div>(Grundbuchamtliche Anschrift)</div>
-              </div>
+                const { houseNumber, postalCode, city, street } =
+                  owner.addresses[0];
+                return (
+                  <div
+                    key={idx}
+                    style={{
+                      display: "flex",
+                      gap: "3rem",
+                      paddingBottom: "1.4rem",
+                      borderLeft:
+                        owners.length > 1 ? "1px solid #d9d9d9" : "0px",
+                      paddingLeft: "10px",
+                    }}
+                  >
+                    <div>{nameNumber}</div>
+                    <div>
+                      <div style={{ paddingBottom: "0.6rem" }}>
+                        {salutation} {firstName || ""} {surName},{" "}
+                        {salutation !== "Firma" ? "*" + formattedDate : ""}
+                      </div>
+                      <div>
+                        {street} {houseNumber}
+                      </div>
+                      <div>
+                        {postalCode}, {city}
+                      </div>
+                      <div>(Grundbuchamtliche Anschrift)</div>
+                    </div>
+                  </div>
+                );
+              })}
             </div>
-          );
-        })}
-      </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
