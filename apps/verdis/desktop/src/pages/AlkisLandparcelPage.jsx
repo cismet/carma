37ac4c96533edb72a194caa9f05d05c0325cfe8d
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-01-17 15:59:33 +0100

    style the title

--- apps/verdis/desktop/src/pages/AlkisLandparcelPage.jsx
+++ apps/verdis/desktop/src/pages/AlkisLandparcelPage.jsx
@@ -3,6 +3,7 @@ import { useSearchParams } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { getJWT } from "../store/slices/auth.js";
 import { getLandparcelHtml } from "../helper/landparcelSearch.jsx";
+import InfoBar from "../components/commons/InfoBar.jsx";
 
 const AlkisLandparcelPage = () => {
   const [searchParams] = useSearchParams();
@@ -24,12 +25,20 @@ const AlkisLandparcelPage = () => {
 
   return (
     <div>
-      {idTitle && (
-        <div className="my-5">
-          <div style={{ cursor: "pointer" }}>{idTitle}</div>
-          {resHtml && <div style={{ marginTop: "40px" }}>{resHtml}</div>}
+      <div className="flex flex-col items-center relative h-full max-h-[calc(100vh-73px)]">
+        <div className="flex flex-col gap-2 w-full bg-zinc-100 h-full overflow-clip p-2">
+          <InfoBar title={`Flurstück: ${idTitle}`} className="py-1" />
+
+          <div className="">
+            {idTitle && (
+              <div className="my-5">
+                {/* <div style={{ cursor: "pointer" }}>{idTitle}</div> */}
+                {resHtml && <div>{resHtml}</div>}
+              </div>
+            )}
+          </div>
         </div>
-      )}
+      </div>
     </div>
   );
 };
