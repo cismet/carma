9f6187ce181882044381188bd15b9ed77cadcff1
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-01-17 13:06:01 +0100

    fix components names

--- apps/verdis/desktop/src/pages/AlkisLandparcelPage.jsx
+++ apps/verdis/desktop/src/pages/AlkisLandparcelPage.jsx
@@ -1,8 +1,14 @@
+import { useState } from "react";
 import { useSearchParams } from "react-router-dom";
+// import { getJWT } from "../../../store/slices/auth";
+import { useDispatch, useSelector } from "react-redux";
+import { getJWT } from "../store/slices/auth.js";
 
 const AlkisLandparcelPage = () => {
   const [searchParams] = useSearchParams();
   const id = searchParams.get("id");
+  const jwt = useSelector(getJWT);
+  const [resHtml, setResHtml] = (useState < JSX.Element) | (null > null);
 
   return (
     <div>
