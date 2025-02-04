e0ae1c34ec54f344145cf615bdc5f7cee1a9c2cd
Friedrich Hartmann <690920+mxfh@users.noreply.github.com>
2025-02-04 22:08:58 +0100

    3419 fix floodingmap, by using withErrorboundary for Forwarder outside of ErrorBoundary context

--- libraries/mapping/engines/cesium/src/lib/utils/CesiumErrorToErrorBoundaryForwarder.tsx
+++ libraries/mapping/engines/cesium/src/lib/utils/CesiumErrorToErrorBoundaryForwarder.tsx
@@ -1,4 +1,8 @@
-import { useErrorBoundary } from "react-error-boundary";
+import {
+  ErrorBoundaryProps,
+  useErrorBoundary,
+  withErrorBoundary,
+} from "react-error-boundary";
 import { useState, useEffect } from "react";
 import { CesiumWidget } from "cesium";
 
@@ -24,34 +28,41 @@ const overrideCesiumWidgetShowErrorPanel = function (
   };
 };
 
-export const CesiumErrorToErrorBoundaryForwarder = () => {
-  const [cesiumError, setCesiumError] = useState<ForwardedCesiumError | null>(
-    null
-  );
-
-  const { showBoundary } = useErrorBoundary();
-
-  useEffect(() => {
-    if (!showBoundary) {
-      console.warn(
-        "CesiumErrorToErrorBoundaryForwarder used outside of error boundary"
-      );
-      return;
-    }
-    console.debug(
-      "overriding CesiumWidget.showErrorPanel with custom Error forwarder"
+export const CesiumErrorToErrorBoundaryForwarder = withErrorBoundary(
+  function CesiumErrorToErrorBoundaryForwarder() {
+    const [cesiumError, setCesiumError] = useState<ForwardedCesiumError | null>(
+      null
     );
-    overrideCesiumWidgetShowErrorPanel(setCesiumError);
-  }, [showBoundary]);
 
-  useEffect(() => {
-    if (cesiumError && showBoundary) {
-      showBoundary(cesiumError);
-      setCesiumError(null);
-    }
-  }, [cesiumError, showBoundary]);
+    const { showBoundary } = useErrorBoundary();
 
-  return null;
-};
+    useEffect(() => {
+      console.debug(
+        "overriding CesiumWidget.showErrorPanel with custom Error forwarder"
+      );
+      overrideCesiumWidgetShowErrorPanel(setCesiumError);
+    }, [showBoundary]);
+
+    useEffect(() => {
+      if (cesiumError && showBoundary) {
+        showBoundary(cesiumError);
+        setCesiumError(null);
+      }
+    }, [cesiumError, showBoundary]);
+
+    return null;
+  },
+  {
+    // render component intentionally missing to not override external ErrorBoundary
+    // withErrorBoundary is used to allow use of this component outside of ErrorBoundary contexts without causing errors because of the missing context
+    onError: (error, info) => {
+      console.error(
+        "Consider using ErrorBoundary to manage Cesium errors with app context",
+        error,
+        info
+      );
+    },
+  } as ErrorBoundaryProps
+);
 
 export default CesiumErrorToErrorBoundaryForwarder;
