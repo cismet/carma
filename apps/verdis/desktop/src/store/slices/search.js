42856564ec2fa5480078b0b65367b4da8f2f6edb
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-02-14 09:18:36 +0100

    open links online

--- apps/verdis/desktop/src/store/slices/search.js
+++ apps/verdis/desktop/src/store/slices/search.js
@@ -791,12 +791,12 @@ export const searchWithPoints = (searchParams) => {
         const ids = result.data.alkis_landparcel[0].id;
         const baseUrl = window.location.origin;
 
-        const url = `http://localhost:3033/renderer/?domain=WUNDA_BLAU&jwt=${jwt}&table=alkis_landparcel&id=${ids}`;
-        // const url = `${baseUrl}/#/alkis-flurstueck/?id=${ids}`;
-        // window.open(url, "_self");
-        fetch(url).catch((error) => {
-          //  i expect an error here
-        });
+        //const url = `http://localhost:3033/renderer/?domain=WUNDA_BLAU&jwt=${jwt}&table=alkis_landparcel&id=${ids}`;
+        const url = `${baseUrl}/#/alkis-flurstueck/?id=${ids}`;
+        window.open(url, "_blank");
+        // fetch(url).catch((error) => {
+        //   //  i expect an error here
+        // });
 
         dispatch(setGraphqlStatus("LOADED"));
       })
