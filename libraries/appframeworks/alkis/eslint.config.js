9c72e012e491b3b2f113e00e574a538159065027
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-02-17 18:13:53 +0100

    update eslint config

--- libraries/appframeworks/alkis/eslint.config.js
+++ libraries/appframeworks/alkis/eslint.config.js
@@ -4,10 +4,21 @@ module.exports = [
   ...baseConfig,
   {
     files: ["**/*.ts", "**/*.tsx", "**/*.js", "**/*.jsx"],
+    parserOptions: {
+      EXPERIMENTAL_useProjectService: {
+        // Increase this value to allow more files
+        maximumDefaultProjectFileMatchCount_THIS_WILL_SLOW_DOWN_LINTING: 1000,
+      },
+    },
     rules: {},
   },
   {
     files: ["**/*.ts", "**/*.tsx"],
+    parserOptions: {
+      EXPERIMENTAL_useProjectService: {
+        maximumDefaultProjectFileMatchCount_THIS_WILL_SLOW_DOWN_LINTING: 1000,
+      },
+    },
     rules: {},
   },
   {
