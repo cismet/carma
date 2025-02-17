da9a4e3317e631e0c3c58ccbe9cbedd1acf7cfb7
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-02-17 16:58:39 +0100

    add info bar

---
+++ libraries/appframeworks/alkis/src/lib/components/InfoBar.tsx
@@ -0,0 +1,16 @@
+import React, { ReactNode } from "react";
+
+type InfoBarProps = {
+  title: string;
+  children?: ReactNode;
+  className?: string;
+};
+
+export const InfoBar = ({ title, children, className }: InfoBarProps) => {
+  return (
+    <div className="flex items-center justify-between w-full">
+      <h4 className={`font-semibold text-lg m-0 ${className}`}>{title}</h4>
+      <div className="flex items-center gap-2">{children}</div>
+    </div>
+  );
+};
