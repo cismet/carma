05e05f1a07d06f77094c5a71a2a2f0b96bf77724
David Glogaza <david.glogaza40@googlemail.com>
2025-02-20 15:31:14 +0100

    add delay & show secondary info elements

--- apps/geoportal/src/app/components/feature-info/LoadingInfoBox.tsx
+++ apps/geoportal/src/app/components/feature-info/LoadingInfoBox.tsx
@@ -1,6 +1,48 @@
 import InfoBox from "react-cismap/topicmaps/InfoBox";
+import { useSelector } from "react-redux";
+import { getLayers } from "../../store/slices/mapping";
+import InfoBoxHeader from "react-cismap/topicmaps/InfoBoxHeader";
+import { useEffect, useState } from "react";
 
 const LoadingInfoBox = () => {
+  const layers = useSelector(getLayers);
+  const [shouldRender, setShouldRender] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShouldRender(true);
+    }, 500);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (!shouldRender) {
+    return null;
+  }
+
+  const featureHeaders = layers.map((layer, i) => {
+    return (
+      <div
+        style={{
+          width: "340px",
+          paddingBottom: 3,
+          paddingLeft: 10 + i * 10,
+          cursor: "pointer",
+        }}
+        key={"overlapping."}
+      >
+        <InfoBoxHeader
+          content={
+            <div className="w-full h-4 flex items-center">
+              <div className="w-14 h-2 bg-zinc-600 rounded-md animate-pulse" />
+            </div>
+          }
+          headerColor={"grey"}
+        ></InfoBoxHeader>
+      </div>
+    );
+  });
+
   return (
     <InfoBox
       pixelwidth={350}
@@ -15,6 +57,7 @@ const LoadingInfoBox = () => {
         <div className="w-36 h-2 bg-zinc-400 rounded-md animate-pulse mb-4" />
       }
       header="Laden"
+      secondaryInfoBoxElements={featureHeaders}
     />
   );
 };
