8773eebf13795274afc2883ad6b04e72e040c6ea
David Glogaza <david.glogaza40@googlemail.com>
2025-02-25 18:03:12 +0100

    add style for ios fullscreen map movement

--- apps/geoportal/src/app/components/GeoportalMap/GeoportalMap.tsx
+++ apps/geoportal/src/app/components/GeoportalMap/GeoportalMap.tsx
@@ -351,7 +351,13 @@ export const GeoportalMap = ({ height, width, allow3d }: MapProps) => {
           locatorControl={false}
           fullScreenControl={false}
           zoomControls={false}
-          mapStyle={{ width, height }}
+          mapStyle={{
+            width,
+            height,
+            touchAction: "none",
+            WebkitOverflowScrolling: "touch",
+            overscrollBehavior: "none",
+          }}
           leafletMapProps={{ editable: true }}
           minZoom={10}
           backgroundlayers="empty"
