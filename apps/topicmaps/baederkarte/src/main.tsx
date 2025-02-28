3f21ef1130aa1cb4e338791b2370baea5d8ab714
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-02-28 13:17:31 +0100

    add config to baederkarte

--- apps/topicmaps/baederkarte/src/main.tsx
+++ apps/topicmaps/baederkarte/src/main.tsx
@@ -2,13 +2,14 @@ import { StrictMode } from "react";
 import * as ReactDOM from "react-dom/client";
 import { GazDataProvider, SelectionProvider } from "@carma-apps/portals";
 import App from "./app/App.jsx";
+import { gazDataConfig } from "./config/gazData.js";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <StrictMode>
-    <GazDataProvider>
+    <GazDataProvider config={gazDataConfig}>
       <SelectionProvider>
         <App />
       </SelectionProvider>
