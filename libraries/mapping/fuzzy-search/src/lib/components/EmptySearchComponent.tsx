83e717bc5a1854d6ff3e91d82d5b02f13187f856
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-03-03 11:15:45 +0100

    add components folder

---
+++ libraries/mapping/fuzzy-search/src/lib/components/EmptySearchComponent.tsx
@@ -0,0 +1,15 @@
+interface EmptySearchComponentProps {
+  pixelwidth: string | number;
+}
+export const EmptySearchComponent = ({
+  pixelwidth = 350,
+}: EmptySearchComponentProps) => {
+  return (
+    <div
+      style={{
+        width: pixelwidth,
+        height: "44px",
+      }}
+    ></div>
+  );
+};
