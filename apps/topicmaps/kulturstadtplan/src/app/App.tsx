45e25e6e321e957fdbef3290b258bf70ba827322
Thorsten Hell <thorsten.hell@cismet.de>
2025-03-14 16:49:33 +0100

    fiexed wrong url

--- apps/topicmaps/kulturstadtplan/src/app/App.tsx
+++ apps/topicmaps/kulturstadtplan/src/app/App.tsx
@@ -24,12 +24,9 @@ export function App() {
   return (
     <TopicMapContextProvider
       appKey="KulturKarteWuppertal2022"
-      // featureItemsURL={
-      //   import.meta.env.VITE_WUPP_ASSET_BASEURL +
-      //   "data/veranstaltungsorte.data.json"
-      // }
       featureItemsURL={
-        "https://wupp-topicmaps-data.cismet.de/data/veranstaltungsorte.data.json"
+        import.meta.env.VITE_WUPP_ASSET_BASEURL +
+        "/data/veranstaltungsorte.data.json"
       }
       referenceSystemDefinition={MappingConstants.proj4crs25832def}
       createFeatureItemsDictionary={createItemsDictionary}
