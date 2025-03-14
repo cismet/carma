5360ffe8bf7ee4703b5de8427ac1dc9585aabf82
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-03-14 11:42:16 +0100

    change gaz configs

--- apps/topicmaps/baederkarte/src/config/gazData.js
+++ apps/topicmaps/baederkarte/src/config/gazData.js
@@ -9,7 +9,7 @@ const endpoints = [
   ENDPOINT.POIS,
 ];
 
-const host = "https://wupp-digitaltwin-assets.cismet.de";
+const host = import.meta.env.VITE_WUPP_ASSET_BASEURL;
 const crs = "25832";
 
 const sources = endpoints.map((endpoint) => {
--- apps/topicmaps/bplan-auskunft/src/config/gazData.js
+++ apps/topicmaps/bplan-auskunft/src/config/gazData.js
@@ -10,7 +10,7 @@ const endpoints = [
   ENDPOINT.KITAS,
   ENDPOINT.BPLAENE,
 ];
-const host = "https://wupp-digitaltwin-assets.cismet.de";
+const host = import.meta.env.VITE_WUPP_ASSET_BASEURL;
 const crs = "25832";
 
 const sources = endpoints.map((endpoint) => {
