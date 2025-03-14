b99e5a26c6d1b5b7a30baef9a28c175800b0ad44
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-03-14 12:02:07 +0100

    set gaz data with env

--- apps/topicmaps/stadtplan/src/app/config/gazData.js
+++ apps/topicmaps/stadtplan/src/app/config/gazData.js
@@ -11,7 +11,7 @@ const sources = [
   ENDPOINT.POIS,
   ENDPOINT.KITAS,
 ];
-const host = "https://wupp-digitaltwin-assets.cismet.de";
+const host = import.meta.env.VITE_WUPP_ASSET_BASEURL;
 const crs = "25832";
 
 export const sourcesConfig = sources.map((endpoint) => {
