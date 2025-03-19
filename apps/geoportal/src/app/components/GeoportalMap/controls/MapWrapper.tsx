5107d900642168da043701aa673bc52d94ee4bba
Friedrich Hartmann <690920+mxfh@users.noreply.github.com>
2025-03-19 14:24:39 +0100

    3495/add device specs to errorboundary header

--- apps/geoportal/src/app/components/GeoportalMap/controls/MapWrapper.tsx
+++ apps/geoportal/src/app/components/GeoportalMap/controls/MapWrapper.tsx
@@ -254,8 +254,6 @@ const MapWrapper = () => {
   lastRenderIntervalRef.current = Date.now() - lastRenderTimeStampRef.current;
   lastRenderTimeStampRef.current = Date.now();
 
-return null;
-
   return (
     <ControlLayout onHeightResize={setLayoutHeight} ifStorybook={false}>
       {zenMode ? (
