2c6af11b55276a6bd968e9056f526896160d8cab
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-03-20 17:28:24 +0100

    comment out check for debugging

--- apps/belis/online/src/serviceWorkerRegistration.js
+++ apps/belis/online/src/serviceWorkerRegistration.js
@@ -34,12 +34,12 @@ export function register(config) {
     // const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
     console.log("xxx publicUrl", baseUrl);
 
-    if (baseUrl !== window.location.origin) {
-      // Our service worker won't work if PUBLIC_URL is on a different origin
-      // from what our page is served on. This might happen if a CDN is used to
-      // serve assets; see https://github.com/facebook/create-react-app/issues/2374
-      return;
-    }
+    // if (baseUrl !== window.location.origin) {
+    // Our service worker won't work if PUBLIC_URL is on a different origin
+    // from what our page is served on. This might happen if a CDN is used to
+    // serve assets; see https://github.com/facebook/create-react-app/issues/2374
+    // return;
+    // }
 
     window.addEventListener("load", () => {
       // const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
