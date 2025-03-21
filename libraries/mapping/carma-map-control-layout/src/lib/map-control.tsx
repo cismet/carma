c4cee17aa35dbcb12ebf7c345f63ef2921a8fc8d
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-03-21 17:43:32 +0100

    clean code

--- libraries/mapping/carma-map-control-layout/src/lib/map-control.tsx
+++ libraries/mapping/carma-map-control-layout/src/lib/map-control.tsx
@@ -121,7 +121,6 @@ const ControlLayout: React.FC<ControlLayoutProps> = ({
   onHeightResize = (set: number | null) => set,
   ifStorybook = true,
 }) => {
-  console.log("xxx ControlLayout");
   const [windowWidth, setWindowWidth] = useState(0);
   const [layoutHeight, setLayoutHeight] = useState<number | null>(null);
   const [screenSizeWatcher, setScreenSizeWatcher] = useState("");
