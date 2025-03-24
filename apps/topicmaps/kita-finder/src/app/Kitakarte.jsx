e50d168bc31b6482b2ba7735cd615852f5dc8afd
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-03-24 18:05:57 +0100

    #276 add control layout

--- apps/topicmaps/kita-finder/src/app/Kitakarte.jsx
+++ apps/topicmaps/kita-finder/src/app/Kitakarte.jsx
@@ -24,6 +24,20 @@ import { TopicMapContext } from "react-cismap/contexts/TopicMapContextProvider";
 import { TopicMapSelectionContent } from "@carma-apps/portals";
 import { EmptySearchComponent } from "@carma-mapping/fuzzy-search";
 import FuzzySearch from "./components/FuzzySearch";
+import {
+  Control,
+  ControlButtonStyler,
+  ControlLayout,
+} from "@carma-mapping/map-controls-layout";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCompress,
+  faExpand,
+  faMinus,
+  faPlus,
+} from "@fortawesome/free-solid-svg-icons";
+import useLeafletZoomControls from "../hooks/useLeafletZoomControls";
+import { RoutedMapLocateControl } from "@carma-mapping/components";
 
 const KitaKarte = () => {
   const { setSelectedFeatureByPredicate, setClusteringOptions } = useContext(
@@ -31,6 +45,7 @@ const KitaKarte = () => {
   );
   const { routedMapRef } = useContext(TopicMapContext);
   const { clusteringOptions } = useContext(FeatureCollectionContext);
+  const { zoomInLeaflet, zoomOutLeaflet } = useLeafletZoomControls();
 
   const { additionalStylingInfo } = useContext(TopicMapStylingContext);
 
@@ -75,6 +90,73 @@ const KitaKarte = () => {
 
   return (
     <>
+      <div
+        className="controls-container"
+        style={{
+          position: "absolute",
+          top: "0px",
+          left: "0px",
+          bottom: "0px",
+          zIndex: 600,
+        }}
+      >
+        <ControlLayout ifStorybook={false}>
+          <Control position="topleft" order={10}>
+            <div className="flex flex-col">
+              <ControlButtonStyler
+                onClick={zoomInLeaflet}
+                className="!border-b-0 !rounded-b-none font-bold !z-[9999999]"
+                dataTestId="zoom-in-control"
+                title="Vergrößern"
+              >
+                <FontAwesomeIcon icon={faPlus} className="text-base" />
+              </ControlButtonStyler>
+              <ControlButtonStyler
+                onClick={zoomOutLeaflet}
+                className="!rounded-t-none !border-t-[1px]"
+                dataTestId="zoom-out-control"
+                title="Verkleinern"
+              >
+                <FontAwesomeIcon icon={faMinus} className="text-base" />
+              </ControlButtonStyler>
+            </div>
+          </Control>
+
+          <Control position="topleft" order={50}>
+            <ControlButtonStyler
+              title={
+                document.fullscreenElement
+                  ? "Vollbildmodus beenden"
+                  : "Vollbildmodus"
+              }
+              onClick={() => {
+                if (document.fullscreenElement) {
+                  document.exitFullscreen();
+                } else {
+                  document.documentElement.requestFullscreen();
+                }
+              }}
+              dataTestId="full-screen-control"
+            >
+              <FontAwesomeIcon
+                icon={document.fullscreenElement ? faCompress : faExpand}
+              />
+            </ControlButtonStyler>
+          </Control>
+          <Control position="topleft" order={60} title="Mein Standort">
+            <RoutedMapLocateControl
+              tourRefLabels={null}
+              disabled={false}
+              nativeTooltip={true}
+            />
+          </Control>
+          <Control position="bottomleft" order={10}>
+            <div data-test-id="fuzzy-search" className="h-full w-full pl-2">
+              <FuzzySearch searchTextPlaceholder={searchTextPlaceholder} />
+            </div>
+          </Control>
+        </ControlLayout>
+      </div>
       <TopicMapComponent
         modalMenu={
           <Menu previewFeatureCollectionProps={featureCollectionProps} />
@@ -116,7 +198,6 @@ const KitaKarte = () => {
           {...featureCollectionProps}
         ></FeatureCollection>
       </TopicMapComponent>
-      <FuzzySearch searchTextPlaceholder={searchTextPlaceholder} />
     </>
   );
 };
