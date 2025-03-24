f3ef21a84ca63b4a35774f36918248cfb1cd4e97
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-03-24 14:08:07 +0100

    #276 add control layout

--- apps/topicmaps/stadtplan/src/app/Stadtplankarte.jsx
+++ apps/topicmaps/stadtplan/src/app/Stadtplankarte.jsx
@@ -20,6 +20,20 @@ import IconComp from "react-cismap/commons/Icon";
 import { getPoiClusterIconCreatorFunction } from "./helper/styler";
 import Menu from "./Menu";
 import FuzzySearch from "./components/FuzzySearch";
+import {
+  Control,
+  ControlButtonStyler,
+  ControlLayout,
+} from "@carma-mapping/map-controls-layout";
+import useLeafletZoomControls from "../hooks/useLeafletZoomControls";
+import { RoutedMapLocateControl } from "@carma-mapping/components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCompress,
+  faExpand,
+  faMinus,
+  faPlus,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Stadtplankarte = ({ poiColors }) => {
   const { setClusteringOptions } = useContext(FeatureCollectionDispatchContext);
@@ -28,6 +42,8 @@ const Stadtplankarte = ({ poiColors }) => {
   const { clusteringOptions, selectedFeature, filterState } = useContext(
     FeatureCollectionContext
   );
+
+  const { zoomInLeaflet, zoomOutLeaflet } = useLeafletZoomControls();
 
   useEffect(() => {
     if (markerSymbolSize) {
@@ -43,9 +59,78 @@ const Stadtplankarte = ({ poiColors }) => {
 
   return (
     <>
+      <div
+        className="controls-container"
+        style={{
+          position: "absolute",
+          top: "0px",
+          left: "0px",
+          bottom: "0px",
+          zIndex: 600,
+        }}
+      >
+        <ControlLayout ifStorybook={false}>
+          <Control position="topleft" order={10}>
+            <div className="flex flex-col">
+              <ControlButtonStyler
+                onClick={zoomInLeaflet}
+                className="!border-b-0 !rounded-b-none font-bold !z-[9999999]"
+                dataTestId="zoom-in-control"
+                title="Vergrößern"
+              >
+                <FontAwesomeIcon icon={faPlus} className="text-base" />
+              </ControlButtonStyler>
+              <ControlButtonStyler
+                onClick={zoomOutLeaflet}
+                className="!rounded-t-none !border-t-[1px]"
+                dataTestId="zoom-out-control"
+                title="Verkleinern"
+              >
+                <FontAwesomeIcon icon={faMinus} className="text-base" />
+              </ControlButtonStyler>
+            </div>
+          </Control>
+
+          <Control position="topleft" order={50}>
+            <ControlButtonStyler
+              title={
+                document.fullscreenElement
+                  ? "Vollbildmodus beenden"
+                  : "Vollbildmodus"
+              }
+              onClick={() => {
+                if (document.fullscreenElement) {
+                  document.exitFullscreen();
+                } else {
+                  document.documentElement.requestFullscreen();
+                }
+              }}
+              dataTestId="full-screen-control"
+            >
+              <FontAwesomeIcon
+                icon={document.fullscreenElement ? faCompress : faExpand}
+              />
+            </ControlButtonStyler>
+          </Control>
+          <Control position="topleft" order={60} title="Mein Standort">
+            <RoutedMapLocateControl
+              tourRefLabels={null}
+              disabled={false}
+              nativeTooltip={true}
+            />
+          </Control>
+          <Control position="bottomleft" order={10}>
+            <div data-test-id="fuzzy-search" className="h-full w-full pl-2">
+              <FuzzySearch />
+            </div>
+          </Control>
+        </ControlLayout>
+      </div>
       <TopicMapComponent
         modalMenu={<Menu />}
-        locatorControl={true}
+        locatorControl={false}
+        fullScreenControl={false}
+        zoomControls={false}
         gazetteerSearchControl={true}
         gazetteerSearchComponent={EmptySearchComponent}
         applicationMenuTooltipString={<MenuTooltip />}
@@ -93,7 +178,6 @@ const Stadtplankarte = ({ poiColors }) => {
         <TopicMapSelectionContent />
         <FeatureCollection></FeatureCollection>
       </TopicMapComponent>
-      <FuzzySearch searchTextPlaceholder={searchTextPlaceholder} />
     </>
   );
 };
