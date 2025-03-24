6a825073c93ccbd0423e6aedeba1a7e027186376
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-03-24 16:44:22 +0100

    #276 clean unused code

--- apps/topicmaps/e-bikes/src/app/components/Map.tsx
+++ apps/topicmaps/e-bikes/src/app/components/Map.tsx
@@ -1,21 +1,16 @@
 import { useContext, useEffect } from "react";
-
 import {
   FeatureCollectionContext,
   FeatureCollectionDispatchContext,
 } from "react-cismap/contexts/FeatureCollectionContextProvider";
 import { TopicMapStylingContext } from "react-cismap/contexts/TopicMapStylingContextProvider";
-
 import {
   UIContext,
   UIDispatchContext,
 } from "react-cismap/contexts/UIContextProvider";
-import { ResponsiveTopicMapContext } from "react-cismap/contexts/ResponsiveTopicMapContextProvider";
-
 import FeatureCollection from "react-cismap/FeatureCollection";
 import GenericInfoBoxFromFeature from "react-cismap/topicmaps/GenericInfoBoxFromFeature";
 import TopicMapComponent from "react-cismap/topicmaps/TopicMapComponent";
-
 import { getPoiClusterIconCreatorFunction } from "../../helper/styler";
 import Menu from "./Menu";
 import SecondaryInfoModal from "./SecondaryInfoModal";
@@ -25,24 +20,11 @@ import {
   MenuTooltip,
   searchTextPlaceholder,
 } from "@carma-collab/wuppertal/e-bikes";
-
-import {
-  SelectionMetaData,
-  TopicMapSelectionContent,
-  useGazData,
-  useSelection,
-  useSelectionTopicMap,
-} from "@carma-apps/portals";
-import {
-  EmptySearchComponent,
-  LibFuzzySearch,
-  SearchResultItem,
-} from "@carma-mapping/fuzzy-search";
-import { ENDPOINT, isAreaType } from "@carma-commons/resources";
-import FuzzySearch from "./FuzzySearch";
+import { TopicMapSelectionContent } from "@carma-apps/portals";
+import { EmptySearchComponent } from "@carma-mapping/fuzzy-search";
 
 const Map = () => {
-  const { setSelectedFeatureByPredicate, setClusteringOptions } = useContext<
+  const { setClusteringOptions } = useContext<
     typeof FeatureCollectionDispatchContext
   >(FeatureCollectionDispatchContext);
   const { markerSymbolSize } = useContext<typeof TopicMapStylingContext>(
@@ -55,13 +37,6 @@ const Map = () => {
   const { setSecondaryInfoVisible } =
     useContext<typeof UIDispatchContext>(UIDispatchContext);
 
-  const { responsiveState, gap, windowSize } = useContext<
-    typeof ResponsiveTopicMapContext
-  >(ResponsiveTopicMapContext);
-
-  const pixelwidth =
-    responsiveState === "normal" ? "300px" : windowSize.width - gap;
-
   useEffect(() => {
     if (markerSymbolSize) {
       setClusteringOptions({
@@ -70,31 +45,6 @@ const Map = () => {
       });
     }
   }, [markerSymbolSize]);
-
-  const { gazData } = useGazData();
-  const { setSelection } = useSelection();
-
-  useSelectionTopicMap();
-
-  const onGazetteerSelection = (selection: SearchResultItem | null) => {
-    if (!selection) {
-      setSelection(null);
-      return;
-    }
-    const selectionMetaData: SelectionMetaData = {
-      selectedFrom: "gazetteer",
-      selectionTimestamp: Date.now(),
-      isAreaSelection: isAreaType(selection.type as ENDPOINT),
-    };
-    setSelection(Object.assign({}, selection, selectionMetaData));
-
-    setTimeout(() => {
-      const gazId = selection.more?.pid || selection.more?.kid;
-      setSelectedFeatureByPredicate(
-        (feature) => feature.properties.id === gazId
-      );
-    }, 100);
-  };
 
   return (
     <>
@@ -132,7 +82,6 @@ const Map = () => {
         <TopicMapSelectionContent />
         <FeatureCollection></FeatureCollection>
       </TopicMapComponent>
-      <FuzzySearch searchTextPlaceholder={searchTextPlaceholder} />
     </>
   );
 };
