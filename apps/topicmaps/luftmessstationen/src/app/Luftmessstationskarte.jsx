4522a9ad34db2e7ef570a94111ce935aef07b706
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-03-25 16:43:07 +0100

    #276 add layout control

--- apps/topicmaps/luftmessstationen/src/app/Luftmessstationskarte.jsx
+++ apps/topicmaps/luftmessstationen/src/app/Luftmessstationskarte.jsx
@@ -1,19 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "leaflet/dist/leaflet.css";
-import React, { useContext, useEffect, useState } from "react";
+import { useEffect } from "react";
 import "react-bootstrap-typeahead/css/Typeahead.css";
 import ContactButton from "react-cismap/ContactButton";
-import {
-  FeatureCollectionContext,
-  FeatureCollectionDispatchContext,
-} from "react-cismap/contexts/FeatureCollectionContextProvider";
 import FeatureCollection from "react-cismap/FeatureCollection";
 import "react-cismap/topicMaps.css";
 import GenericInfoBoxFromFeature from "react-cismap/topicmaps/GenericInfoBoxFromFeature";
 import TopicMapComponent from "react-cismap/topicmaps/TopicMapComponent";
 import "./App.css";
-import { getStatus } from "./helper/convertItemToFeature";
-import { getGazData } from "./helper/getGazData";
 import MyMenu from "./Menu";
 import InfoPanel from "./SecondaryInfo";
 import {
@@ -22,64 +16,104 @@ import {
   InfoBoxTextContent,
 } from "@carma-collab/wuppertal/luftmessstationen";
 
+import { TopicMapSelectionContent } from "@carma-apps/portals";
+import { EmptySearchComponent } from "@carma-mapping/fuzzy-search";
+import FuzzySearch from "./components/FuzzySearch";
 import {
-  TopicMapSelectionContent,
-  useGazData,
-  useSelection,
-  useSelectionTopicMap,
-} from "@carma-apps/portals";
+  Control,
+  ControlButtonStyler,
+  ControlLayout,
+} from "@carma-mapping/map-controls-layout";
+import useLeafletZoomControls from "../hooks/useLeafletZoomControls";
+import { RoutedMapLocateControl } from "@carma-mapping/components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  EmptySearchComponent,
-  LibFuzzySearch,
-} from "@carma-mapping/fuzzy-search";
-import { isAreaType } from "@carma-commons/resources";
-import { ResponsiveTopicMapContext } from "react-cismap/contexts/ResponsiveTopicMapContextProvider";
+  faCompress,
+  faExpand,
+  faMinus,
+  faPlus,
+} from "@fortawesome/free-solid-svg-icons";
 
 function Comp() {
-  const { setSelectedFeatureByPredicate } = useContext(
-    FeatureCollectionDispatchContext
-  );
-
-  const { responsiveState, gap, windowSize } = useContext(
-    ResponsiveTopicMapContext
-  );
-
-  const pixelwidth =
-    responsiveState === "normal" ? "300px" : windowSize.width - gap;
+  const { zoomInLeaflet, zoomOutLeaflet } = useLeafletZoomControls();
 
   useEffect(() => {
     document.title = "Luftmessstationskarte Wuppertal";
   }, []);
 
-  const { gazData } = useGazData();
-  const { setSelection } = useSelection();
-
-  useSelectionTopicMap();
-
-  const onGazetteerSelection = (selection) => {
-    if (!selection) {
-      setSelection(null);
-      return;
-    }
-    const selectionMetaData = {
-      selectedFrom: "gazetteer",
-      selectionTimestamp: Date.now(),
-      isAreaSelection: isAreaType(selection.type),
-    };
-    setSelection(Object.assign({}, selection, selectionMetaData));
-
-    setTimeout(() => {
-      const gazId = selection.more?.mid;
-      setSelectedFeatureByPredicate(
-        (feature) => feature.properties.id === gazId
-      );
-    }, 100);
-  };
-
   return (
     <>
+      <div
+        className="controls-container"
+        style={{
+          position: "absolute",
+          top: "0px",
+          left: "0px",
+          bottom: "0px",
+          zIndex: 600,
+        }}
+      >
+        <ControlLayout ifStorybook={false}>
+          <Control position="topleft" order={10}>
+            <div className="flex flex-col">
+              <ControlButtonStyler
+                onClick={zoomInLeaflet}
+                className="!border-b-0 !rounded-b-none font-bold !z-[9999999]"
+                dataTestId="zoom-in-control"
+                title="Vergrößern"
+              >
+                <FontAwesomeIcon icon={faPlus} className="text-base" />
+              </ControlButtonStyler>
+              <ControlButtonStyler
+                onClick={zoomOutLeaflet}
+                className="!rounded-t-none !border-t-[1px]"
+                dataTestId="zoom-out-control"
+                title="Verkleinern"
+              >
+                <FontAwesomeIcon icon={faMinus} className="text-base" />
+              </ControlButtonStyler>
+            </div>
+          </Control>
+
+          <Control position="topleft" order={50}>
+            <ControlButtonStyler
+              title={
+                document.fullscreenElement
+                  ? "Vollbildmodus beenden"
+                  : "Vollbildmodus"
+              }
+              onClick={() => {
+                if (document.fullscreenElement) {
+                  document.exitFullscreen();
+                } else {
+                  document.documentElement.requestFullscreen();
+                }
+              }}
+              dataTestId="full-screen-control"
+            >
+              <FontAwesomeIcon
+                icon={document.fullscreenElement ? faCompress : faExpand}
+              />
+            </ControlButtonStyler>
+          </Control>
+          <Control position="topleft" order={60} title="Mein Standort">
+            <RoutedMapLocateControl
+              tourRefLabels={null}
+              disabled={false}
+              nativeTooltip={true}
+            />
+          </Control>
+          <Control position="bottomleft" order={10}>
+            <div data-test-id="fuzzy-search" className="h-full w-full pl-2">
+              <FuzzySearch searchTextPlaceholder={searchTextPlaceholder} />
+            </div>
+          </Control>
+        </ControlLayout>
+      </div>
       <TopicMapComponent
-        locatorControl={true}
+        locatorControl={false}
+        fullScreenControl={false}
+        zoomControls={false}
         modalMenu={<MyMenu />}
         applicationMenuTooltipString={<MenuTooltip />}
         gazetteerSearchComponent={EmptySearchComponent}
@@ -103,7 +137,7 @@ function Comp() {
         }
         secondaryInfo={<InfoPanel />}
       >
-        <ContactButton
+        {/* <ContactButton
           title="Rückfrage zu den Messwerten"
           action={() => {
             let link = document.createElement("a");
@@ -132,20 +166,12 @@ function Comp() {
             link.href = mailToHref;
             link.click();
           }}
-        />
+        /> */}
         <TopicMapSelectionContent />
 
         <FeatureCollection></FeatureCollection>
         {/* <LogSelection /> */}
       </TopicMapComponent>
-      <div className="custom-left-control">
-        <LibFuzzySearch
-          gazData={gazData}
-          onSelection={onGazetteerSelection}
-          placeholder={searchTextPlaceholder}
-          pixelwidth={pixelwidth}
-        />
-      </div>
     </>
   );
 }
