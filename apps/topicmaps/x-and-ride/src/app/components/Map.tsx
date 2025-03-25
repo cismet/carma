70f970ddfdb927383d54d69d38ce4ef43eb0b13f
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-03-25 12:57:26 +0100

    #276 add layout controls

--- apps/topicmaps/x-and-ride/src/app/components/Map.tsx
+++ apps/topicmaps/x-and-ride/src/app/components/Map.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect } from "react";
 import {
   FeatureCollectionContext,
   FeatureCollectionDispatchContext,
@@ -20,21 +20,24 @@ import {
   InfoBoxTextTitle,
   InfoBoxTextContent,
 } from "@carma-collab/wuppertal/x-and-ride";
+import { TopicMapSelectionContent } from "@carma-apps/portals";
+import { EmptySearchComponent } from "@carma-mapping/fuzzy-search";
 import {
-  SelectionMetaData,
-  TopicMapSelectionContent,
-  useGazData,
-  useSelection,
-  useSelectionTopicMap,
-} from "@carma-apps/portals";
+  Control,
+  ControlButtonStyler,
+  ControlLayout,
+} from "@carma-mapping/map-controls-layout";
+import { RoutedMapLocateControl } from "@carma-mapping/components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  EmptySearchComponent,
-  LibFuzzySearch,
-  SearchResultItem,
-} from "@carma-mapping/fuzzy-search";
-import { ENDPOINT, isAreaType } from "@carma-commons/resources";
-import { ResponsiveTopicMapContext } from "react-cismap/contexts/ResponsiveTopicMapContextProvider";
+  faCompress,
+  faExpand,
+  faMinus,
+  faPlus,
+} from "@fortawesome/free-solid-svg-icons";
 import FuzzySearch from "./FuzzySearch";
+import useLeafletZoomControls from "../../hooks/useLeafletZoomControls";
+
 const Map = () => {
   const { setClusteringOptions } = useContext<
     typeof FeatureCollectionDispatchContext
@@ -52,12 +55,7 @@ const Map = () => {
     setSecondaryInfoVisible,
   } = useContext<typeof UIDispatchContext>(UIDispatchContext);
 
-  const { responsiveState, gap, windowSize } = useContext<
-    typeof ResponsiveTopicMapContext
-  >(ResponsiveTopicMapContext);
-
-  const pixelwidth =
-    responsiveState === "normal" ? "300px" : windowSize.width - gap;
+  const { zoomInLeaflet, zoomOutLeaflet } = useLeafletZoomControls();
 
   useEffect(() => {
     if (markerSymbolSize) {
@@ -68,36 +66,80 @@ const Map = () => {
     }
   }, [markerSymbolSize]);
 
-  const { gazData } = useGazData();
-  const { setSelection } = useSelection();
-
-  useSelectionTopicMap();
-
-  const onGazetteerSelection = (selection: SearchResultItem | null) => {
-    if (!selection) {
-      setSelection(null);
-      return;
-    }
-    const selectionMetaData: SelectionMetaData = {
-      selectedFrom: "gazetteer",
-      selectionTimestamp: Date.now(),
-      isAreaSelection: isAreaType(selection.type as ENDPOINT),
-    };
-    setSelection(Object.assign({}, selection, selectionMetaData));
-
-    // setTimeout(() => {
-    //   const gazId = selection.more?.pid || selection.more?.kid;
-    //   setSelectedFeatureByPredicate(
-    //     (feature) => feature.properties.id === gazId
-    //   );
-    // }, 100);
-  };
-
   return (
     <>
+      <div
+        className="controls-container"
+        style={{
+          position: "absolute",
+          top: "0px",
+          left: "0px",
+          bottom: "0px",
+          zIndex: 600,
+        }}
+      >
+        <ControlLayout ifStorybook={false}>
+          <Control position="topleft" order={10}>
+            <div className="flex flex-col">
+              <ControlButtonStyler
+                onClick={zoomInLeaflet}
+                className="!border-b-0 !rounded-b-none font-bold !z-[9999999]"
+                dataTestId="zoom-in-control"
+                title="Vergrößern"
+              >
+                <FontAwesomeIcon icon={faPlus} className="text-base" />
+              </ControlButtonStyler>
+              <ControlButtonStyler
+                onClick={zoomOutLeaflet}
+                className="!rounded-t-none !border-t-[1px]"
+                dataTestId="zoom-out-control"
+                title="Verkleinern"
+              >
+                <FontAwesomeIcon icon={faMinus} className="text-base" />
+              </ControlButtonStyler>
+            </div>
+          </Control>
+
+          <Control position="topleft" order={50}>
+            <ControlButtonStyler
+              title={
+                document.fullscreenElement
+                  ? "Vollbildmodus beenden"
+                  : "Vollbildmodus"
+              }
+              onClick={() => {
+                if (document.fullscreenElement) {
+                  document.exitFullscreen();
+                } else {
+                  document.documentElement.requestFullscreen();
+                }
+              }}
+              dataTestId="full-screen-control"
+            >
+              <FontAwesomeIcon
+                icon={document.fullscreenElement ? faCompress : faExpand}
+              />
+            </ControlButtonStyler>
+          </Control>
+          <Control position="topleft" order={60} title="Mein Standort">
+            <RoutedMapLocateControl
+              tourRefLabels={null}
+              disabled={false}
+              nativeTooltip={true}
+            />
+          </Control>
+          <Control position="bottomleft" order={10}>
+            <div data-test-id="fuzzy-search" className="h-full w-full pl-2">
+              <FuzzySearch searchTextPlaceholder={searchTextPlaceholder} />
+            </div>
+          </Control>
+        </ControlLayout>
+      </div>
       <TopicMapComponent
         modalMenu={<Menu />}
-        locatorControl={true}
+        locatorControl={false}
+        fullScreenControl={false}
+        zoomControls={false}
         photoLightBox
         gazetteerSearchControl={true}
         gazetteerSearchComponent={EmptySearchComponent}
@@ -134,7 +176,6 @@ const Map = () => {
         <TopicMapSelectionContent />
         <FeatureCollection></FeatureCollection>
       </TopicMapComponent>
-      <FuzzySearch searchTextPlaceholder={searchTextPlaceholder} />
     </>
   );
 };
