473524c7a6378d32b911586e51d24f4e4c0bffce
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-03-26 15:28:31 +0100

    #276 style custom scale

--- apps/topicmaps/fnp-inspektor/src/app/components/CustomScaleControl.tsx
+++ apps/topicmaps/fnp-inspektor/src/app/components/CustomScaleControl.tsx
@@ -39,7 +39,11 @@ const CustomScaleControl = () => {
   useEffect(() => {
     console.log("xxx scale", scaleLabel);
   }, [scaleLabel]);
-  return <div className="text-white">{scaleLabel}</div>;
+  return (
+    <div className="bg-white bg-opacity-60 border-2 border-gray-600 px-2 py-0.5 text-xs w-24">
+      {scaleLabel}
+    </div>
+  );
 };
 
 export default CustomScaleControl;
