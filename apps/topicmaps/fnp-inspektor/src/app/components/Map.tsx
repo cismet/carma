f4156490e8497176a11a58582b46318efed9a99d
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-03-26 11:29:55 +0100

    #276 add layout controls

--- apps/topicmaps/fnp-inspektor/src/app/components/Map.tsx
+++ apps/topicmaps/fnp-inspektor/src/app/components/Map.tsx
@@ -47,10 +47,22 @@ import {
   loadingText,
 } from "@carma-collab/wuppertal/fnp-inspektor";
 import type { UnknownAction } from "redux";
-import { ResponsiveTopicMapContext } from "react-cismap/contexts/ResponsiveTopicMapContextProvider";
 import { TopicMapSelectionContent } from "@carma-apps/portals";
 import { EmptySearchComponent } from "@carma-mapping/fuzzy-search";
 import FuzzySearch from "./FuzzySearch";
+import {
+  Control,
+  ControlButtonStyler,
+  ControlLayout,
+} from "@carma-mapping/map-controls-layout";
+import { RoutedMapLocateControl } from "@carma-mapping/components";
+import {
+  faCompress,
+  faExpand,
+  faMinus,
+  faPlus,
+} from "@fortawesome/free-solid-svg-icons";
+import useLeafletZoomControls from "../../hooks/useLeafletZoomControls";
 
 const { ScaleControl } = TransitiveReactLeaflet;
 
@@ -71,6 +83,7 @@ const Map = () => {
   const wrapperRef = useRef<HTMLDivElement>(null);
   const [width, setWidth] = useState<number>(0);
   const { routedMapRef } = useContext<typeof TopicMapContext>(TopicMapContext);
+  const { zoomInLeaflet, zoomOutLeaflet } = useLeafletZoomControls();
 
   const setAevVisible = (visible) => {
     if (visible && !aevVisible) {
@@ -311,11 +324,82 @@ const Map = () => {
   return (
     <div style={{ position: "relative" }} ref={wrapperRef}>
       {title}
+      <div
+        className="controls-container"
+        style={{
+          position: "absolute",
+          top: "0px",
+          left: "0px",
+          bottom: "0px",
+          zIndex: 600,
+        }}
+      >
+        <ControlLayout ifStorybook={false}>
+          <Control position="topleft" order={10}>
+            <div className="flex flex-col">
+              <ControlButtonStyler
+                onClick={zoomInLeaflet}
+                className="!border-b-0 !rounded-b-none font-bold !z-[9999999]"
+                dataTestId="zoom-in-control"
+                title="Vergrößern"
+              >
+                <FontAwesomeIcon icon={faPlus} className="text-base" />
+              </ControlButtonStyler>
+              <ControlButtonStyler
+                onClick={zoomOutLeaflet}
+                className="!rounded-t-none !border-t-[1px]"
+                dataTestId="zoom-out-control"
+                title="Verkleinern"
+              >
+                <FontAwesomeIcon icon={faMinus} className="text-base" />
+              </ControlButtonStyler>
+            </div>
+          </Control>
+
+          <Control position="topleft" order={50}>
+            <ControlButtonStyler
+              title={
+                document.fullscreenElement
+                  ? "Vollbildmodus beenden"
+                  : "Vollbildmodus"
+              }
+              onClick={() => {
+                if (document.fullscreenElement) {
+                  document.exitFullscreen();
+                } else {
+                  document.documentElement.requestFullscreen();
+                }
+              }}
+              dataTestId="full-screen-control"
+            >
+              <FontAwesomeIcon
+                icon={document.fullscreenElement ? faCompress : faExpand}
+              />
+            </ControlButtonStyler>
+          </Control>
+          <Control position="topleft" order={60} title="Mein Standort">
+            <RoutedMapLocateControl
+              tourRefLabels={null}
+              disabled={false}
+              nativeTooltip={true}
+            />
+          </Control>
+          <Control position="bottomleft" order={10}>
+            <div data-test-id="fuzzy-search" className="h-full w-full pl-2">
+              <FuzzySearch
+                mode={mapMode.mode}
+                searchTextPlaceholder={searchTextPlaceholder}
+              />
+            </div>
+          </Control>
+        </ControlLayout>
+      </div>
       <TopicMapComponent
         initialLoadingText={loadingText}
-        fullScreenControl
         //   pendingLoader={isLoading ? 1 : 0}
-        locatorControl
+        locatorControl={false}
+        fullScreenControl={false}
+        zoomControls={false}
         gazetteerSearchControl={true}
         gazetteerSearchComponent={EmptySearchComponent}
         backgroundlayers={"wupp-plan-live"}
@@ -338,7 +422,7 @@ const Map = () => {
           metric={true}
           imperial={false}
           updateWhenIdle={false}
-          position="bottomleft"
+          position="topleft"
         />
 
         {(aevVisible || mapMode.mode === "arbeitskarte") && (
@@ -395,10 +479,6 @@ const Map = () => {
         )}
         {backgrounds}
       </TopicMapComponent>
-      <FuzzySearch
-        mode={mapMode.mode}
-        searchTextPlaceholder={searchTextPlaceholder}
-      />
     </div>
   );
 };
