1643396b6b8d8f42c1c43a35332d80a7cbfed017
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-03-26 13:11:32 +0100

    #276 add layout controls

--- apps/topicmaps/bplan-auskunft/src/app/components/Map.tsx
+++ apps/topicmaps/bplan-auskunft/src/app/components/Map.tsx
@@ -23,6 +23,20 @@ import { Layer } from "leaflet";
 import { TopicMapSelectionContent } from "@carma-apps/portals";
 import { EmptySearchComponent } from "@carma-mapping/fuzzy-search";
 import FuzzySearch from "./FuzzySearch";
+import {
+  Control,
+  ControlButtonStyler,
+  ControlLayout,
+} from "@carma-mapping/map-controls-layout";
+import { RoutedMapLocateControl } from "@carma-mapping/components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCompress,
+  faExpand,
+  faMinus,
+  faPlus,
+} from "@fortawesome/free-solid-svg-icons";
+import useLeafletZoomControls from "../../hooks/useLeafletZoomControls";
 
 const Map = () => {
   const dispatch = useDispatch();
@@ -35,6 +49,7 @@ const Map = () => {
   let refRoutedMap = useRef(null);
   const zoom = searchParams.get("zoom");
   const { routedMapRef } = useContext<typeof TopicMapContext>(TopicMapContext);
+  const { zoomInLeaflet, zoomOutLeaflet } = useLeafletZoomControls();
 
   interface MapFeature extends Layer {
     id: string;
@@ -156,11 +171,83 @@ const Map = () => {
 
   return (
     <>
+      {" "}
+      <div
+        className="controls-container"
+        style={{
+          position: "absolute",
+          top: "0px",
+          left: "0px",
+          bottom: "0px",
+          zIndex: 600,
+        }}
+      >
+        <ControlLayout ifStorybook={false}>
+          <Control position="topleft" order={10}>
+            <div className="flex flex-col">
+              <ControlButtonStyler
+                onClick={zoomInLeaflet}
+                className="!border-b-0 !rounded-b-none font-bold !z-[9999999]"
+                dataTestId="zoom-in-control"
+                title="Vergrößern"
+              >
+                <FontAwesomeIcon icon={faPlus} className="text-base" />
+              </ControlButtonStyler>
+              <ControlButtonStyler
+                onClick={zoomOutLeaflet}
+                className="!rounded-t-none !border-t-[1px]"
+                dataTestId="zoom-out-control"
+                title="Verkleinern"
+              >
+                <FontAwesomeIcon icon={faMinus} className="text-base" />
+              </ControlButtonStyler>
+            </div>
+          </Control>
+
+          <Control position="topleft" order={50}>
+            <ControlButtonStyler
+              title={
+                document.fullscreenElement
+                  ? "Vollbildmodus beenden"
+                  : "Vollbildmodus"
+              }
+              onClick={() => {
+                if (document.fullscreenElement) {
+                  document.exitFullscreen();
+                } else {
+                  document.documentElement.requestFullscreen();
+                }
+              }}
+              dataTestId="full-screen-control"
+            >
+              <FontAwesomeIcon
+                icon={document.fullscreenElement ? faCompress : faExpand}
+              />
+            </ControlButtonStyler>
+          </Control>
+          <Control position="topleft" order={60} title="Mein Standort">
+            <RoutedMapLocateControl
+              tourRefLabels={null}
+              disabled={false}
+              nativeTooltip={true}
+            />
+          </Control>
+          <Control position="bottomleft" order={10}>
+            <div data-test-id="fuzzy-search" className="h-full w-full pl-2">
+              <FuzzySearch
+                setFeatures={setFeatures}
+                setSelectedIndex={setSelectedIndex}
+              />
+            </div>
+          </Control>
+        </ControlLayout>
+      </div>
       <TopicMapComponent
         initialLoadingText="Laden der B-Plan-Daten"
-        fullScreenControl
         pendingLoader={isLoading ? 1 : 0}
-        locatorControl
+        locatorControl={false}
+        fullScreenControl={false}
+        zoomControls={false}
         ref={refRoutedMap}
         gazetteerSearchControl={true}
         gazetteerSearchComponent={EmptySearchComponent}
@@ -255,10 +342,6 @@ const Map = () => {
           featureClickHandler={featureClick}
         />
       </TopicMapComponent>
-      {/* <FuzzySearch
-        setFeatures={setFeatures}
-        setSelectedIndex={setSelectedIndex}
-      /> */}
     </>
   );
 };
