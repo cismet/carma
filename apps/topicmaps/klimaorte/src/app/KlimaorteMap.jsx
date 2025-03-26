6e9375b41ba7450bdc314a5207b5520904be3a3b
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-03-26 09:10:39 +0100

    #276 add layout controls

--- apps/topicmaps/klimaorte/src/app/KlimaorteMap.jsx
+++ apps/topicmaps/klimaorte/src/app/KlimaorteMap.jsx
@@ -20,7 +20,20 @@ import {
   TopicMapContext,
   TopicMapDispatchContext,
 } from "react-cismap/contexts/TopicMapContextProvider";
-import { ResponsiveTopicMapContext } from "react-cismap/contexts/ResponsiveTopicMapContextProvider";
+import {
+  Control,
+  ControlButtonStyler,
+  ControlLayout,
+} from "@carma-mapping/map-controls-layout";
+import useLeafletZoomControls from "../hooks/useLeafletZoomControls";
+import { RoutedMapLocateControl } from "@carma-mapping/components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCompress,
+  faExpand,
+  faMinus,
+  faPlus,
+} from "@fortawesome/free-solid-svg-icons";
 
 import { removeQueryPart } from "react-cismap/tools/routingHelper";
 import { LightBoxDispatchContext } from "react-cismap/contexts/LightBoxContextProvider";
@@ -50,6 +63,7 @@ function KlimaorteMap() {
   } = useContext(FeatureCollectionContext);
   const { zoomToFeature, setAppMode } = useContext(TopicMapDispatchContext);
   const { history, appMode } = useContext(TopicMapContext);
+  const { zoomInLeaflet, zoomOutLeaflet } = useLeafletZoomControls();
 
   useEffect(() => {
     if (appMode === undefined) {
@@ -219,46 +233,115 @@ function KlimaorteMap() {
   }
 
   return (
-    <div>
-      <ModeSwitcher
-        mode={appMode}
-        setMode={(mode) => {
-          window.location.href = getModeUrl(mode);
-          // setModeState(mode);
-          setAppMode(mode);
+    <>
+      <div
+        className="controls-container"
+        style={{
+          position: "absolute",
+          top: "0px",
+          left: "0px",
+          bottom: "0px",
+          zIndex: 600,
         }}
-      />
-      <TopicMapComponent
-        maxZoom={19}
-        minZoom={8}
-        applicationMenuTooltipString={<MenuTooltip appMode={appMode} />}
-        locatorControl={true}
-        modalMenu={<MyMenu mode={appMode} />}
-        gazetteerSearchControl={true}
-        gazetteerSearchComponent={EmptySearchComponent}
-        infoBox={
-          <InfoBox
-            key={JSON.stringify(selectedFeature)}
-            appMode={appMode}
-            pixelwidth={400}
-            secondaryInfoBoxElements={secondaryInfoBoxElements}
-            moreDataAvailable={moreDataAvailable}
-            selectedFeature={selectedFeature}
-            secondarySelection={secondarySelection}
-          />
-        }
-        secondaryInfo={<InfoPanel />}
       >
-        <TopicMapSelectionContent />
-        <FeatureCollection
-          key={"featureCollection" + appMode}
-          clusteringOptions={{
-            iconCreateFunction,
+        <ControlLayout ifStorybook={false}>
+          <Control position="topleft" order={10}>
+            <div className="flex flex-col">
+              <ControlButtonStyler
+                onClick={zoomInLeaflet}
+                className="!border-b-0 !rounded-b-none font-bold !z-[9999999]"
+                dataTestId="zoom-in-control"
+                title="Vergrößern"
+              >
+                <FontAwesomeIcon icon={faPlus} className="text-base" />
+              </ControlButtonStyler>
+              <ControlButtonStyler
+                onClick={zoomOutLeaflet}
+                className="!rounded-t-none !border-t-[1px]"
+                dataTestId="zoom-out-control"
+                title="Verkleinern"
+              >
+                <FontAwesomeIcon icon={faMinus} className="text-base" />
+              </ControlButtonStyler>
+            </div>
+          </Control>
+          <Control position="topleft" order={50}>
+            <ControlButtonStyler
+              title={
+                document.fullscreenElement
+                  ? "Vollbildmodus beenden"
+                  : "Vollbildmodus"
+              }
+              onClick={() => {
+                if (document.fullscreenElement) {
+                  document.exitFullscreen();
+                } else {
+                  document.documentElement.requestFullscreen();
+                }
+              }}
+              dataTestId="full-screen-control"
+            >
+              <FontAwesomeIcon
+                icon={document.fullscreenElement ? faCompress : faExpand}
+              />
+            </ControlButtonStyler>
+          </Control>
+          <Control position="topleft" order={60} title="Mein Standort">
+            <RoutedMapLocateControl
+              tourRefLabels={null}
+              disabled={false}
+              nativeTooltip={true}
+            />
+          </Control>
+          <Control position="bottomleft" order={10}>
+            <div data-test-id="fuzzy-search" className="h-full w-full pl-2">
+              <FuzzySearch searchTextPlaceholder={searchTextPlaceholder} />
+            </div>
+          </Control>
+        </ControlLayout>
+      </div>
+      <div>
+        <ModeSwitcher
+          mode={appMode}
+          setMode={(mode) => {
+            window.location.href = getModeUrl(mode);
+            // setModeState(mode);
+            setAppMode(mode);
           }}
         />
-      </TopicMapComponent>
-      <FuzzySearch searchTextPlaceholder={searchTextPlaceholder} />
-    </div>
+        <TopicMapComponent
+          maxZoom={19}
+          minZoom={8}
+          applicationMenuTooltipString={<MenuTooltip appMode={appMode} />}
+          locatorControl={false}
+          fullScreenControl={false}
+          zoomControls={false}
+          modalMenu={<MyMenu mode={appMode} />}
+          gazetteerSearchControl={true}
+          gazetteerSearchComponent={EmptySearchComponent}
+          infoBox={
+            <InfoBox
+              key={JSON.stringify(selectedFeature)}
+              appMode={appMode}
+              pixelwidth={400}
+              secondaryInfoBoxElements={secondaryInfoBoxElements}
+              moreDataAvailable={moreDataAvailable}
+              selectedFeature={selectedFeature}
+              secondarySelection={secondarySelection}
+            />
+          }
+          secondaryInfo={<InfoPanel />}
+        >
+          <TopicMapSelectionContent />
+          <FeatureCollection
+            key={"featureCollection" + appMode}
+            clusteringOptions={{
+              iconCreateFunction,
+            }}
+          />
+        </TopicMapComponent>
+      </div>
+    </>
   );
 }
 
