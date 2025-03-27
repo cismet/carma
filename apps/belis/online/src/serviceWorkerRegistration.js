904940ca3a80fde7987dc0227d47461f4658f835
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-03-27 15:02:47 +0100

    #278 remove base url from config

--- apps/belis/online/src/serviceWorkerRegistration.js
+++ apps/belis/online/src/serviceWorkerRegistration.js
@@ -46,7 +46,6 @@ export function register(config) {
 
       const swUrl = `${baseUrl}service-worker.js`;
       // const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
-      console.log("xxx load swUrl", swUrl);
 
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
--- apps/belis/online/vite.config.mts
+++ apps/belis/online/vite.config.mts
@@ -39,7 +39,7 @@ export default defineConfig({
   //   }),
   // ],
 
-  base: './',
+  // base: './',
 
   // Uncomment this if you are using workers.
   // worker: {
