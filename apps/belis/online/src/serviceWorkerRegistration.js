ba112d31f416ec7b78fc51d77fc7f3dc7b2635d9
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-03-28 08:53:53 +0100

    #278 clean code

--- apps/belis/online/src/serviceWorkerRegistration.js
+++ apps/belis/online/src/serviceWorkerRegistration.js
@@ -21,31 +21,19 @@ const isLocalhost = Boolean(
 );
 
 export function register(config) {
-  console.log("xxx register env", process.env.NODE_ENV);
-
-  const baseUrl = window.location.origin + window.location.pathname;
-
-  // old check
-  //(process.env.NODE_ENV === "production" && "serviceWorker" in navigator)
-
-  if (baseUrl && "serviceWorker" in navigator) {
+  if (process.env.NODE_ENV === "production" && "serviceWorker" in navigator) {
     // The URL constructor is available in all browsers that support SW.
-    // const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
-    console.log("xxx baseUrl", baseUrl);
+    const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
 
-    // if (baseUrl !== window.location.origin) {
-    // Our service worker won't work if PUBLIC_URL is on a different origin
-    // from what our page is served on. This might happen if a CDN is used to
-    // serve assets; see https://github.com/facebook/create-react-app/issues/2374
-    // return;
-    // }
+    if (publicUrl.origin !== window.location.origin) {
+      // Our service worker won't work if PUBLIC_URL is on a different origin
+      // from what our page is served on. This might happen if a CDN is used to
+      // serve assets; see https://github.com/facebook/create-react-app/issues/2374
+      return;
+    }
 
     window.addEventListener("load", () => {
-      // const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
-      // const baseUrl = window.location.origin + window.location.pathname;
-
-      const swUrl = `${baseUrl}service-worker.js`;
-      // const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
 
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
