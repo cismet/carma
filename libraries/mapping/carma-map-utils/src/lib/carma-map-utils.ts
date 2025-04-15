c69cd130fd890410c2010ad39ba3d9e03c7f5869
Thorsten Hell <thorsten.hell@cismet.de>
2025-04-15 19:04:01 +0200

    add getBoundingBoxForLeafletMap

--- libraries/mapping/carma-map-utils/src/lib/carma-map-utils.ts
+++ libraries/mapping/carma-map-utils/src/lib/carma-map-utils.ts
@@ -1,3 +1,27 @@
 export function carmaMapUtils(): string {
   return "carma-map-utils";
 }
+import proj4 from "proj4";
+
+export function getBoundingBoxForLeafletMap(
+  leafletMap,
+  referenceSystemDefinition
+) {
+  const bounds = leafletMap.leafletElement.getBounds();
+  const projectedNE = proj4(
+    "EPSG:4326",
+    referenceSystemDefinition,
+    [bounds._northEast.lng, bounds._northEast.lat]
+  );
+  const projectedSW = proj4(
+    "EPSG:4326",
+    referenceSystemDefinition,
+    [bounds._southWest.lng, bounds._southWest.lat]
+  );
+  return {
+    left: projectedSW[0],
+    top: projectedNE[1],
+    right: projectedNE[0],
+    bottom: projectedSW[1],
+  };
+}
