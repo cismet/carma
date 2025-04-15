8c523d6d962649d87add55d25fd889825dfbcebf
Thorsten Hell <thorsten.hell@cismet.de>
2025-04-15 19:23:05 +0200

    add tooltip

--- apps/topicmaps/bplan-auskunft/src/app/components/Map.tsx
+++ apps/topicmaps/bplan-auskunft/src/app/components/Map.tsx
@@ -189,7 +189,11 @@ const Map = () => {
             />
           </Control>
           <Control position="bottomleft" order={10}>
-            <div data-test-id="fuzzy-search" className="h-full w-full pl-2">
+            <div
+              data-test-id="fuzzy-search"
+              title="B-Pläne suchen"
+              className="h-full w-full pl-2"
+            >
               <FuzzySearchWrapper
                 setFeatures={setFeatures}
                 setSelectedIndex={setSelectedIndex}
