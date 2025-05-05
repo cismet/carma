771f63c2dec3c814c4e776692ed24c1ed2dc5287
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-05-05 08:32:39 +0200

    #287 fix errors after editing

--- apps/verdis/online/src/app/components/EditModeControlButton.tsx
+++ apps/verdis/online/src/app/components/EditModeControlButton.tsx
@@ -4,7 +4,7 @@ import "leaflet-editable";
 
 interface EditModeControlButtonProps {
   mapRef: React.RefObject<any>;
-  featuresInEditmode: boolean;
+  featuresInEditMode: boolean;
   onFeatureChange: (editing: boolean) => void;
   position?: L.ControlPosition;
   html?: string;
@@ -14,7 +14,7 @@ interface EditModeControlButtonProps {
 
 const EditModeControlButton: React.FC<EditModeControlButtonProps> = ({
   mapRef,
-  featuresInEditmode,
+  featuresInEditMode,
   onFeatureChange,
   position = "topleft",
   html = '<i class="fas fa-edit"></i>',
@@ -26,10 +26,6 @@ const EditModeControlButton: React.FC<EditModeControlButtonProps> = ({
 
     if (!map) return;
 
-    // if (!map.editTools) {
-    //   map.editTools = new L.Editable(map);
-    // }
-
     const ControlClass = L.Control.extend({
       options: { position, kind, html },
       onAdd() {
@@ -37,14 +33,14 @@ const EditModeControlButton: React.FC<EditModeControlButtonProps> = ({
         const link = L.DomUtil.create("a", "", div);
         link.href = "#";
         link.title = "Verändern der selektierten Anmerkung";
-        link.innerHTML = featuresInEditmode
+        link.innerHTML = featuresInEditMode
           ? `<span style="padding:2px 4px; border-radius:4px; border:3px solid #008AFA;">
                ${html}
              </span>`
           : html;
 
         L.DomEvent.on(link, "click", L.DomEvent.stop).on(link, "click", () =>
-          onFeatureChange(!featuresInEditmode)
+          onFeatureChange(!featuresInEditMode)
         );
         L.DomEvent.disableClickPropagation(div);
         return div;
@@ -67,7 +63,7 @@ const EditModeControlButton: React.FC<EditModeControlButtonProps> = ({
       }
     });
 
-    if (!featuresInEditmode) {
+    if (!featuresInEditMode) {
       map.eachLayer((layer) => {
         if (
           layer.feature?.properties?.type === "annotation" &&
@@ -97,7 +93,7 @@ const EditModeControlButton: React.FC<EditModeControlButtonProps> = ({
       map.off("moveend", reapplyEdit);
       map.off("zoomend", reapplyEdit);
     };
-  }, [mapRef, featuresInEditmode, onFeatureChange, selectedFeatureId]);
+  }, [mapRef, featuresInEditMode, selectedFeatureId]);
 
   return null;
 };
