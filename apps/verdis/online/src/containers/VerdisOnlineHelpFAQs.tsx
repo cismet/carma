0d7c39b762eb33a049f6d06ec1109665acdbac2e
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-05-06 13:29:24 +0200

    #287 helper faq page

--- apps/verdis/online/src/containers/VerdisOnlineHelpFAQs.tsx
+++ apps/verdis/online/src/containers/VerdisOnlineHelpFAQs.tsx
@@ -1,17 +1,34 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-nocheck
-import FAQs from "./../app/components/helpandsettings/Menu50FAQ";
+import {
+  Menu10Datengrundlagen,
+  Menu20MeinKassenzeichen,
+  Menu30KartenhintergruendeText,
+  Menu40Anleitung,
+  Menu41Mailservice,
+  Menu42Aenderungen,
+  Menu50FAQ,
+  Menu60Datenschutz,
+} from "@carma-collab/wuppertal/verdis-online";
+import Section from "react-cismap/topicmaps/menu/Section";
 
 const VerdisOnlineHelp = () => {
   return (
     <div style={{ margin: 25 }}>
-      <FAQs
-        key="FAQs"
-        // applicationMenuActiveKey={applicationMenuActiveKey}
-        setApplicationMenuActiveKey={() => {}}
-        showModalMenu={() => {}}
-        showOnSeperatePage={true}
+      <h3>Kompaktanleitung und Hintergrundinformationen</h3>
+      <hr />
+      <Menu10Datengrundlagen />
+      <Menu20MeinKassenzeichen />
+      <Section
+        key="kartenhintergruende"
+        sectionKey="kartenhintergruende"
+        sectionTitle="Hintergrundkarten"
+        sectionBsStyle="info"
+        sectionContent={<Menu30KartenhintergruendeText />}
       />
+      <Menu40Anleitung />
+      <Menu41Mailservice />
+      <Menu42Aenderungen />
+      <Menu50FAQ />
+      <Menu60Datenschutz />
     </div>
   );
 };
