a44f2b928b21a9fd2590905a2076fa71fe11a072
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-05-06 11:19:30 +0200

    #287 add new help route

--- apps/verdis/online/src/app/components/helpandsettings/Menu99Footer.tsx
+++ apps/verdis/online/src/app/components/helpandsettings/Menu99Footer.tsx
@@ -2,13 +2,21 @@ import { Button } from "react-bootstrap";
 import { KompaktanleitungFooter } from "@carma-collab/wuppertal/verdis-online";
 import { getApplicationVersion } from "@carma-commons/utils";
 import versionData from "../../../version.json";
+import { Link } from "react-router-dom";
 
 const Menu99Footer = () => {
   const version = getApplicationVersion(versionData);
   return (
     <div style={{ display: "flex" }}>
       <KompaktanleitungFooter version={version} />
-      <Button size="sm" variant="info">
+      <Button
+        as={Link}
+        to="/verdisOnlineHilfeFAQs"
+        target="_blank"
+        rel="noopener noreferrer"
+        size="sm"
+        variant="info"
+      >
         In eigenem Fenster öffnen
       </Button>
     </div>
