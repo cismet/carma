a4b0a7f6c7e46fa595c1d95644c43d1380c32d81
Friedrich Hartmann <690920+mxfh@users.noreply.github.com>
2025-05-09 13:51:10 +0200

    OLB describe all data for Cameras in Exterior Orientation

--- apps/geoportal/src/app/oblique/config.ts
+++ apps/geoportal/src/app/oblique/config.ts
@@ -61,40 +61,87 @@ export const CAMERA_ID_TO_UP_VECTOR_MATRIX_MAPPING = Object.freeze({
   "176": { rowIndex: 1, negate: true }, // left
 });
 
-const INTERIOR_ORIENTATIONS = Object.freeze({
-  "170": {
-    x: 7102.5638,
-    y: 5313,
-    width: 14204,
-    height: 10652,
-    focalLength: 108.644,
-    z: 265.9574468085,
-  },
-  "171": {
-    x: 5347.5745,
-    y: 7078.0957,
-    width: 10652,
-    height: 14204,
-    focalLength: 108.723,
-    z: 265.9574468085,
-  },
-  "174": {
-    x: 7120.6489,
-    y: 5336.9362,
-    width: 14204,
-    height: 10652,
-    focalLength: 108.632,
-    z: 265.9574468085,
-  },
-  "176": {
-    x: 5351.5638,
-    y: 7099.9043,
-    width: 10652,
-    height: 14204,
-    focalLength: 108.74,
-    z: 265.9574468085,
-  },
-});
+type INTERIOR_ORIENTATION = {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+  focalLength: number; // nominally 110mm
+  ppm: number; // pixels per mm ; equals 1/ Pixel size 3.76µm
+  rollCorrection?: number; // looks like front and rear need some extra roll correction not present in the calibration data
+  label?: string; // label for the camera
+  CCD_INTERIOR_ORIENTATION: [
+    [number, number, number],
+    [number, number, number]
+  ]; // CCD interior orientation matrix as is from CAMERA_DEFINTION
+};
+
+const INTERIOR_ORIENTATIONS: Record<string, INTERIOR_ORIENTATION> =
+  Object.freeze({
+    // iXM-RS150F Camera or older
+    // parameters manually extracted from the camera calibration data in the prj file
+    // https://www.phaseone.com/wp-content/uploads/2024/01/iXM-RS150F_Fact-Sheet_Display_EN_2023.pdf
+    "170": {
+      label: "front",
+      model: "iXM-RS150F Camera",
+      x: 7102.5638,
+      y: 5313,
+      width: 14204,
+      height: 10652,
+      focalLength: 108.644,
+      ppm: 265.9574468085,
+      rollCorrection: 0,
+      CCD_INTERIOR_ORIENTATION: [
+        [0, -265.9574468085, 7102.5638],
+        [-265.9574468085, 0, 5313],
+      ],
+    },
+    "171": {
+      label: "right",
+      model: "iXM-RS150F Camera",
+      x: 5347.5745,
+      y: 7078.0957,
+      width: 10652,
+      height: 14204,
+      focalLength: 108.723,
+      ppm: 265.9574468085,
+      rollCorrection: 0,
+      CCD_INTERIOR_ORIENTATION: [
+        [-265.9574468085, 0, 5347.5745],
+        [0, 265.9574468085, 7078.0957],
+      ],
+    },
+    "174": {
+      label: "back",
+      model: "iXM-RS150F Camera",
+      x: 7120.6489,
+      y: 5336.9362,
+      width: 14204,
+      height: 10652,
+      focalLength: 108.632,
+      ppm: 265.9574468085,
+      rollCorrection: 0,
+      CCD_INTERIOR_ORIENTATION: [
+        [0, 265.9574468085, 7120.6489],
+        [265.9574468085, 0, 5336.9362],
+      ],
+    },
+    "176": {
+      label: "left",
+      model: "iXM-RS150F Camera",
+      x: 5351.5638,
+      y: 7099.9043,
+      width: 10652,
+      height: 14204,
+      focalLength: 108.74,
+      ppm: 265.9574468085,
+      rollCorrection: 0,
+      CCD_INTERIOR_ORIENTATION: [
+        [265.9574468085, 0, 5351.5638],
+        [0, -265.9574468085, 7099.9043],
+      ],
+    },
+  });
 
 const getOffsetFromIntOri = ({
   x,
