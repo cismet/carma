a25b562234e429a778abc47355a22c7d42b225fe
Friedrich Hartmann <690920+mxfh@users.noreply.github.com>
2025-05-13 21:03:01 +0200

    minor

--- apps/geoportal/src/app/oblique/hooks/useOblique.ts
+++ apps/geoportal/src/app/oblique/hooks/useOblique.ts
@@ -5,7 +5,9 @@ import { ObliqueContext } from "../components/ObliqueProvider";
 export const useOblique = () => {
   const context = useContext(ObliqueContext);
   if (!context) {
-    throw new Error("useOblique must be used within an ObliqueContextProvider");
+    throw new Error(
+      "useOblique must be used within an ObliqueProvider to access the context"
+    );
   }
   return context;
 };
