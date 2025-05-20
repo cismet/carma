a9f6dfe33c091dbae5b3d06e1d16f0b027206b65
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-05-20 14:59:10 +0200

    #287 add playground style

--- apps/verdis/online/src/main.tsx
+++ apps/verdis/online/src/main.tsx
@@ -13,10 +13,41 @@ import VerdisOnlineLanding from "./app/components/VerdisOnlineLanding";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Layout from "./app/components/Layout";
 import VerdisOnlineHelp from "./containers/VerdisOnlineHelpFAQs";
+import { PLAYGROUND } from "./constants/cids";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
+let appOverlayStyle;
+
+if (PLAYGROUND === true) {
+  appOverlayStyle = {
+    position: "absolute",
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
+    border: "6px solid #46EE57",
+    pointerEvents: "none", // This ensures the overlay doesn't interfere with any interactions
+    boxSizing: "border-box",
+    zIndex: 999999, // High z-index to ensure it's on top
+  };
+} else if (PLAYGROUND === "unconfigured") {
+  appOverlayStyle = {
+    position: "absolute",
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
+    border: "6px solid #F62143",
+    pointerEvents: "none", // This ensures the overlay doesn't interfere with any interactions
+    boxSizing: "border-box",
+    zIndex: 999999, // High z-index to ensure it's on top
+  };
+} else {
+  appOverlayStyle = {};
+}
 
 const persistor = persistStore(store);
 
@@ -50,6 +81,7 @@ root.render(
           <Layout>
             <RouterProvider router={router} />
           </Layout>
+          <div style={appOverlayStyle} />
         </PersistGate>
       </TopicMapContextProvider>
     </Provider>
