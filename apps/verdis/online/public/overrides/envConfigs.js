98e8677a360b59012c06ae3c0a7e13ce035b8dce
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-05-20 14:49:24 +0200

    #287 add public config

---
+++ apps/verdis/online/public/overrides/envConfigs.js
@@ -0,0 +1,9 @@
+//Playground
+window.VERDIS_ONLINE_ENV_CONFIG = {
+  DOMAIN: "VERDIS_GRUNDIS",
+  GEOM_CLASS: "GEOM",
+  GEOM_FIELD: "geo_field",
+  SERVICE: "https://verdis-cloud.cismet.de/verdis/api/",
+  STAC_SERVICE: "https://verdis-cloud.cismet.de/verdis/api/",
+  PLAYGROUND: true,
+};
