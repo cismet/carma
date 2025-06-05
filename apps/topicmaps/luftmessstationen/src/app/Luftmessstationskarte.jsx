be5b22d1558c2b7d3ee50e810f67a9607cb9a380
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-06-05 10:23:00 +0200

    #323 improve mobile monitoring stations

--- apps/topicmaps/luftmessstationen/src/app/Luftmessstationskarte.jsx
+++ apps/topicmaps/luftmessstationen/src/app/Luftmessstationskarte.jsx
@@ -1,24 +1,23 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "leaflet/dist/leaflet.css";
 import { useEffect, useContext } from "react";
-
 import "react-bootstrap-typeahead/css/Typeahead.css";
 import FeatureCollection from "react-cismap/FeatureCollection";
 import "react-cismap/topicMaps.css";
-import GenericInfoBoxFromFeature from "react-cismap/topicmaps/GenericInfoBoxFromFeature";
 import TopicMapComponent from "react-cismap/topicmaps/TopicMapComponent";
 import "./App.css";
 import { FeatureCollectionContext } from "react-cismap/contexts/FeatureCollectionContextProvider";
 import { ResponsiveTopicMapContext } from "react-cismap/contexts/ResponsiveTopicMapContextProvider";
-
 import MyMenu from "./Menu";
-
 import {
   searchTextPlaceholder,
   MenuTooltip,
   InfoBoxTextContent,
 } from "@carma-collab/wuppertal/luftmessstationen";
-import { TopicMapSelectionContent } from "@carma-apps/portals";
+import {
+  GenericInfoBoxFromFeature,
+  TopicMapSelectionContent,
+} from "@carma-apps/portals";
 import { EmptySearchComponent } from "@carma-mapping/fuzzy-search";
 import FuzzySearchWrapper from "./components/FuzzySearchWrapper";
 import {
@@ -36,6 +35,7 @@ import {
 import { getApplicationVersion } from "@carma-commons/utils";
 import versionData from "../version.json";
 import { version as reactCismapVersion } from "react-cismap/meta";
+import { TAILWIND_CLASSNAMES_FULLSCREEN_FIXED } from "@carma-commons/utils";
 
 import SIMComponentDictionary from "@carma-collab/wuppertal/secondary-info-modals";
 const LuftmessstationenSIM = SIMComponentDictionary["luftmessstationenSIM"];
@@ -85,82 +85,69 @@ function Comp() {
   }, []);
 
   return (
-    <>
-      <div
-        className="controls-container"
-        style={{
-          position: "absolute",
-          top: "0px",
-          left: "0px",
-          bottom: "0px",
-          zIndex: 600,
-        }}
-      >
-        <ControlLayout ifStorybook={false}>
-          <Control position="topleft" order={10}>
-            <ZoomControl />
-          </Control>
+    <div className={TAILWIND_CLASSNAMES_FULLSCREEN_FIXED}>
+      <ControlLayout ifStorybook={false}>
+        <Control position="topleft" order={10}>
+          <ZoomControl />
+        </Control>
 
-          <Control position="topleft" order={50}>
-            <FullscreenControl />
-          </Control>
-          <Control position="topleft" order={60} title="Mein Standort">
-            <RoutedMapLocateControl
-              tourRefLabels={null}
-              disabled={false}
-              nativeTooltip={true}
-            />
-          </Control>
-          <Control position="topleft" order={70}>
-            <ControlButtonStyler
-              onClick={contactButtonHandler}
-              title="Rückfrage zu den Messwerten"
-            >
-              <FontAwesomeIcon icon={faComment} className="text-base" />
-            </ControlButtonStyler>
-          </Control>
-          <Control position="bottomleft" order={10}>
-            <div data-test-id="fuzzy-search" className="h-full w-full pl-2">
-              <FuzzySearchWrapper
-                searchTextPlaceholder={searchTextPlaceholder}
-              />
-            </div>
-          </Control>
-        </ControlLayout>
-      </div>
-      <TopicMapComponent
-        locatorControl={false}
-        fullScreenControl={false}
-        zoomControls={false}
-        modalMenu={<MyMenu />}
-        applicationMenuTooltipString={<MenuTooltip />}
-        gazetteerSearchComponent={EmptySearchComponent}
-        infoBox={
-          <GenericInfoBoxFromFeature
-            pixelwidth={350}
-            config={{
-              displaySecondaryInfoAction: true,
-
-              city: "Wuppertal",
-              navigator: {
-                noun: {
-                  singular: "Messstation",
-                  plural: "Messstationen",
-                },
-              },
-              noCurrentFeatureTitle: "Keine Messtationen gefunden",
-              noCurrentFeatureContent: <InfoBoxTextContent />,
-            }}
+        <Control position="topleft" order={50}>
+          <FullscreenControl />
+        </Control>
+        <Control position="topleft" order={60} title="Mein Standort">
+          <RoutedMapLocateControl
+            tourRefLabels={null}
+            disabled={false}
+            nativeTooltip={true}
           />
-        }
-        secondaryInfo={<InfoPanel />}
-      >
-        <TopicMapSelectionContent />
+        </Control>
+        <Control position="topleft" order={70}>
+          <ControlButtonStyler
+            onClick={contactButtonHandler}
+            title="Rückfrage zu den Messwerten"
+          >
+            <FontAwesomeIcon icon={faComment} className="text-base" />
+          </ControlButtonStyler>
+        </Control>
+        <Control position="bottomleft" order={10}>
+          <div data-test-id="fuzzy-search" style={{ marginTop: "4px" }}>
+            <FuzzySearchWrapper searchTextPlaceholder={searchTextPlaceholder} />
+          </div>
+        </Control>
+        <TopicMapComponent
+          locatorControl={false}
+          fullScreenControl={false}
+          zoomControls={false}
+          modalMenu={<MyMenu />}
+          applicationMenuTooltipString={<MenuTooltip />}
+          gazetteerSearchComponent={EmptySearchComponent}
+          infoBox={
+            <GenericInfoBoxFromFeature
+              pixelwidth={350}
+              config={{
+                displaySecondaryInfoAction: true,
 
-        <FeatureCollection></FeatureCollection>
-        {/* <LogSelection /> */}
-      </TopicMapComponent>
-    </>
+                city: "Wuppertal",
+                navigator: {
+                  noun: {
+                    singular: "Messstation",
+                    plural: "Messstationen",
+                  },
+                },
+                noCurrentFeatureTitle: "Keine Messtationen gefunden",
+                noCurrentFeatureContent: <InfoBoxTextContent />,
+              }}
+            />
+          }
+          secondaryInfo={<InfoPanel />}
+        >
+          <TopicMapSelectionContent />
+
+          <FeatureCollection></FeatureCollection>
+          {/* <LogSelection /> */}
+        </TopicMapComponent>
+      </ControlLayout>
+    </div>
   );
 }
 
