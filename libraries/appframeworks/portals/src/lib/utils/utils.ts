aab299c2a29dd2d288d4cc8f10cf3f5742575d88
David Glogaza <david.glogaza40@googlemail.com>
2025-06-16 11:33:29 +0200

    add topicmap

--- libraries/appframeworks/portals/src/lib/utils/utils.ts
+++ libraries/appframeworks/portals/src/lib/utils/utils.ts
@@ -8,6 +8,26 @@ import { sandboxedEvalExternal } from "../components/SandboxedEvalProvider";
 import { LeafletMap } from "@carma-mapping/engines/leaflet";
 import localforage from "localforage";
 
+export const getInternetExplorerVersion = () => {
+  var rV = -1; // Return value assumes failure.
+
+  if (
+    navigator.appName === "Microsoft Internet Explorer" ||
+    navigator.appName === "Netscape"
+  ) {
+    var uA = navigator.userAgent;
+    var rE = new RegExp("MSIE ([0-9]{1,}[.0-9]{0,})");
+
+    if (rE.exec(uA) !== null) {
+      rV = parseFloat(RegExp.$1);
+    } else if (!!navigator.userAgent.match(/Trident.*rv:11\./)) {
+      /*check for IE 11*/
+      rV = 11;
+    }
+  }
+  return rV;
+};
+
 export const setFromLocalforage = async (
   lfKey,
   setter,
