b1813a31d7d06354ada67b74befeaad2ccc55987
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-06-25 10:13:59 +0200

    #324 improve filter function

--- apps/topicmaps/vorhabenkarte/src/helper/filter.ts
+++ apps/topicmaps/vorhabenkarte/src/helper/filter.ts
@@ -1,36 +1,17 @@
-export interface Project {
-  id: number;
-  titel: string;
-  abgeschlossen: boolean;
-  abgeschlossen_am?: string;
-  veroeffentlicht: boolean;
-}
-
-const filterProjectsToShow = (projects: Project[]): Project[] => {
-  const sixMonthsAgo = new Date();
-  sixMonthsAgo.setMonth(sixMonthsAgo.getMonth() - 6);
-
-  return projects.filter((p) => {
-    if (!p.veroeffentlicht) return false;
-
-    if (!p.abgeschlossen) return true;
-
-    if (!p.abgeschlossen_am) return false;
-    const doneDate = new Date(p.abgeschlossen_am);
-    return doneDate >= sixMonthsAgo;
-  });
-};
-
 const itemFilterFunction = ({ filterState }) => {
   return (item) => {
     let result = false;
 
-    if (item.veroeffentlicht) {
-      result = true;
-    }
+    const sixMonthsAgo = new Date();
+    sixMonthsAgo.setMonth(sixMonthsAgo.getMonth() - 6);
 
-    if (!item.abgeschlossen) {
+    if (!item.veroeffentlicht) {
+      result = false;
+    } else if (!item.abgeschlossen) {
       result = true;
+    } else if (item.abgeschlossen_am) {
+      const doneDate = new Date(item.abgeschlossen_am);
+      result = doneDate >= sixMonthsAgo;
     }
 
     return result;
