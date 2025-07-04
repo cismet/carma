63220d6e44243629237aca5b47076796b73555b6
David Glogaza <david.glogaza40@googlemail.com>
2025-07-04 10:57:10 +0200

    change file upload styling

--- libraries/appframeworks/portals/src/lib/components/FileUploader.tsx
+++ libraries/appframeworks/portals/src/lib/components/FileUploader.tsx
@@ -1,5 +1,6 @@
-import { faX } from "@fortawesome/free-solid-svg-icons";
+import { faUpload, faX } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Button } from "antd";
 import { useRef } from "react";
 
 interface FileUploaderProps {
@@ -23,25 +24,44 @@ const FileUploader = ({ file, setFile }: FileUploaderProps) => {
         type="file"
         onChange={handleFileChange}
         accept="image/*"
-        style={{ display: file ? "none" : "block" }}
+        className="sr-only"
       />
-      {file && (
+      {file ? (
         <div className="relative overflow-hidden bg-white isolate rounded-md flex justify-center items-center w-full aspect-[1.7777/1]">
-          <FontAwesomeIcon
-            className="absolute right-1 top-1 cursor-pointer z-50"
+          <Button
             onClick={() => {
               setFile(null);
               if (inputRef.current) {
                 inputRef.current.value = "";
               }
             }}
-            icon={faX}
-          />
+            className="absolute right-1 top-1 cursor-pointer z-50"
+            type="text"
+            size="small"
+          >
+            <FontAwesomeIcon icon={faX} className="shadow-lg shadow-white" />
+          </Button>
           <img
             className={`object-cover relative h-full overflow-clip w-[calc(130%+7.2px)]`}
             src={URL.createObjectURL(file)}
             alt="Preview"
           />
+        </div>
+      ) : (
+        <div className="flex flex-col items-center justify-center rounded-md border border-dashed p-6 text-center w-full aspect-[1.7777/1]">
+          <FontAwesomeIcon className="text-2xl text-gray-600" icon={faUpload} />
+          <p className="mt-2 text-md text-gray-600">Datei hochladen</p>
+          <div className="mt-4">
+            <Button
+              onClick={() => {
+                if (inputRef.current) {
+                  inputRef.current.click();
+                }
+              }}
+            >
+              Datei Auswählen
+            </Button>
+          </div>
         </div>
       )}
     </div>
