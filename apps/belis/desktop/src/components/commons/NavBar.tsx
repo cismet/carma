8b5a67c7f9d66fe1324b2bf7b4bbc6c193f8126e
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-07-08 13:47:58 +0200

    #341 fix layout gaps

--- apps/belis/desktop/src/components/commons/NavBar.tsx
+++ apps/belis/desktop/src/components/commons/NavBar.tsx
@@ -4,9 +4,9 @@ import UserBar from "./UserBar";
 
 const NavBar = () => {
   return (
-    <div className="flex justify-between gap-3 w-full h-full min-h-screen overflow-clip px-2">
-      <div className="w-[200px] mt-2">Belis-desktop</div>
-      <div className="grow bg-[#F1F1F1]">
+    <div className="bg-[#F1F1F1] flex justify-between gap-3 w-full h-full min-h-screen overflow-clip">
+      <div className="w-[200px] bg-white pl-2 pt-2">Belis-desktop</div>
+      <div className="grow">
         {/* <div className="mx-3 mt-2">Top navbar</div> */}
         <UserBar />
         <Outlet />
