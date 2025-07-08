425403097eb9b0a274511754e0d1b02a7d2947eb
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-07-08 17:47:37 +0200

    #341 add fetching filter

---
+++ libraries/appframeworks/belis/src/lib/utils/fetchingHelper.ts
@@ -0,0 +1,8 @@
+export const filter = {
+  tdta_leuchten: { title: "Leuchten", enabled: true },
+  tdta_standort_mast: { title: "Masten (ohne Leuchten)", enabled: true },
+  mauerlasche: { title: "Mauerlaschen", enabled: true },
+  leitung: { title: "Leitungen", enabled: true },
+  schaltstelle: { title: "Schaltstellen", enabled: true },
+  abzweigdose: { title: "Abzweigdosen", enabled: true },
+};
