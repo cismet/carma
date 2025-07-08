c85f0352a23ee848b3279a1a688983a16146fc13
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-07-08 15:17:38 +0200

    #341 remove menu icon

--- apps/belis/desktop/src/components/commons/NavBar.tsx
+++ apps/belis/desktop/src/components/commons/NavBar.tsx
@@ -5,10 +5,7 @@ import UserBar from "./UserBar";
 const NavBar = () => {
   return (
     <div className="bg-[#F1F1F1] flex justify-between w-full h-full min-h-screen overflow-clip">
-      <div className="w-[250px] bg-white pl-2 pt-2">
-        <span className="cursor-pointer">
-          <MenuOutlined style={{ textAlign: "left" }} />
-        </span>
+      <div className="w-[230px] bg-white pl-2 pt-2">
         <span className="ml-2">Belis-desktop</span>
       </div>
       <div className="grow">
