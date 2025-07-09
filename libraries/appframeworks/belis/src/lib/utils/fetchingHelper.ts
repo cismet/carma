3dd02024a84c3d4357b6e853e050c1c2c13ffa00
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-07-09 09:02:14 +0200

    #341 add more fetching helpers

--- libraries/appframeworks/belis/src/lib/utils/fetchingHelper.ts
+++ libraries/appframeworks/belis/src/lib/utils/fetchingHelper.ts
@@ -108,3 +108,215 @@ export async function fetchGraphQL(
     throw new Error(e);
   }
 }
+
+export const createFeatureFromData = (data, type) => {
+  const feature = {
+    text: "-",
+    id: type,
+    enriched: true,
+    type: "Feature",
+    selected: false,
+    featuretype: type,
+    // geometry: geomfactory(data),
+    geometry: geomFactories[type](data),
+    crs: {
+      type: "name",
+      properties: {
+        name: "urn:ogc:def:crs:EPSG::25832",
+      },
+    },
+    properties: {},
+  };
+
+  //The geometry could be deleted to save some memory
+  //need to be a different approach in the geometryfactory then
+  //not sure beacuse the properties would double up the memoryconsumption though
+  //
+  // const properties=JSON.parse(JSON.stringify(o));
+  // delete propertiesgeomFactories[key](o)
+
+  feature.properties = data;
+
+  feature.id = feature.id + "-" + data.id;
+  feature.properties.docs = getDocs(feature);
+  return feature;
+};
+
+const integrateIntermediateResultsIntoObjects = (
+  intermediateResults,
+  item,
+  type,
+  id
+) => {
+  // console.log("integrateIntermediateResultsIntoObjects " + type, id);
+
+  if (intermediateResults && intermediateResults[type]) {
+    const irs = intermediateResults[type][id];
+    if (irs?.object) {
+      for (const ir of irs.object) {
+        for (const key of Object.keys(ir)) {
+          // console.log("iv in ", { item, key });
+
+          item[key] = ir[key];
+          //will ad a property with _iv postfix to the item, so that the ui can recognize it
+          item[key + "_iv"] = true;
+        }
+      }
+    }
+  }
+};
+
+const enrichAndSetFeatures = (
+  dispatch,
+  state,
+  featureCollectionIn
+  // removeFromIntermediateResults
+) => {
+  console.time("features enirched");
+
+  const tasks = [];
+
+  const currentFeatureCollection = getFeatureCollection(state);
+
+  const stillInMap = currentFeatureCollection.filter((f) =>
+    featureCollectionIn.find((test) => f.id === test.id)
+  );
+  const newInMap = featureCollectionIn.filter(
+    (f) => stillInMap.find((test) => f.id === test.id) === undefined
+  );
+  // console.log("stillInMap", stillInMap);
+  // console.log("newInMap", newInMap);
+  // console.log("oldInMap", featureCollectionIn);
+  const featureCollection = [...stillInMap, ...newInMap];
+  //const featureCollection = featureCollectionIn;
+
+  // prerendering featureCollection
+  // needs to change listitems defauklt attributes to "... wird geladen"
+  //dispatch(setFeatureCollection(featureCollection));
+
+  for (const f of featureCollection) {
+    tasks.push(cloneFeature(f));
+  }
+  const selectedFeature = getSelectedFeature(state);
+  // let intermediateResultsToBeRemoved = [];
+
+  Promise.all(tasks).then(
+    (enrichedFeatureCollection) => {
+      console.timeEnd("features enirched");
+      const sortedElements = [];
+      const typeCount = {};
+      let selectionStillInMap = false;
+      // console.time("cycling through enriched fc");
+
+      for (const feature of enrichedFeatureCollection) {
+        feature.intermediateResultsIntegrated = new Date().getTime();
+        integrateIntermediateResults(
+          feature,
+          state.offlineActionDb.intermediateResults
+        );
+
+        if (typeCount[feature.featuretype] === undefined) {
+          typeCount[feature.featuretype] = 1;
+        } else {
+          typeCount[feature.featuretype] = typeCount[feature.featuretype] + 1;
+        }
+
+        if (feature.properties.is_deleted !== true) {
+          sortedElements.push(feature);
+          // console.log("shown feature", feature);
+        } else {
+          // console.log("deleted feature", feature);
+        }
+        // sortedElements.push(feature);
+
+        if (selectedFeature && feature.id === selectedFeature.id) {
+          selectionStillInMap = true;
+          // feature.selected = true;
+        }
+      }
+      // console.timeEnd("cycling through enriched fc");
+
+      sortedElements.sort(compareFeature);
+      if (!selectionStillInMap) {
+        // dispatch(setSelectedFeature(null));
+      }
+      let index = 0;
+      for (const f of sortedElements) {
+        f.index = index++;
+      }
+
+      // dispatch(
+      //   setFeatureCollectionInfoForMode({
+      //     mode: MODES.OBJECTS,
+      //     info: { typeCount },
+      //   })
+      // );
+
+      // dispatch(
+      //   setFeatureCollectionForMode({
+      //     features: sortedElements,
+      //     mode: MODES.OBJECTS,
+      //   })
+      // );
+
+      // dispatch(setDoneForMode({ mode: MODES.OBJECTS, done: true }));
+    },
+    (error) => {
+      alert("problem" + error);
+      //todo: do something
+    }
+  );
+};
+
+export const loadObjectsIntoFeatureCollection = ({
+  boundingBox,
+  _inFocusMode,
+  _zoom,
+  _overridingFilterState,
+  jwt,
+  onlineDataForcing = false,
+}) => {
+  if (boundingBox) {
+    //const boundingBox=
+    return async (dispatch, getState) => {
+      let queryparts = "";
+
+      for (const filterKey of Object.keys(filter)) {
+        if (filter[filterKey].enabled === true) {
+          const qp = onlineQueryParts[filterKey];
+          queryparts += qp + "\n";
+        }
+      }
+      const gqlQuery = `query q($bbPoly: geometry!) {${queryparts}}`;
+
+      const queryParameter = {
+        bbPoly: createQueryGeomFromBB(convertedBoundingBox),
+      };
+      try {
+        console.time("query returned");
+        // online query
+        const response = await fetchGraphQL(gqlQuery, queryParameter, jwt);
+        console.timeEnd("query returned");
+
+        if (response?.ok) {
+          const featureCollection = [];
+          for (const key of Object.keys(response.data || {})) {
+            const objects = response.data[key];
+            for (const o of objects) {
+              const feature = createFeatureFromData(o, key);
+              featureCollection.push(feature);
+            }
+          }
+          enrichAndSetFeatures(dispatch, state, featureCollection, true);
+        } else {
+          throw new Error("Error in fetchGraphQL (" + response.status + ")");
+        }
+      } catch (e) {
+        console.log("error was thrown", e);
+        console.log("errorneous query", { gqlQuery, queryParameter, jwt });
+        // dispatch(setRequestBasis(undefined));
+        // dispatch(setHealthState({ jwt, healthState: HEALTHSTATUS.ERROR }));
+      }
+    };
+  }
+};
