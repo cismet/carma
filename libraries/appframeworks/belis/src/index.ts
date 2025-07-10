ba5d5ef757d1ea0e323ef94ecf3bf3c5cef00728
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-07-10 11:26:31 +0200

    #341 add map blocker component

--- libraries/appframeworks/belis/src/index.ts
+++ libraries/appframeworks/belis/src/index.ts
@@ -1,5 +1,6 @@
 export * from "./lib/lib-belis-library";
 export { backgroundLayerConfigurations } from "./lib/components/BackgroundLayers";
+export { MapBlocker } from "./lib/components/MapBlocker";
 export {
   loadObjectsIntoFeatureCollection,
   featuresFilter,
