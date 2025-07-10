781c2da74e2b1078a18ed65d9a61c51e35c391a5
David Glogaza <david.glogaza40@googlemail.com>
2025-07-10 16:15:47 +0200

    update event listeners on lib locate control

--- libraries/mapping/carma-map-components/src/lib/components/RoutedMapLocateControl/hooks/useRoutedMapLocateControl.ts
+++ libraries/mapping/carma-map-components/src/lib/components/RoutedMapLocateControl/hooks/useRoutedMapLocateControl.ts
@@ -28,11 +28,12 @@ export const useRoutedMapLocateControl = () => {
         }, 300);
       };
 
-      map.on("move", handleMapMove);
+      map.on("dragend zoomend", handleMapMove);
       map.on("locationfound", handleLocationFound);
 
       return () => {
-        map.off("move", handleMapMove);
+        map.off("dragend", handleMapMove);
+        map.off("zoomend", handleMapMove);
         map.off("locationfound", handleLocationFound);
       };
     }
