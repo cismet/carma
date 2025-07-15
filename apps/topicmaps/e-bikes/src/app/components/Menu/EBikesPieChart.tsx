4fb4de971b15453bcd0f1d0f042b73b3f0ec801d
David Glogaza <david.glogaza40@googlemail.com>
2025-07-15 10:46:25 +0200

    fix e bikes filter chart

--- apps/topicmaps/e-bikes/src/app/components/Menu/EBikesPieChart.tsx
+++ apps/topicmaps/e-bikes/src/app/components/Menu/EBikesPieChart.tsx
@@ -1,8 +1,10 @@
 import { useContext } from "react";
 import { FeatureCollectionContext } from "react-cismap/contexts/FeatureCollectionContextProvider";
 import { getColorForProperties } from "../../../helper/styler";
-// import { Doughnut } from "react-chartjs-2";
-// import "chart.js/auto";
+import ReactChartkick, { PieChart } from "react-chartkick";
+import { Chart } from "chart.js";
+
+ReactChartkick.addAdapter(Chart);
 
 const EBikesPieChart = ({ visible = true }) => {
   const { filteredItems } = useContext<typeof FeatureCollectionContext>(
@@ -42,23 +44,6 @@ const EBikesPieChart = ({ visible = true }) => {
       piechartColor.push(colormodel[key]);
     }
 
-    const labels = piechartData.map((data) => {
-      return data[0];
-    });
-
-    const tmpData = piechartData.map((data) => {
-      return data[1];
-    });
-
-    const data = {
-      labels: labels,
-      datasets: [
-        {
-          data: tmpData,
-          backgroundColor: piechartColor,
-        },
-      ],
-    };
     return (
       <td
         style={{
@@ -69,27 +54,13 @@ const EBikesPieChart = ({ visible = true }) => {
           justifyContent: "center",
         }}
       >
-        <div style={{ width: "40%" }}>
-          {/* <Doughnut
-            data={data}
-            options={{
-              plugins: {
-                legend: {
-                  display: false,
-                },
-                title: {
-                  display: true,
-                  text: "Verteilung",
-                  font: {
-                    weight: "bold",
-                    size: 20,
-                  },
-                  color: "black",
-                },
-              },
-            }}
-          /> */}
-        </div>
+        <PieChart
+          data={piechartData}
+          donut={true}
+          title="Verteilung"
+          legend={false}
+          colors={piechartColor}
+        />
       </td>
     );
   } else {
