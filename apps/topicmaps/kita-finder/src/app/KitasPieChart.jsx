d4d08c4d6348e9db8be0b796a3e72ced7c230249
David Glogaza <david.glogaza40@googlemail.com>
2025-07-15 10:46:04 +0200

    fix kita filter chart

--- apps/topicmaps/kita-finder/src/app/KitasPieChart.jsx
+++ apps/topicmaps/kita-finder/src/app/KitasPieChart.jsx
@@ -1,11 +1,13 @@
-// import { Doughnut } from "react-chartjs-2";
-// import "chart.js/auto";
 import { constants as kitasConstants } from "./helper/constants";
 import { getColorForProperties } from "./helper/styler";
 import { useContext } from "react";
 import { FeatureCollectionContext } from "react-cismap/contexts/FeatureCollectionContextProvider";
 import { useSelector } from "react-redux";
 import { getFeatureRenderingOption } from "./store/slices/ui";
+import ReactChartkick, { PieChart } from "react-chartkick";
+import { Chart } from "chart.js";
+
+ReactChartkick.addAdapter(Chart);
 
 const KitasPieChart = ({ visible = true }) => {
   const { filteredItems } = useContext(FeatureCollectionContext);
@@ -53,23 +55,6 @@ const KitasPieChart = ({ visible = true }) => {
       piechartColor.push(colormodel[key]);
     }
 
-    const labels = piechartData.map((data) => {
-      return data[0];
-    });
-
-    const tmpData = piechartData.map((data) => {
-      return data[1];
-    });
-
-    const data = {
-      labels: labels,
-      datasets: [
-        {
-          data: tmpData,
-          backgroundColor: piechartColor,
-        },
-      ],
-    };
     return (
       <td
         style={{
@@ -80,27 +65,13 @@ const KitasPieChart = ({ visible = true }) => {
           justifyContent: "center",
         }}
       >
-        <div style={{ width: "40%" }}>
-          {/* <Doughnut
-            data={data}
-            options={{
-              plugins: {
-                legend: {
-                  display: false,
-                },
-                title: {
-                  display: true,
-                  text: "Verteilung",
-                  font: {
-                    weight: "bold",
-                    size: 20,
-                  },
-                  color: "black",
-                },
-              },
-            }}
-          /> */}
-        </div>
+        <PieChart
+          data={piechartData}
+          donut={true}
+          title="Verteilung"
+          legend={false}
+          colors={piechartColor}
+        />
       </td>
     );
   } else {
