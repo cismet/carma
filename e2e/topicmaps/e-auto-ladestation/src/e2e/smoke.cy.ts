996f62fa04e1975f577102ff58b0427ade727d77
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-07-17 12:22:27 +0200

    #346 add smoke test e-auto

---
+++ e2e/topicmaps/e-auto-ladestation/src/e2e/smoke.cy.ts
@@ -0,0 +1,13 @@
+describe("e-auto-ladestation smoke test", () => {
+  beforeEach(() => cy.visit("/"));
+
+  it("map loads with key controls", () => {
+    cy.get("[data-test-id=zoom-control]").should("be.visible");
+
+    cy.get("[data-test-id=fuzzy-search]").should("be.visible");
+
+    cy.get("#cmdShowModalApplicationMenu").should("be.visible");
+
+    cy.get("[data-test-id=info-box]").should("be.visible");
+  });
+});
