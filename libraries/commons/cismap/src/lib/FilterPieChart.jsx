5773bced1e965bfcba7225985f5adce74a8ab9f5
David Glogaza <david.glogaza40@googlemail.com>
2025-07-18 20:55:10 +0200

    add cismap filter panel

---
+++ libraries/commons/cismap/src/lib/FilterPieChart.jsx
@@ -0,0 +1,37 @@
+import { PieChart } from "@carma-apps/portals";
+
+export const FilterPieChart = ({
+  filteredItems,
+  itemGetClassKey = () => "noclassavailable",
+  getColor = (item) => "red",
+  visible = true,
+}) => {
+  if (visible) {
+    let stats = {};
+    let colormodel = {};
+    let piechartData = [];
+    let piechartColor = [];
+    if (filteredItems) {
+      for (let item of filteredItems) {
+        const itemClassKey = itemGetClassKey(item);
+
+        if (stats[itemClassKey] === undefined) {
+          stats[itemClassKey] = 1;
+          colormodel[itemClassKey] = getColor(item);
+        } else {
+          stats[itemClassKey] += 1;
+        }
+      }
+
+      for (let key in stats) {
+        piechartData.push([key, stats[key]]);
+        piechartColor.push(colormodel[key]);
+      }
+    }
+    return <PieChart data={piechartData} colors={piechartColor} />;
+  } else {
+    return null;
+  }
+};
+
+export default FilterPieChart;
