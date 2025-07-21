1a82f16ac8fe845906841cde2d8fe2cdf2fac7eb
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-07-21 15:58:43 +0200

    temp set

---
+++ apps/topicmaps/vorhabenkarte/src/helper/createItemsDictionary.ts
@@ -0,0 +1,10 @@
+import { predicateBy } from "react-cismap/tools/stringHelper";
+
+const createItemsDictionary = (items) => {
+  //   const topicsSet = new Set();
+  const topicsSet = ["test", "test 1", "test 2"];
+
+  return { topicsSet };
+};
+
+export default createItemsDictionary;
