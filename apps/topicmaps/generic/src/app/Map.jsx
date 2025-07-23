e650529f2e3ad87fb0f8f9e5df8456c283c1119c
David Glogaza <david.glogaza40@googlemail.com>
2025-07-23 09:46:07 +0200

    update generic topicmap controls layout

--- apps/topicmaps/generic/src/app/Map.jsx
+++ apps/topicmaps/generic/src/app/Map.jsx
@@ -13,14 +13,20 @@ import SecondaryInfoModal from "./SecondaryInfoModal";
 import { FeatureCollectionContext } from "react-cismap/contexts/FeatureCollectionContextProvider";
 import { UIDispatchContext } from "react-cismap/contexts/UIContextProvider";
 import { MenuFooter } from "@carma-collab/wuppertal/commons";
-import { getApplicationVersion } from "@carma-commons/utils";
+import {
+  getApplicationVersion,
+  TAILWIND_CLASSNAMES_FULLSCREEN_FIXED,
+} from "@carma-commons/utils";
 import versionData from "../version.json";
 import { GenericDigitalTwinReferenceSection } from "@carma-collab/wuppertal/commons";
 import {
   TopicMapSelectionContent,
   useSelectionTopicMap,
 } from "@carma-apps/portals";
-import { EmptySearchComponent } from "@carma-mapping/fuzzy-search";
+import {
+  EmptySearchComponent,
+  LibFuzzySearch,
+} from "@carma-mapping/fuzzy-search";
 import FuzzySearchWrapper from "./components/FuzzySearchWrapper";
 import { Control, ControlLayout } from "@carma-mapping/map-controls-layout";
 import {
@@ -35,6 +41,7 @@ import { TopicMapContext } from "react-cismap/contexts/TopicMapContextProvider";
 import Menu from "./components/Menu";
 import { TopicMapStylingContext } from "react-cismap/contexts/TopicMapStylingContextProvider";
 import md5 from "md5";
+import { ResponsiveTopicMapContext } from "react-cismap/contexts/ResponsiveTopicMapContextProvider";
 const host = import.meta.env.VITE_WUPP_ASSET_BASEURL;
 const downloadText = (text, filename) => {
   var element = document.createElement("a");
@@ -129,6 +136,9 @@ const Map = ({
   const { selectedFeature } = useContext(FeatureCollectionContext);
   const [globalHits, setGlobalHits] = useState({});
   const { markerSymbolSize } = useContext(TopicMapStylingContext);
+  const { responsiveState, gap, windowSize } = useContext(
+    ResponsiveTopicMapContext
+  );
   const [cl_key, setClKey] = useState("");
   const { routedMapRef } = useSelectionTopicMap() ?? {};
   const [selectedVectorObject, setSelectedVectorObject] = useState(undefined);
@@ -213,137 +223,132 @@ const Map = ({
   }
 
   return (
-    <>
-      <div
-        className="controls-container"
-        style={{
-          position: "absolute",
-          top: "0px",
-          left: "0px",
-          bottom: "0px",
-          zIndex: 600,
-        }}
-      >
-        <ControlLayout ifStorybook={false}>
-          {config?.tm?.zoomControls && (
-            <Control position="topleft" order={10}>
-              <ZoomControl />
-            </Control>
-          )}
+    <div className={TAILWIND_CLASSNAMES_FULLSCREEN_FIXED}>
+      <ControlLayout ifStorybook={false}>
+        {config?.tm?.zoomControls && (
+          <Control position="topleft" order={10}>
+            <ZoomControl />
+          </Control>
+        )}
 
-          {config?.tm?.fullScreenControl && (
-            <Control position="topleft" order={50}>
-              <FullscreenControl />
-            </Control>
-          )}
-          {config?.tm?.locatorControl && (
-            <Control position="topleft" order={60} title="Mein Standort">
-              <RoutedMapLocateControl
-                tourRefLabels={null}
-                disabled={false}
-                nativeTooltip={true}
+        {config?.tm?.fullScreenControl && (
+          <Control position="topleft" order={50}>
+            <FullscreenControl />
+          </Control>
+        )}
+        {config?.tm?.locatorControl && (
+          <Control position="topleft" order={60} title="Mein Standort">
+            <RoutedMapLocateControl
+              tourRefLabels={null}
+              disabled={false}
+              nativeTooltip={true}
+            />
+          </Control>
+        )}
+        {config?.tm?.gazetteerSearchBox && (
+          <Control position="bottomleft" order={10}>
+            <div style={{ marginTop: "4px" }}>
+              <LibFuzzySearch
+                gazData={featureGazData}
+                placeholder={config.tm.gazetteerSearchBoxPlaceholdertext}
+                clickAfterGazetteerHit={config.tm.clickAfterGazetteerHit}
+                pixelwidth={
+                  responsiveState === "normal"
+                    ? "300px"
+                    : windowSize.width - gap
+                }
               />
-            </Control>
-          )}
-          {config?.tm?.gazetteerSearchBox && (
-            <Control position="bottomleft" order={10}>
-              <div className="h-full w-full pl-2">
-                <FuzzySearchWrapper
-                  featureGazData={featureGazData}
-                  placeholder={config.tm.gazetteerSearchBoxPlaceholdertext}
-                  clickAfterGazetteerHit={config.tm.clickAfterGazetteerHit}
-                />
-              </div>
-            </Control>
-          )}
-        </ControlLayout>
-      </div>
-      <SecondaryInfoModal
-        feature={selectedFeature}
-        footer={
-          <MenuFooter
-            version={getApplicationVersion(versionData)}
-            setAppMenuActiveMenuSection={setAppMenuActiveMenuSection}
-          />
-        }
-      />
-      <TopicMapComponent
-        {...config.tm}
-        locatorControl={false}
-        fullScreenControl={false}
-        zoomControls={false}
-        gazetteerSearchComponent={EmptySearchComponent}
-        infoBox={
-          config.tm.vectorLayers && config.tm.noFeatureCollection === true ? (
-            <FeatureInfobox selectedFeature={feature} />
-          ) : (
-            <>
-              {config.tm.noFeatureCollection !== true && (
-                <GenericInfoBoxFromFeature config={config.info} />
+            </div>
+          </Control>
+        )}
+
+        <SecondaryInfoModal
+          feature={selectedFeature}
+          footer={
+            <MenuFooter
+              version={getApplicationVersion(versionData)}
+              setAppMenuActiveMenuSection={setAppMenuActiveMenuSection}
+            />
+          }
+        />
+        <TopicMapComponent
+          {...config.tm}
+          locatorControl={false}
+          fullScreenControl={false}
+          zoomControls={false}
+          gazetteerSearchComponent={EmptySearchComponent}
+          infoBox={
+            config.tm.vectorLayers && config.tm.noFeatureCollection === true ? (
+              <FeatureInfobox selectedFeature={feature} />
+            ) : (
+              <>
+                {config.tm.noFeatureCollection !== true && (
+                  <GenericInfoBoxFromFeature config={config.info} />
+                )}
+              </>
+            )
+          }
+          hamburgerMenu={config?.tm?.applicationMenu}
+          modalMenu={
+            <Menu
+              menuTitle={config?.tm?.applicationMenuTitle}
+              checkBoxSettingsSectionTitle={null}
+              skipClusteringSettings={
+                config?.tm?.applicationMenuSkipClusteringSettings
+              }
+              skipSymbolsizeSetting={
+                config?.tm?.applicationMenuSkipSymbolsizeSetting
+              }
+              simpleHelp={config?.simpleHelpObject}
+              previewMapPosition={config?.tm?.previewMapPosition}
+              previewChildren={renderCismapLayers(
+                config,
+                markerSymbolSize,
+                setGlobalHits,
+                selectedVectorObject,
+                layerInformation
               )}
+              previewFeatureCollectionCount={
+                config?.tm?.previewFeatureCollectionCount
+              }
+              {...(getSymbolSVG ? { getSymbolSVG } : {})}
+              previewChildrenKey={cl_key}
+              introductionMarkdown={
+                config?.tm?.applicationMenuIntroductionMarkdown
+              }
+              sectionmapping={
+                config?.tm?.applicationMenuIntroductionMarkdownSectionMapping
+              }
+              menuIcon={config?.tm?.applicationMenuIconname}
+              menuFooter={
+                config?.tm?.applicationMenuFooter || (
+                  <MenuFooter
+                    version={getApplicationVersion(versionData)}
+                    setAppMenuActiveMenuSection={setAppMenuActiveMenuSection}
+                  />
+                )
+              }
+              sections={[<GenericDigitalTwinReferenceSection />]}
+              layerHelpBlocks={layerHelpBlocks}
+            />
+          }
+        >
+          {renderCismapLayers(
+            config,
+            markerSymbolSize,
+            setGlobalHits,
+            selectedVectorObject,
+            layerInformation
+          )}
+          {config.tm.noFeatureCollection !== true && (
+            <>
+              <FeatureCollection />
             </>
-          )
-        }
-        hamburgerMenu={config?.tm?.applicationMenu}
-        modalMenu={
-          <Menu
-            menuTitle={config?.tm?.applicationMenuTitle}
-            checkBoxSettingsSectionTitle={null}
-            skipClusteringSettings={
-              config?.tm?.applicationMenuSkipClusteringSettings
-            }
-            skipSymbolsizeSetting={
-              config?.tm?.applicationMenuSkipSymbolsizeSetting
-            }
-            simpleHelp={config?.simpleHelpObject}
-            previewMapPosition={config?.tm?.previewMapPosition}
-            previewChildren={renderCismapLayers(
-              config,
-              markerSymbolSize,
-              setGlobalHits,
-              selectedVectorObject,
-              layerInformation
-            )}
-            previewFeatureCollectionCount={
-              config?.tm?.previewFeatureCollectionCount
-            }
-            {...(getSymbolSVG ? { getSymbolSVG } : {})}
-            previewChildrenKey={cl_key}
-            introductionMarkdown={
-              config?.tm?.applicationMenuIntroductionMarkdown
-            }
-            sectionmapping={
-              config?.tm?.applicationMenuIntroductionMarkdownSectionMapping
-            }
-            menuIcon={config?.tm?.applicationMenuIconname}
-            menuFooter={
-              config?.tm?.applicationMenuFooter || (
-                <MenuFooter
-                  version={getApplicationVersion(versionData)}
-                  setAppMenuActiveMenuSection={setAppMenuActiveMenuSection}
-                />
-              )
-            }
-            sections={[<GenericDigitalTwinReferenceSection />]}
-            layerHelpBlocks={layerHelpBlocks}
-          />
-        }
-      >
-        {renderCismapLayers(
-          config,
-          markerSymbolSize,
-          setGlobalHits,
-          selectedVectorObject,
-          layerInformation
-        )}
-        {config.tm.noFeatureCollection !== true && (
-          <>
-            <FeatureCollection />
-          </>
-        )}
-        <TopicMapSelectionContent />
-      </TopicMapComponent>
-    </>
+          )}
+          <TopicMapSelectionContent />
+        </TopicMapComponent>
+      </ControlLayout>
+    </div>
   );
 };
 
