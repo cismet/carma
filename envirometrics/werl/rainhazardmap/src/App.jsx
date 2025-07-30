50f96a469b28aaae45d7c9f36be122112310932f
Thorsten Hell <thorsten.hell@cismet.de>
2025-07-30 13:23:02 +0200

    initial implementation of rainhazardmap werl

---
+++ envirometrics/werl/rainhazardmap/src/App.jsx
@@ -0,0 +1,164 @@
+import HeavyRainHazardMap from "@cismet-dev/react-cismap-envirometrics-maps/HeavyRainHazardMap";
+import React, { useContext, useEffect, useState } from "react";
+import { md5FetchJSON } from "react-cismap/tools/fetching";
+import GenericModalApplicationMenu from "react-cismap/topicmaps/menu/ModalApplicationMenu";
+import versionData from "./version.json";
+import config from "./config";
+import { getApplicationVersion } from "@carma-commons/utils";
+import { getCollabedHelpComponentConfig } from "@carma-pecher-collab/xanten";
+import "./notification.css";
+import footerLogoUrl from "./assets/images/Signet_AIS_RZ.png";
+import {
+  EmptySearchComponent,
+  LibFuzzySearch,
+} from "@carma-mapping/fuzzy-search";
+import {
+  TopicMapSelectionContent,
+  useSelection,
+  useSelectionTopicMap,
+} from "@carma-apps/portals";
+import {
+  useAttributionControlStyling,
+  Control,
+  ControlButtonStyler,
+  ControlLayout,
+} from "@carma-mapping/map-controls-layout";
+import {
+  FullscreenControl,
+  RoutedMapLocateControl,
+  ZoomControl,
+} from "@carma-mapping/components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCompress, faExpand } from "@fortawesome/free-solid-svg-icons";
+import ContactButton from "./components/ContactButton";
+import { ResponsiveTopicMapContext } from "react-cismap/contexts/ResponsiveTopicMapContextProvider";
+import { isAreaTypeWithGEP } from "@carma-commons/resources";
+
+function App() {
+  const version = getApplicationVersion(versionData);
+  const email = "starkregen@xanten.de";
+  const [gazData, setGazData] = useState([]);
+  const { responsiveState, gap, windowSize } = useContext(
+    ResponsiveTopicMapContext
+  );
+  const urlPrefix = window.location.origin + window.location.pathname;
+  const { attributionHeight } = useAttributionControlStyling({
+    styles: { marginLeft: "16px", marginTop: "2px" },
+  });
+
+  const getGazData = async (setGazData, url) => {
+    const prefix = "GazDataForStarkregengefahrenkarteByCismet";
+    const data = await md5FetchJSON(prefix, url);
+
+    setGazData(data || []);
+  };
+
+  useEffect(() => {
+    getGazData(setGazData, urlPrefix + "/data/adressen_xanten.json");
+  }, []);
+
+  const { setSelection } = useSelection();
+  useSelectionTopicMap();
+
+  const ifDesktop = responsiveState === "normal";
+
+  const onGazetteerSelection = (selection) => {
+    if (!selection) {
+      setSelection(null);
+      return;
+    }
+
+    const selectionMetaData = {
+      selectedFrom: "gazetteer",
+      selectionTimestamp: Date.now(),
+      isAreaSelection: isAreaTypeWithGEP(selection),
+    };
+    setSelection(Object.assign({}, selection, selectionMetaData));
+  };
+
+  return (
+    <>
+      <div
+        className="controls-container"
+        style={{
+          position: "absolute",
+          top: "0px",
+          left: "0px",
+          bottom: "0px",
+          zIndex: 600,
+        }}
+      >
+        <ControlLayout ifStorybook={false}>
+          <Control position="topleft" order={10}>
+            <ZoomControl />
+          </Control>
+
+          <Control position="topleft" order={50}>
+            <FullscreenControl />
+          </Control>
+          <Control position="topleft" order={60} title="Mein Standort">
+            <RoutedMapLocateControl
+              tourRefLabels={null}
+              disabled={false}
+              nativeTooltip={true}
+            />
+          </Control>
+          <Control position="topleft" order={70}>
+            <ContactButton emailaddress={email} />
+          </Control>
+          <Control position="bottomleft" order={10}>
+            <div className="h-full w-full pl-2">
+              <div
+                className="custom-left-control"
+                style={{
+                  marginBottom: ifDesktop ? "0" : attributionHeight + 4,
+                }}
+              >
+                <LibFuzzySearch
+                  gazData={gazData}
+                  onSelection={onGazetteerSelection}
+                  pixelwidth={
+                    responsiveState === "normal"
+                      ? "300px"
+                      : windowSize.width - gap
+                  }
+                  placeholder="Adresssuche"
+                />
+              </div>
+            </div>
+          </Control>
+        </ControlLayout>
+      </div>
+      <HeavyRainHazardMap
+        appMenu={
+          <GenericModalApplicationMenu
+            {...getCollabedHelpComponentConfig({
+              version,
+              reactCismapRHMVersion: "_",
+              footerLogoUrl,
+              email,
+            })}
+          />
+        }
+        applicationMenuTooltipString="Anleitung | Hintergrund"
+        initialState={config.initialState}
+        contactButtonEnabled={false}
+        locatorControl={false}
+        fullScreenControl={false}
+        zoomControls={false}
+        emailaddress={email}
+        config={config.config}
+        homeZoom={13}
+        homeCenter={[51.658873404435404, 6.437902450561524]}
+        modeSwitcherTitle="AIS Starkregenvorsorge Werl"
+        documentTitle="AIS Starkregenvorsorge Werl"
+        gazetteerSearchControl={true}
+        gazetteerSearchComponent={EmptySearchComponent}
+      >
+        <TopicMapSelectionContent />
+      </HeavyRainHazardMap>
+    </>
+  );
+}
+
+export default App;
---
+++ envirometrics/werl/rainhazardmap/src/main.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import "react-bootstrap-typeahead/css/Typeahead.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "leaflet/dist/leaflet.css";
+import "react-cismap/topicMaps.css";
+import App from "./App";
+import { GazDataProvider, SelectionProvider } from "@carma-apps/portals";
+import { gazDataConfig } from "./config/gazData";
+import { MappingConstants } from "react-cismap";
+import TopicMapContextProvider from "react-cismap/contexts/TopicMapContextProvider";
+import config from "./config";
+
+const appKey = "cismetRainhazardMap";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <GazDataProvider config={gazDataConfig}>
+      <SelectionProvider>
+        <TopicMapContextProvider
+          appKey={appKey + ".Xerl"}
+          referenceSystem={MappingConstants.crs3857}
+          referenceSystemDefinition={MappingConstants.proj4crs3857def}
+          baseLayerConf={config.overridingBaseLayerConf}
+          infoBoxPixelWidth={370}
+        >
+          <App />
+        </TopicMapContextProvider>
+      </SelectionProvider>
+    </GazDataProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
