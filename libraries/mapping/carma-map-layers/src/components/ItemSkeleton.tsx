949e98eb83853cc39d2192abc14dd3621444594e
David Glogaza <david.glogaza40@googlemail.com>
2025-08-07 09:43:41 +0200

    remove key prop

--- libraries/mapping/carma-map-layers/src/components/ItemSkeleton.tsx
+++ libraries/mapping/carma-map-layers/src/components/ItemSkeleton.tsx
@@ -1,9 +1,6 @@
-const ItemSkeleton = ({ key }: { key: string }) => {
+const ItemSkeleton = () => {
   return (
-    <div
-      key={key}
-      className="bg-white rounded-lg shadow-sm h-80 w-full flex flex-col gap-2 animate-pulse"
-    >
+    <div className="bg-white rounded-lg shadow-sm h-80 w-full flex flex-col gap-2 animate-pulse">
       <div className="h-40 p-2 w-full bg-slate-200 rounded-t-lg"></div>
       <div className="h-4 mt-4 bg-slate-200 rounded mx-8 w-2/3"></div>
     </div>
