6a6d6c5448b59abdcff13f708b4fc4ed0251df4a
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-08-20 17:41:52 +0200

    #365 migrate luftmessstationen to px

---
+++ e2e/topicmaps/luftmessstationen/playwright.config.ts
@@ -0,0 +1,33 @@
+import { defineConfig } from "@playwright/test";
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ */
+export default defineConfig({
+  testDir: "./src/e2e",
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: "html",
+  use: {
+    baseURL: "http://localhost:4222",
+    trace: "on-first-retry",
+    screenshot: "only-on-failure",
+    channel: process.env.CI ? "chrome" : undefined,
+  },
+  projects: [
+    {
+      name: "chromium",
+      use: {
+        channel: process.env.CI ? "chrome" : undefined,
+      },
+    },
+  ],
+  webServer: {
+    command: "npx nx serve luftmessstationen --port=4222",
+    url: "http://localhost:4222",
+    reuseExistingServer: false,
+    timeout: 60_000,
+  },
+});
