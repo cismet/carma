96394d54ad7ef40546015b4f39bf058862e088c1
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-08-22 13:57:57 +0200

    #365 migrate layer group title

---
+++ e2e/geoportal/geoportal/src/e2e/layer-group-toggle.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect } from "@playwright/test";
+
+test.describe("geoportal layer group icon", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("/");
+  });
+
+  test("Toggle visibility of layer group button", async ({ page }) => {
+    const btn = page.locator("#layer-karte").getByRole("button");
+    const slider = page.getByText("Transparenz:");
+    await expect(slider).toHaveCount(0);
+
+    await expect(btn).toBeVisible();
+
+    await btn.click();
+    await expect(slider).toBeVisible();
+
+    await btn.click();
+    await expect(slider).toHaveCount(0);
+  });
+});
