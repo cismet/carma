6888255ee0ff55c6d38b434a49bdc8151683d9aa
Friedrich Hartmann <friedrich.hartmann@cismet.de>
2025-08-28 03:14:40 +0200

    log tailwind resolutin for ebikes

--- apps/topicmaps/e-bikes/tailwind.config.cjs
+++ apps/topicmaps/e-bikes/tailwind.config.cjs
@@ -1,12 +1,22 @@
 const { join } = require("path");
+const { workspaceRoot } = require('@nx/devkit');
+const { createGlobPatternsForDependencies } = require("@nx/react/tailwind");
+const resolveConfig = require('tailwindcss/resolveConfig');
+const { inspect } = require('util');
 
-module.exports = {
-  presets: [join(__dirname, "../../..", "tailwind.preset.cjs")],
+const preset = require(join(workspaceRoot, 'tailwind.preset.cjs'));
+
+const depsGlobs = createGlobPatternsForDependencies(__dirname);
+
+const config = {
+  presets: [preset],
   content: [
-    join(__dirname, "src/**/*!(*.stories|*.spec|*.test).{tsx}"),
+    join(__dirname, "src/**/*!(*.stories|*.spec|*.test).{js,ts,jsx,tsx}"),
+    ...depsGlobs,
   ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
 };
+
+const resolved = resolveConfig(config);
+console.log('[tailwind] resolved content:\n' + inspect(resolved.content, { depth: null, colors: false, maxArrayLength: null }));
+console.log('[tailwind] resolved presets:\n' + inspect(resolved.presets, { depth: null, colors: false, maxArrayLength: null }));
+module.exports = config;
--- tailwind.preset.cjs
+++ tailwind.preset.cjs
@@ -1,9 +1,24 @@
-/* opt in libraries here*/
 module.exports = {
-  content: [
-    "libraries/(appframeworks|commons|mapping)/**/src/**/*!(*.spec|*.stories|*.test).{tsx}",
-    "libraries/collaborations/**/src/**/*!(*.spec|*.stories|*.test).{jsx,tsx}",
-  ],
-  theme: { extend: {} },
-  plugins: [],
+  theme: {
+     extend: {
+       screens: {
+         "xs": "320px",
+       },
+       spacing: {
+         // mobile first standard margins
+         'safe-top': 'max(2px, env(safe-area-inset-top))',
+         'safe-bottom': 'max(2px, env(safe-area-inset-bottom))',
+         'safe-left': 'max(6px, env(safe-area-inset-left))',
+         'safe-right': 'max(6px, env(safe-area-inset-right))',
+         // xs and above
+         'safe-top-xs': 'max(5px, env(safe-area-inset-top))',
+         'safe-bottom-xs': 'max(5px, env(safe-area-inset-bottom))',
+         'safe-left-xs': 'max(12px, env(safe-area-inset-left))',
+         'safe-right-xs': 'max(12px, env(safe-area-inset-right))',
+       }
+     },
+   },
+   corePlugins: {
+     preflight: false,
+   },
 };
