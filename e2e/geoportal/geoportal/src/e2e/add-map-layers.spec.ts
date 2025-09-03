67b698d7fa96a63caa3c03d7d9ff9eebbf14c4c4
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-09-03 09:33:49 +0200

    #324 simplify add map layer test

--- e2e/geoportal/geoportal/src/e2e/add-map-layers.spec.ts
+++ e2e/geoportal/geoportal/src/e2e/add-map-layers.spec.ts
@@ -50,50 +50,29 @@ test.describe("Geoportal add map layers", () => {
     const modal = page.locator(".ant-modal-content");
     await expect(modal).toBeVisible();
 
+    // Search inside modal
+    const searchInput = modal.locator("input");
+    await expect(searchInput).toBeVisible();
+    await searchInput.fill("orange");
+    await page.waitForTimeout(3300);
     const cards = page.locator('[data-test-id="card-layer-prev"]');
     await expect(cards.first()).toBeVisible();
 
-    const initialCardsCount = await cards.count();
-    expect(initialCardsCount).toBeGreaterThan(8);
-
-    // Favorites section should not exist yet
-    const favoritesSection = page.locator("#Favoriten");
-    await expect(favoritesSection).toHaveCount(0);
-
-    // Locate the "SPW2 Orange" card
-    const orangeCard = cards.filter({ hasText: "orange" });
-    await expect(orangeCard).toHaveCount(1);
-
     // Apply layer to map
-    const applyBtn = orangeCard.locator('[data-test-id="apply-layer-to-map"]');
+    const applyBtn = cards.locator('[data-test-id="apply-layer-to-map"]');
     await expect(applyBtn).toBeVisible();
     await applyBtn.click();
 
     // Wait for WMS response and tiles to show for that layer
-    await waitForWmsOrange(page, 8000);
+    // await waitForWmsOrange(page, 8000);
     await waitForTilesWithLayer(page, "spw2_orange", 8000);
 
-    // Search inside modal
-    const searchInput = modal.locator("input");
-    await expect(searchInput).toBeVisible();
-    await searchInput.fill("Expresskarte");
-
-    const filteredCount = await cards.count();
-    expect(filteredCount).toBeGreaterThan(2);
-
     // Clear search (click the "x" icon)
-    await page.locator(".anticon.anticon-close-circle").click();
-
-    const afterClearCount = await cards.count();
-    expect(afterClearCount).toBeGreaterThan(20);
+    await page.locator(".sticky > div > button").click();
 
     // Sanity: Leaflet has layers and tiles in the DOM
     const tileImgs = page.locator(".leaflet-layer div img");
     const tileCount = await tileImgs.count();
     expect(tileCount).toBeGreaterThan(0);
-
-    // Apply the same layer again (still interactable)
-    await expect(applyBtn).toBeVisible();
-    await applyBtn.click();
   });
 });
