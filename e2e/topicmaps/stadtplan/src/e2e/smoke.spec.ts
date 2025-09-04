08354f1e28adf648aa274e60ac3e2dc2b2b0d571
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-09-04 13:32:17 +0200

    #324 add mocks to city map

--- e2e/topicmaps/stadtplan/src/e2e/smoke.spec.ts
+++ e2e/topicmaps/stadtplan/src/e2e/smoke.spec.ts
@@ -1,8 +1,49 @@
 import { test } from "@playwright/test";
 import { runMapSmokeTest, setupSmokeTest } from "@carma-commons/e2e";
+import { setupAllMocks } from "@carma-commons/e2e";
 
 test.describe("stadtplan smoke test", () => {
-  test.beforeEach(async ({ page }) => {
+  test.beforeEach(async ({context, page }) => {
+    await setupAllMocks(context, ["bezirke", "quartiere", 'poi', 'kitas']);
+
+
+    await context.route("**/v2/data/**/poi.data.json*", (route) =>
+      route.fulfill({
+        status: 200,
+        contentType: "application/json",
+        body: JSON.stringify([
+          {
+            "id": 623,
+            "name": "Barmeniapark",
+            "stadt": "Wuppertal",
+            "info": "Parkanlage",
+            "geojson": {
+              "type": "Point",
+              "crs": {
+                "type": "name",
+                "properties": {
+                  "name": "EPSG:25832"
+                }
+              },
+              "coordinates": [
+                371420.692539062,
+                5679302.684169922
+              ]
+            },
+            "mainlocationtype": {
+              "id": 15,
+              "name": "Grünanlagen und Wälder",
+              "signatur": "Icon_Parkanlage_farbig.svg",
+              "lebenslagen": [
+                "Erholung",
+                "Freizeit"
+              ]
+            }
+          },
+        ]),
+      })
+    );
+
     await setupSmokeTest(page, "/", {
       navigationTimeout: 30000,
       waitForNetworkIdle: true,
