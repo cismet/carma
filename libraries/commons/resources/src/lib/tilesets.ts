1cde930711f8dd3e4726cfcee17de1936fdf7b1b
Friedrich Hartmann <friedrich.hartmann@cismet.de>
2025-09-18 11:09:30 +0200

    minor

--- libraries/commons/resources/src/lib/tilesets.ts
+++ libraries/commons/resources/src/lib/tilesets.ts
@@ -1,6 +1,6 @@
 import { type Cesium3DTileset } from "cesium";
-import { type PlainCartesian3 } from "@carma/types";
-import { type CesiumCustomChaderOptions } from "@carma/types";
+
+import type { CesiumCustomChaderOptions, PlainCartesian3 } from "@carma/types";
 
 export enum TilesetType {
   MESH = "mesh",
