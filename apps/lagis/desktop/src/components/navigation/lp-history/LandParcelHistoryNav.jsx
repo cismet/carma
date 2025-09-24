36abfbcc2542b5b1b9377d2a5650359dcce485f2
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-09-24 15:32:39 +0200

    #410 show lp history nav ui

--- apps/lagis/desktop/src/components/navigation/lp-history/LandParcelHistoryNav.jsx
+++ apps/lagis/desktop/src/components/navigation/lp-history/LandParcelHistoryNav.jsx
@@ -1,13 +1,30 @@
 import React from 'react'
 import { LeftCircleOutlined, RightCircleOutlined } from '@ant-design/icons';
+import { Dropdown, Button } from 'antd';
+import { useSelector } from 'react-redux';
+import { getPrevious, getNext } from '../../../store/slices/lpHistoryNav';
 
 function LandParcelHistoryNav() {
+  const previous = useSelector(getPrevious);
+  const next = useSelector(getNext);
+  const prevItems = previous.map((item) => ({
+    key: item.id,
+    label: item,
+  }));
+  const nextItems = next.map((item) => ({
+    key: item.id,
+    label: item,
+  }));
   return (
     <div className="flex gap-1 items-center">
       <LeftCircleOutlined />
-      <div>Previous</div>
+      <Dropdown menu={{ items: prevItems }} placement="bottom" open={true}>
+        <Button>Previous</Button>
+      </Dropdown>
       <RightCircleOutlined />
-      <div>Next</div>
+      <Dropdown menu={{ items: nextItems }} placement="bottom" open={true}>
+        <Button>Next</Button>
+      </Dropdown>
     </div>
   )
 }
