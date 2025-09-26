9116c47858e8175a7947177c72ad96258e77ea54
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-09-26 18:43:38 +0200

    #410 add simple draw control

--- libraries/commons/measurements/src/lib/lib-measurements.tsx
+++ libraries/commons/measurements/src/lib/lib-measurements.tsx
@@ -1,5 +1,12 @@
 import React from "react";
+import { useContext, useEffect } from "react";
+import { TopicMapContext } from "react-cismap/contexts/TopicMapContextProvider";
 
 export function LibMeasurements() {
+  const { routedMapRef } = useContext<typeof TopicMapContext>(TopicMapContext);
+  useEffect(() => {
+    const map = routedMapRef?.leafletMap?.leafletElement;
+    if (!map) return;
+  }, [routedMapRef]);
   return <div>Measurements</div>;
 }
--- playgrounds/measurements-playground/src/app/App.tsx
+++ playgrounds/measurements-playground/src/app/App.tsx
@@ -1,18 +1,39 @@
 import TopicMapComponent from "react-cismap/topicmaps/TopicMapComponent";
 import { suppressReactCismapErrors } from "@carma-commons/utils";
 import { LibMeasurements } from "@carma-commons/measurements";
+import { Control, ControlLayout } from "@carma-mapping/map-controls-layout";
+import { ZoomControl } from "@carma-mapping/components";
+import { ControlButtonStyler } from "@carma-mapping/map-controls-layout";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRuler } from "@fortawesome/free-solid-svg-icons";
 
 suppressReactCismapErrors();
 
 export function App() {
   return (
     <>
-      <LibMeasurements />
-
+      <ControlLayout ifStorybook={false}>
+        <Control position="topleft" order={10}>
+          <ZoomControl />
+        </Control>
+        <Control position="topleft" order={10}>
+          <ControlButtonStyler
+            onClick={() => {
+              console.log("xxx measurements");
+            }}
+          >
+            <FontAwesomeIcon icon={faRuler} />
+          </ControlButtonStyler>
+        </Control>
+      </ControlLayout>
       <TopicMapComponent
         gazetteerSearchComponent={<></>}
-        // infoBox={<GenericInfoBoxFromFeature />}
-      ></TopicMapComponent>
+        locatorControl={false}
+        fullScreenControl={false}
+        zoomControls={false}
+      >
+        <LibMeasurements />
+      </TopicMapComponent>
     </>
   );
 }
