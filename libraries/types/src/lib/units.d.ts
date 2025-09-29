867f1790b7db5d8e2ad0c1404e657b7d75876282
Friedrich Hartmann <friedrich.hartmann@cismet.de>
2025-09-29 21:44:53 +0200

    move zoom type out of base units

--- libraries/types/src/lib/units.d.ts
+++ libraries/types/src/lib/units.d.ts
@@ -8,7 +8,6 @@ declare const radiansSymbol: unique symbol;
 declare const metersSymbol: unique symbol;
 declare const ratioSymbol: unique symbol;
 declare const percentSymbol: unique symbol;
-declare const zoomSymbol: unique symbol;
 
 // ratio, like Percent but normalized to unit range
 // eg 0.05
@@ -19,20 +18,6 @@ export type Percent = NumericUnit<typeof percentSymbol>;
 export type Degrees = NumericUnit<typeof degreesSymbol>;
 export type Radians = NumericUnit<typeof radiansSymbol>;
 export type Meters = NumericUnit<typeof metersSymbol>;
-
-// XYZ SLIPPY MAP
-
-declare const tileSize256: 256;
-declare const tileSize512: 512;
-
-type zoomQuality<TileSize extends number> = TileSize & {
-  readonly tileSize: TileSize;
-};
-
-// Leaflet Zoom
-export type Zoom = zoomQuality<typeof tileSize256>;
-// MapLibre Zoom defined for 512x512 tiles
-export type Zoom512 = zoomQuality<typeof tileSize512>;
 
 // SCREEN UNITS
 
