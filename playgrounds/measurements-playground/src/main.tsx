9361c482f9ee018cb75815ac4fc2cc4f33021032
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-09-30 13:46:24 +0200

    #416 add redux provider

--- playgrounds/measurements-playground/src/main.tsx
+++ playgrounds/measurements-playground/src/main.tsx
@@ -1,12 +1,8 @@
 import { StrictMode } from "react";
 import * as ReactDOM from "react-dom/client";
-
+import { Provider } from "react-redux";
 import { TopicMapContextProvider } from "react-cismap/contexts/TopicMapContextProvider";
-
-import {
-  GazDataProvider,
-  SelectionProvider,
-} from "@carma-appframeworks/portals";
+import store from "./app/store";
 
 import { App } from "./app/App";
 
@@ -14,18 +10,22 @@ import "./styles.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "react-bootstrap-typeahead/css/Typeahead.css";
 import "leaflet/dist/leaflet.css";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+
+const persistor = persistStore(store);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <StrictMode>
-    <GazDataProvider>
-      <SelectionProvider>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
         <TopicMapContextProvider>
           <App />
         </TopicMapContextProvider>
-      </SelectionProvider>
-    </GazDataProvider>
+      </PersistGate>
+    </Provider>
   </StrictMode>
 );
