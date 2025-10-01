68338f6e091b40e4df5ea68709cd848b9759b6c2
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-10-01 17:56:43 +0200

    #410 add basic context methods

--- libraries/commons/measurements/src/lib/components/MapMeasurementsProvider.tsx
+++ libraries/commons/measurements/src/lib/components/MapMeasurementsProvider.tsx
@@ -1,13 +1,54 @@
 import { createContext, useContext, useEffect, useState } from "react";
 export type ActiveShape = null | number | string | any;
 export interface MapMeasurementsContextType {
+  shapes: any[];
+  setShapes: (shapes: any[]) => void;
   activeShape: ActiveShape;
   setActiveShape: (shape: ActiveShape) => void;
+  visibleShapes: any[];
+  setVisibleShapes: (shapes: any[]) => void;
+
+  showAll: boolean;
+  deleteAll: boolean;
+  drawingShape: boolean;
+  lastActiveShapeBeforeDrawing: null | any;
+  moveToShape: null | any;
+  updateShape: boolean;
+  mapMovingEnd: boolean;
+  updateTitleStatus: boolean;
+  setDrawingShape: (drawingShape: boolean) => void;
+  setShowAll: (showAll: boolean) => void;
+  setDeleteAll: (deleteAll: boolean) => void;
+  setMoveToShape: (moveToShape: any) => void;
+  setUpdateShape: (updateShape: boolean) => void;
+  setMapMovingEnd: (mapMovingEnd: boolean) => void;
+  setUpdateTitleStatus: (updateTitleStatus: boolean) => void;
+  setLastActiveShapeBeforeDrawing: (lastActiveShapeBeforeDrawing: any) => void;
 }
 export const MapMeasurementsContext = createContext<MapMeasurementsContextType>(
   {
+    shapes: [],
+    setShapes: (shapes: any[]) => {},
     activeShape: null,
     setActiveShape: (shape: ActiveShape) => {},
+    visibleShapes: [],
+    setVisibleShapes: (shapes: any[]) => {},
+    showAll: false,
+    deleteAll: false,
+    drawingShape: false,
+    lastActiveShapeBeforeDrawing: null,
+    moveToShape: null,
+    updateShape: false,
+    mapMovingEnd: false,
+    updateTitleStatus: false,
+    setDrawingShape: (drawingShape: boolean) => {},
+    setShowAll: (showAll: boolean) => {},
+    setDeleteAll: (deleteAll: boolean) => {},
+    setMoveToShape: (moveToShape: any) => {},
+    setUpdateShape: (updateShape: boolean) => {},
+    setMapMovingEnd: (mapMovingEnd: boolean) => {},
+    setUpdateTitleStatus: (updateTitleStatus: boolean) => {},
+    setLastActiveShapeBeforeDrawing: (lastActiveShapeBeforeDrawing: any) => {},
   }
 );
 
@@ -17,13 +58,49 @@ export const MapMeasurementsProvider = ({
   children: React.ReactNode;
 }) => {
   const [activeShape, setActiveShape] = useState<ActiveShape>(null);
+  const [shapes, setShapes] = useState<any[]>([]);
+  const [visibleShapes, setVisibleShapes] = useState<any[]>([]);
+  const [showAll, setShowAll] = useState(false);
+  const [deleteAll, setDeleteAll] = useState(false);
+  const [drawingShape, setDrawingShape] = useState(false);
+  const [lastActiveShapeBeforeDrawing, setLastActiveShapeBeforeDrawing] =
+    useState<any>(null);
+  const [moveToShape, setMoveToShape] = useState<any>(null);
+  const [updateShape, setUpdateShape] = useState(false);
+  const [mapMovingEnd, setMapMovingEnd] = useState(false);
+  const [updateTitleStatus, setUpdateTitleStatus] = useState(false);
 
-  //   useEffect(() => {
-  //     console.log("xxx activeShape", activeShape);
-  //   }, [activeShape]);
+  useEffect(() => {
+    console.log("setActiveShape", activeShape);
+  }, [activeShape]);
 
   return (
-    <MapMeasurementsContext.Provider value={{ activeShape, setActiveShape }}>
+    <MapMeasurementsContext.Provider
+      value={{
+        shapes,
+        setShapes,
+        activeShape,
+        setActiveShape,
+        visibleShapes,
+        setVisibleShapes,
+        showAll,
+        setShowAll,
+        deleteAll,
+        setDeleteAll,
+        drawingShape,
+        setDrawingShape,
+        lastActiveShapeBeforeDrawing,
+        setLastActiveShapeBeforeDrawing,
+        moveToShape,
+        setMoveToShape,
+        updateShape,
+        setUpdateShape,
+        mapMovingEnd,
+        setMapMovingEnd,
+        updateTitleStatus,
+        setUpdateTitleStatus,
+      }}
+    >
       {children}
     </MapMeasurementsContext.Provider>
   );
