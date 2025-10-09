d52f467c3caa8e1fc300be19e809aaa63e498370
Thorsten Hell <thorsten.hell@cismet.de>
2025-10-09 16:13:54 +0200

    responsive layout

--- apps/topicmaps/tz-baumbewirtschaftung/src/app/components/TitleControl.jsx
+++ apps/topicmaps/tz-baumbewirtschaftung/src/app/components/TitleControl.jsx
@@ -1,7 +1,8 @@
 import { useContext } from "react";
 import { ResponsiveTopicMapContext } from "react-cismap/contexts/ResponsiveTopicMapContextProvider";
 import { FeatureCollectionContext } from "react-cismap/contexts/FeatureCollectionContextProvider";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSignOut, faUser } from "@fortawesome/free-solid-svg-icons";
 // Simple TitleControl modeled after potenzialflächen
 // Expects: props.logout() and props.jwt
 const TitleControl = ({
@@ -18,23 +19,64 @@ const TitleControl = ({
     dateInfo = d.toLocaleString();
   }
 
-  let secondaryInfo = "";
+  let username = "";
   let actiontext = "anmelden";
 
   if (jwt) {
     try {
       const payload = JSON.parse(atob(jwt.split(".")[1]));
-      secondaryInfo = payload.preferred_username || payload.sub || "angemeldet";
+      username = payload.preferred_username || payload.sub || "angemeldet";
       actiontext = "abmelden";
     } catch (e) {
-      secondaryInfo = "angemeldet";
+      username = "angemeldet";
       actiontext = "abmelden";
     }
   }
 
-  const titleContent = (
+  // Determine if we're in mobile/narrow mode
+  const isNarrow = (windowSize?.width || 300) < 600;
+
+  const titleContent = isNarrow ? (
+    // Mobile layout: Title on first row, username + logout on second row
     <div>
-      <b>{title}</b> ({secondaryInfo + (dateInfo ? ", " + dateInfo : "")})
+      <div style={{ marginBottom: 4, textAlign: "center" }}>
+        <b>{title}</b>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <span
+          style={{
+            fontSize: "0.9em",
+            display: "flex",
+            alignItems: "center",
+            gap: "4px",
+          }}
+        >
+          <FontAwesomeIcon icon={faUser} />
+          {username}
+          {dateInfo && <span>, {dateInfo}</span>}
+        </span>
+        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+        <a
+          style={{ color: "#337ab7", cursor: "pointer", marginLeft: 10 }}
+          onClick={() => {
+            logout?.();
+          }}
+        >
+          {actiontext} <FontAwesomeIcon icon={faSignOut} />
+        </a>
+      </div>
+    </div>
+  ) : (
+    // Desktop layout: Everything on one row
+    <div>
+      <b>{title}</b> (<FontAwesomeIcon icon={faUser} />{" "}
+      {username + (dateInfo ? ", " + dateInfo : "")})
       <div style={{ float: "right", paddingRight: 10 }}>
         {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
         <a
@@ -43,7 +85,7 @@ const TitleControl = ({
             logout?.();
           }}
         >
-          {actiontext}
+          {actiontext} <FontAwesomeIcon icon={faSignOut} />
         </a>
       </div>
     </div>
@@ -54,7 +96,7 @@ const TitleControl = ({
       className="mode-container-switcher"
       style={{
         width: (windowSize?.width || 300) - 54 - 12 - 38 - 12 + "px",
-        height: "30px",
+        height: isNarrow ? "auto" : "30px",
         position: "absolute",
         left: 54,
         top: 12,
@@ -65,12 +107,12 @@ const TitleControl = ({
         <tr>
           <td
             style={{
-              textAlign: "center",
+              textAlign: isNarrow ? "left" : "center",
               verticalAlign: "middle",
               background: "#ffffff",
               color: "black",
               opacity: "0.9",
-              paddingLeft: "10px",
+              padding: isNarrow ? "6px 10px" : "0 0 0 10px",
             }}
           >
             {titleContent}
