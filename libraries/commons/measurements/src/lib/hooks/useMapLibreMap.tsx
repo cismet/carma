ed65899cec956ab45acacb7e921766abea4c6f61
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-10-16 19:45:27 +0200

    #416 use map libre hook

--- libraries/commons/measurements/src/lib/hooks/useMapLibreMap.tsx
+++ libraries/commons/measurements/src/lib/hooks/useMapLibreMap.tsx
@@ -4,23 +4,25 @@ export const useMapLibreMap = () => {
   const [maplibreMap, setMaplibreMap] = useState<any>(null);
   useEffect(() => {
     // Highlight points (black for serious mode)
-    maplibreMap.addLayer({
-      id: "highlight-point-black",
-      type: "circle",
-      source: "highlight",
-      filter: [
-        "all",
-        ["==", ["geometry-type"], "Point"],
-        ["==", ["get", "black"], true],
-      ],
-      paint: {
-        "circle-radius": 8,
-        "circle-color": "#000000",
-        "circle-opacity": 0.8,
-        "circle-stroke-width": 2,
-        "circle-stroke-color": "#ffffff",
-      },
-    });
+    if (maplibreMap) {
+      maplibreMap.addLayer({
+        id: "highlight-point-black",
+        type: "circle",
+        source: "highlight",
+        filter: [
+          "all",
+          ["==", ["geometry-type"], "Point"],
+          ["==", ["get", "black"], true],
+        ],
+        paint: {
+          "circle-radius": 8,
+          "circle-color": "#000000",
+          "circle-opacity": 0.8,
+          "circle-stroke-width": 2,
+          "circle-stroke-color": "#ffffff",
+        },
+      });
+    }
   }, [maplibreMap]);
   return {
     maplibreMap,
