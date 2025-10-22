107fcd55ab1832782c1fef7d9c8dcef90c774cd6
David Glogaza <david.glogaza40@googlemail.com>
2025-10-22 22:10:39 +0200

    fix link in geoportal secondary info modals

--- apps/geoportal/src/app/components/feature-info/FeatureInfoBox.tsx
+++ apps/geoportal/src/app/components/feature-info/FeatureInfoBox.tsx
@@ -286,7 +286,10 @@ const FeatureInfoBox = ({ pos }: InfoBoxProps) => {
             properties: selectedFeature.properties.wmsProps,
           }}
           versionString={getApplicationVersion(versionData)}
-          Footer={genericSecondaryInfoFooterFactory({ skipTeilzwilling: true })}
+          Footer={genericSecondaryInfoFooterFactory({
+            skipTeilzwilling: true,
+            isTopicMap: false,
+          })}
           skipTeilzwilling={true}
         />
       )}
