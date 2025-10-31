2779896a2a8b1719f4b60f4f1e476b6839a9840b
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-10-31 15:10:56 +0100

    #447 move error boundary to layout

--- apps/lagis/desktop/src/pages/AppLayout.jsx
+++ apps/lagis/desktop/src/pages/AppLayout.jsx
@@ -8,6 +8,9 @@ import useUrlSyncGemarkunFlurFlurstueckHook from "../hooks/useUrlSyncGemarkunFlu
 import { TAILWIND_CLASSNAMES_FULLSCREEN_FIXED } from "@carma-commons/utils";
 import { MobileWarningMessage } from "@carma-mapping/components";
 import { mobileInfo } from "@carma-collab/wuppertal/lagis-desktop";
+import { ErrorBoundary } from "react-error-boundary";
+import AppErrorFallback from "../components/AppErrorFallback";
+
 const AppLayout = () => {
   const [urlParams, setUrlParams] = useSearchParams();
   const [parametersForLink, setParametersForLink] = useState();
@@ -28,55 +31,59 @@ const AppLayout = () => {
   }, [urlParams, parametersForLink]);
   return (
     <>
-      <div
-        style={{
-          background: "#F1F1F1",
-        }}
-        // className="w-full overflow-clip adaptive-full-screen"
-        className={`${TAILWIND_CLASSNAMES_FULLSCREEN_FIXED}`}
-      >
+      <ErrorBoundary FallbackComponent={AppErrorFallback}>
         <div
-          // className="flex h-[calc(100%-16px)]"
-          className="flex h-full"
-          style={{ paddingRight: "16px" }}
+          style={{
+            background: "#F1F1F1",
+          }}
+          // className="w-full overflow-clip adaptive-full-screen"
+          className={`${TAILWIND_CLASSNAMES_FULLSCREEN_FIXED}`}
         >
-          <div className="h-full">
-            <SidebarMenu parametersForLink={parametersForLink} />
-          </div>
           <div
-            className="flex-1 w-[calc(100%-341px)] flex flex-col justify-between gap-2"
-            style={{ paddingLeft: "16px" }}
+            // className="flex h-[calc(100%-16px)]"
+            className="flex h-full"
+            style={{ paddingRight: "16px" }}
           >
-            <div
-              className="h-[32px]"
-              style={{
-                // paddingTop: "8px"
-                paddingTop: "max(8px, env(safe-area-inset-top))",
-              }}
-            >
-              <UserBar />
+            <div className="h-full">
+              <SidebarMenu parametersForLink={parametersForLink} />
             </div>
-
             <div
-              className="h-[calc(100%-50px)] w-full"
-              style={{ paddingBottom: "max(8px, env(safe-area-inset-bottom))" }}
+              className="flex-1 w-[calc(100%-341px)] flex flex-col justify-between gap-2"
+              style={{ paddingLeft: "16px" }}
             >
-              <Outlet />
-            </div>
+              <div
+                className="h-[32px]"
+                style={{
+                  // paddingTop: "8px"
+                  paddingTop: "max(8px, env(safe-area-inset-top))",
+                }}
+              >
+                <UserBar />
+              </div>
 
-            {/* <div className="h-[calc(1%-10px)]">
+              <div
+                className="h-[calc(100%-50px)] w-full"
+                style={{
+                  paddingBottom: "max(8px, env(safe-area-inset-bottom))",
+                }}
+              >
+                <Outlet />
+              </div>
+
+              {/* <div className="h-[calc(1%-10px)]">
             <FooterSection />
           </div> */}
+            </div>
           </div>
         </div>
-      </div>
-      <MobileWarningMessage
-        headerText={mobileInfo.headerText}
-        bodyText={mobileInfo.bodyText}
-        confirmButtonText={mobileInfo.confirmButtonText}
-        isHardMode={mobileInfo.isHardMode}
-        messageWidth={993}
-      />
+        <MobileWarningMessage
+          headerText={mobileInfo.headerText}
+          bodyText={mobileInfo.bodyText}
+          confirmButtonText={mobileInfo.confirmButtonText}
+          isHardMode={mobileInfo.isHardMode}
+          messageWidth={993}
+        />
+      </ErrorBoundary>
     </>
   );
 };
