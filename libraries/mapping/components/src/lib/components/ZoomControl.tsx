ce973b4d2eebd28b47072cbcc178df987fcdaa5d
David Glogaza <david.glogaza40@googlemail.com>
2025-11-24 19:36:48 +0100

    add tooltip

--- libraries/mapping/components/src/lib/components/ZoomControl.tsx
+++ libraries/mapping/components/src/lib/components/ZoomControl.tsx
@@ -1,6 +1,7 @@
 import { ControlButtonStyler } from "@carma-mapping/map-controls-layout";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMinus, faPlus } from "@fortawesome/free-solid-svg-icons";
+import { Tooltip } from "antd";
 import {
   useLeafletZoomControls,
   useLibreZoomControls,
@@ -20,22 +21,26 @@ export const ZoomControl = ({
 
   return (
     <div data-test-id="zoom-control" className="flex flex-col">
-      <ControlButtonStyler
-        onClick={mapEngine === "leaflet" ? zoomInLeaflet : zoomInLibre}
-        className="!border-b-0 !rounded-b-none font-bold !z-[9999999]"
-        dataTestId="zoom-in-control"
-        title="Vergrößern"
-      >
-        <FontAwesomeIcon icon={faPlus} className="text-base" />
-      </ControlButtonStyler>
-      <ControlButtonStyler
-        onClick={mapEngine === "leaflet" ? zoomOutLeaflet : zoomOutLibre}
-        className="!rounded-t-none !border-t-[1px]"
-        dataTestId="zoom-out-control"
-        title="Verkleinern"
-      >
-        <FontAwesomeIcon icon={faMinus} className="text-base" />
-      </ControlButtonStyler>
+      <Tooltip title="Maßstab vergrößern (Zoom in)" placement="right">
+        <ControlButtonStyler
+          onClick={mapEngine === "leaflet" ? zoomInLeaflet : zoomInLibre}
+          className="!border-b-0 !rounded-b-none font-bold !z-[9999999]"
+          dataTestId="zoom-in-control"
+          title="Vergrößern"
+        >
+          <FontAwesomeIcon icon={faPlus} className="text-base" />
+        </ControlButtonStyler>
+      </Tooltip>
+      <Tooltip title="Maßstab verkleinern (Zoom out)" placement="right">
+        <ControlButtonStyler
+          onClick={mapEngine === "leaflet" ? zoomOutLeaflet : zoomOutLibre}
+          className="!rounded-t-none !border-t-[1px]"
+          dataTestId="zoom-out-control"
+          title="Verkleinern"
+        >
+          <FontAwesomeIcon icon={faMinus} className="text-base" />
+        </ControlButtonStyler>
+      </Tooltip>
     </div>
   );
 };
