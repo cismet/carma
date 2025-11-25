607078d405c7b86c9d222c594799d8f58591e7eb
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-11-25 14:22:26 +0100

    #471 add different reports

--- e2e/topicmaps/hitzeinderstadt/playwright.config.ts
+++ e2e/topicmaps/hitzeinderstadt/playwright.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: "github",
+  reporter: process.env.CI ? "github" : "html",
   use: {
     baseURL: "http://localhost:4222",
     trace: "on-first-retry",
