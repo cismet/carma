bbba3b3b2a10d17d7c9d1d2cbe6c1f1848a16097
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-11-25 11:08:50 +0100

    #471 add vorhabenkarte e2e tests

---
+++ e2e/topicmaps/vorhabenkarte/playwright.config.ts
@@ -0,0 +1,33 @@
+import { defineConfig } from "@playwright/test";
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ */
+export default defineConfig({
+  testDir: "./src/e2e",
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: "github",
+  use: {
+    baseURL: "http://localhost:4222",
+    trace: "on-first-retry",
+    screenshot: "only-on-failure",
+    channel: process.env.PW_CHANNEL || undefined,
+  },
+  projects: [
+    {
+      name: "chrome",
+      use: {
+        channel: process.env.PW_CHANNEL || undefined,
+      },
+    },
+  ],
+  webServer: {
+    command: "npx nx serve vorhabenkarte --port=4222",
+    url: "http://localhost:4222",
+    reuseExistingServer: false,
+    timeout: 60_000,
+  },
+});
