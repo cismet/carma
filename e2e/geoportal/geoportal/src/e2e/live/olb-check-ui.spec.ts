8e1386990f36d8f80bd8159bac83e9c0bcf13b2d
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-11-27 17:25:18 +0100

    #471 run olb ui test with mock

--- e2e/geoportal/geoportal/src/e2e/live/olb-check-ui.spec.ts
+++ e2e/geoportal/geoportal/src/e2e/live/olb-check-ui.spec.ts
@@ -10,7 +10,7 @@ test.describe("geoportal layer group icon", () => {
     // test.slow();
     await setupAllMocks(context);
     await mockGeoportalServices(context);
-    // await mockObliqueServices(context);
+    await mockObliqueServices(context);
     await page.goto(
       "/#/?lat=51.2527066&lng=7.2051585&h=925.81&heading=324.58&pitch=311.88&fov=40.76&m=1&ff=oblq&is3d=1"
     );
