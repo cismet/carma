194b1cfe23f93180b8d59cf192dfe94f53691bd8
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-12-04 11:18:19 +0100

    #463 create draw polygon tests

---
+++ e2e/geoportal/geoportal/src/e2e/other/draw-polygon.spec.ts
@@ -0,0 +1,40 @@
+import { setupAllMocks, mockGeoportalServices } from "@carma-commons/e2e";
+import { test, expect } from "@playwright/test";
+
+test.describe("geoportal measurements", () => {
+  test.beforeEach(async ({ context, page }) => {
+    await setupAllMocks(context);
+    await mockGeoportalServices(context);
+
+    await page.goto("/#/?lat=51.272538&lng=7.2000334&zoom=17");
+    // await page.waitForLoadState("networkidle");
+  });
+
+  test("measurements", async ({ page }) => {
+    const map = page.locator("#routedMap");
+
+    // Open measurement UI
+    await expect(
+      page.locator('[data-test-id="measurement-control"]')
+    ).toBeVisible();
+    await page.locator('[data-test-id="measurement-control"]').click();
+    await expect(
+      page.locator('[data-test-id="empty-measurement-info"]')
+    ).toBeVisible();
+
+    // Ensure map is ready
+    await expect(map).toBeVisible();
+
+    // ---- Polygon ----
+    await map.click({ modifiers: ["Alt"], position: { x: 300, y: 200 } });
+    await map.click({ modifiers: ["Alt"], position: { x: 400, y: 200 } });
+    await map.click({ modifiers: ["Alt"], position: { x: 400, y: -100 } });
+    await map.click({ modifiers: ["Alt"], position: { x: 300, y: 100 } });
+    await map.click({ position: { x: 300, y: 200 } });
+
+    await expect(page.getByText("Fläche", { exact: true })).toBeVisible();
+    await expect(
+      page.getByRole("button", { name: "delete-measurement-btn" })
+    ).toBeVisible();
+  });
+});
