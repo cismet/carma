269d14c00bacb6e89d61a6ed20c1ca3beafd6c4f
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-12-12 11:02:26 +0100

    #485 change potential areas e2e settings

--- e2e/wunda/potenzialflaechen-online/playwright.config.ts
+++ e2e/wunda/potenzialflaechen-online/playwright.config.ts
@@ -9,22 +9,24 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: "github",
+  reporter: process.env.CI ? "github" : "html",
   use: {
     baseURL: "http://localhost:4222",
     serviceWorkers: "block",
     viewport: null,
     trace: "on-first-retry",
     screenshot: "only-on-failure",
-    navigationTimeout: 15_000,
-    actionTimeout: 10_000,
+    // navigationTimeout: 15_000,
+    // actionTimeout: 10_000,
+    testIdAttribute: "data-test-id",
   },
   projects: [
     {
       name: "chrome",
       use: {
-        navigationTimeout: 15_000,
-        actionTimeout: 10_000,
+        // navigationTimeout: 15_000,
+        // actionTimeout: 10_000,
+        // Use PW_CHANNEL to select system Chrome; otherwise bundled Chromium
         channel: process.env.PW_CHANNEL || undefined,
       },
     },
