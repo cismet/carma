ab6034d03d3a208c6b43fe0b24e32bceaad42543
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-12-16 17:29:24 +0100

    #493 improve save bauart function

--- apps/belis/desktop/src/constants/belis.ts
+++ apps/belis/desktop/src/constants/belis.ts
@@ -6,7 +6,9 @@ export const DOMAIN = "BELIS2";
 export const ENDPOINT = REST_SERVICE + `/graphql/` + DOMAIN + "/execute";
 export const SAVE_ENDPOINT =
   REST_SERVICE +
-  "/actions/WUNDA_BLAU.SaveObject/tasks?resultingInstanceType=result";
+  "/actions/" +
+  DOMAIN +
+  ".SaveObject/tasks?resultingInstanceType=result";
 
 export const jwtTestQuery = gql`
   ${queries.jwtTestQuery}
--- apps/belis/desktop/src/helper/apiMethods.ts
+++ apps/belis/desktop/src/helper/apiMethods.ts
@@ -6,24 +6,32 @@ export const savebauart = async (jwt: string) => {
       bezeichnung: "Test Schaltschrank",
       id: 1,
     };
+
     const formData = new FormData();
+    const taskparams = JSON.stringify({
+      parameters: {
+        className: "bauart",
+        data: JSON.stringify(dataToSave),
+      },
+    });
+
     formData.append(
       "taskparams",
-      JSON.stringify({
-        parameters: {
-          className: "gp_entdecken",
-          data: JSON.stringify(dataToSave),
-        },
-      })
+      new Blob([taskparams], { type: "application/json" }),
+      "taskparams"
     );
-    const response = await fetch(SAVE_ENDPOINT, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${jwt}`,
-      },
-      body: formData,
-    });
+
+    const response = await fetch(
+      SAVE_ENDPOINT + "?resultingInstanceType=result",
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${jwt}`,
+        },
+        body: formData,
+      }
+    );
+
     console.log(response);
   } catch (error) {
     console.log(error);
