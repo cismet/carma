873385b0ae930aa95b1ee181d12f57a26b9e90d6
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-12-16 14:44:30 +0100

    #493 add example data

---
+++ apps/belis/desktop/src/helper/dataExample.ts
@@ -0,0 +1,254 @@
+export const bauart = {
+  data: {
+    bauart: [
+      {
+        bezeichnung: "Schaltschrank",
+        id: 1,
+      },
+      {
+        bezeichnung: "Blechkasten",
+        id: 3,
+      },
+      {
+        bezeichnung: "Schaltraum",
+        id: 4,
+      },
+      {
+        bezeichnung: "RS-Gehäuse Z6",
+        id: 6,
+      },
+      {
+        bezeichnung: "RS-Kasten alt",
+        id: 7,
+      },
+      {
+        bezeichnung: "Schleifenkasten",
+        id: 8,
+      },
+      {
+        bezeichnung: "Kunststoffschaltschrank",
+        id: 2,
+      },
+    ],
+  },
+};
+
+export const straßenschluessel = {
+  data: {
+    tkey_strassenschluessel: [
+      {
+        id: 1,
+        pk: "00000",
+        strasse: "AUSSERHALB",
+      },
+      {
+        id: 2,
+        pk: "00001",
+        strasse: "ACHENBACHSTR",
+      },
+      {
+        id: 3,
+        pk: "00002",
+        strasse: "ACHENBACHTREPPE",
+      },
+      {
+        id: 4,
+        pk: "00003",
+        strasse: "ACKERSTR",
+      },
+      {
+        id: 5,
+        pk: "00004",
+        strasse: "ADALBERT-STIFTER-WEG",
+      },
+      {
+        id: 6,
+        pk: "00005",
+        strasse: "ADAMSBUSCH",
+      },
+      {
+        id: 7,
+        pk: "00006",
+        strasse: "ADERSSTR",
+      },
+      {
+        id: 8,
+        pk: "00007",
+        strasse: "ADLERSTR",
+      },
+      {
+        id: 9,
+        pk: "00008",
+        strasse: "ADOLFSTR",
+      },
+      {
+        id: 10,
+        pk: "00009",
+        strasse: "ADOLF-VORWERK-STR",
+      },
+      {
+        id: 11,
+        pk: "00010",
+        strasse: "AHORNSTR",
+      },
+      {
+        id: 12,
+        pk: "00011",
+        strasse: "AHRSTR",
+      },
+      {
+        id: 13,
+        pk: "00012",
+        strasse: "AKAZIENSTR",
+      },
+      {
+        id: 14,
+        pk: "00013",
+        strasse: "ALARICHSTR",
+      },
+      {
+        id: 15,
+        pk: "00014",
+        strasse: "ALBENSTR",
+      },
+    ],
+  },
+};
+
+export const infobausteinTemplate = {
+  data: {
+    infobaustein_template: [
+      {
+        schluessel: "2",
+        id: 3,
+        bezeichnung: "Angaben Unfall",
+        ar_bausteineArray: [
+          {
+            infobaustein: {
+              bezeichnung: "Unfalldatum",
+              id: 18,
+              pflichtfeld: null,
+              schluessel: "1",
+              wert: null,
+            },
+          },
+          {
+            infobaustein: {
+              bezeichnung: "Unfallzeit",
+              id: 19,
+              pflichtfeld: null,
+              schluessel: "2",
+              wert: null,
+            },
+          },
+          {
+            infobaustein: {
+              bezeichnung: "KFZ-Zeichen",
+              id: 20,
+              pflichtfeld: null,
+              schluessel: "3",
+              wert: null,
+            },
+          },
+          {
+            infobaustein: {
+              bezeichnung: "Aktenzeichen",
+              id: 21,
+              pflichtfeld: null,
+              schluessel: "4",
+              wert: null,
+            },
+          },
+          {
+            infobaustein: {
+              bezeichnung: "Fahrer",
+              id: 22,
+              pflichtfeld: null,
+              schluessel: "5",
+              wert: null,
+            },
+          },
+          {
+            infobaustein: {
+              bezeichnung: "Anschrift Fahrer",
+              id: 23,
+              pflichtfeld: null,
+              schluessel: "6",
+              wert: null,
+            },
+          },
+          {
+            infobaustein: {
+              bezeichnung: "Halter",
+              id: 24,
+              pflichtfeld: null,
+              schluessel: "7",
+              wert: null,
+            },
+          },
+          {
+            infobaustein: {
+              bezeichnung: "Anschrift Halter",
+              id: 25,
+              pflichtfeld: null,
+              schluessel: "8",
+              wert: null,
+            },
+          },
+          {
+            infobaustein: {
+              bezeichnung: "Rufnummer",
+              id: 26,
+              pflichtfeld: null,
+              schluessel: "9",
+              wert: null,
+            },
+          },
+        ],
+      },
+      {
+        schluessel: "1",
+        id: 4,
+        bezeichnung: "Angaben Störung",
+        ar_bausteineArray: [
+          {
+            infobaustein: {
+              bezeichnung: "Name",
+              id: 449,
+              pflichtfeld: null,
+              schluessel: "1",
+              wert: null,
+            },
+          },
+          {
+            infobaustein: {
+              bezeichnung: "Tel. Nr",
+              id: 450,
+              pflichtfeld: null,
+              schluessel: "2",
+              wert: null,
+            },
+          },
+          {
+            infobaustein: {
+              bezeichnung: "E-Mail",
+              id: 451,
+              pflichtfeld: null,
+              schluessel: "3",
+              wert: null,
+            },
+          },
+          {
+            infobaustein: {
+              bezeichnung: "Herkunft",
+              id: 452,
+              pflichtfeld: null,
+              schluessel: "4",
+              wert: null,
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
