021180a1cb1fc6a08ff264aedca0671f21b658b9
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-12-17 14:08:50 +0100

    clean vite cache in pw config

--- apps/geoportal/src/app/components/GeoportalMap/controls/MapWrapper.tsx
+++ apps/geoportal/src/app/components/GeoportalMap/controls/MapWrapper.tsx
@@ -486,7 +486,7 @@ const MapWrapper = () => {
           <Control position="topcenter" order={10}>
             {isLeaflet && <LayerWrapper />}
           </Control>
-          {/* <Control position="bottomleft" order={10}>
+          <Control position="bottomleft" order={10}>
             <div ref={tourRefLabels.gazetteer} className={`h-full w-full`}>
               <LibFuzzySearch
                 gazData={gazData}
@@ -500,7 +500,7 @@ const MapWrapper = () => {
                 selection={configSelection}
               />
             </div>
-          </Control> */}
+          </Control>
         </div>
       )}
       <ControlLayoutCanvas>
