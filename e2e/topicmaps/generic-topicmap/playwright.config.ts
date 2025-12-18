226ae28f1c50c9f9ff376fd97a4af0bcd0a2337c
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-12-18 11:01:43 +0100

    #488 add output directory in all test projects

--- e2e/topicmaps/generic-topicmap/playwright.config.ts
+++ e2e/topicmaps/generic-topicmap/playwright.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "@playwright/test";
  */
 export default defineConfig({
   testDir: "./src/e2e",
+  outputDir: "./test-results",
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
