33f3b23c73afefbe23a510e99ffdd9935a86c889
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-12-19 11:51:24 +0100

    #500 check load map layers  features

--- e2e/geoportal/geoportal/src/e2e/other/save-map-to-favorite.spec.ts
+++ e2e/geoportal/geoportal/src/e2e/other/save-map-to-favorite.spec.ts
@@ -14,10 +14,73 @@ test.describe("Geoportal - save map to favorite", () => {
   test("Save Map dialog — save map with layers to Favorites", async ({
     page,
   }) => {
-    const addLayersBtn = page.locator(
-      '[data-test-id="kartenebenen-hinzufügen-btn"]'
-    );
+    // Check map layers and save map button are visible
+    const saveMapBtn = page.getByTestId("speichern-btn");
+    await expect(saveMapBtn).toBeVisible();
+    await saveMapBtn.click();
+    const addLayersBtn = page.getByTestId("kartenebenen-hinzufügen-btn");
     await expect(addLayersBtn).toBeVisible();
-    await addLayersBtn.click();
+    const layerTagPlayground = page.getByRole("button", {
+      name: "Kinderspielplätze",
+    });
+    await expect(layerTagPlayground).toBeVisible();
+    const layerTagKindergarten = page.getByRole("button", {
+      name: "Kindertagesstätten",
+    });
+    await expect(layerTagKindergarten).toBeVisible();
+
+    // Check dialog content
+    const dialogTitle = page.getByRole("heading", { name: "Karte speichern" });
+    await expect(dialogTitle).toBeVisible();
+    const titleInput = page.getByRole("textbox", { name: "Titel" });
+    await titleInput.fill("Kita title");
+    const contentInput = page.getByRole("textbox", { name: "Inhalt" });
+    await contentInput.fill("Kita content");
+    const saveFavoriteBtn = page.getByRole("button", {
+      name: "Als Favorit speichern",
+    });
+    await saveFavoriteBtn.click();
+
+    await expect(dialogTitle).not.toBeVisible();
+
+    // const successMessage = page.getByText(
+    //   "Karte a wurde erfolgreich gespeichert."
+    // );
+    // await expect(successMessage).toBeVisible();
+
+    // close layers tags
+    page.locator('[id="removeLayerButton-wuppPOI\\:poi_ksp"]').click();
+    await expect(layerTagPlayground).not.toBeVisible();
+    page.locator('[id="removeLayerButton-wuppPOI\\:poi_kita"]').click();
+    await expect(layerTagKindergarten).not.toBeVisible();
+    const messageAlert = page
+      .getByRole("img", { name: "check-circle" })
+      .locator("path");
+    await expect(messageAlert).toBeVisible();
+    await expect(messageAlert).not.toBeVisible();
+
+    // Go to favorites
+    await expect(addLayersBtn).toBeVisible();
+    addLayersBtn.click();
+    const favoriteBtn = page.getByText("Favoriten");
+    await expect(favoriteBtn).toBeVisible();
+    favoriteBtn.click();
+    const favoriteTitle = page.getByRole("heading", { name: "Kita title" });
+    await expect(favoriteTitle).toBeVisible();
+    const loadBtn = page.getByTestId("card-layer-prev").getByRole("button");
+
+    await expect(loadBtn).toBeVisible();
+    // Load favorite map
+    await loadBtn.click();
+    await expect(messageAlert).toBeVisible();
+    await expect(messageAlert).not.toBeVisible();
+    page.getByTestId("card-layer-prev").getByRole("button");
+    const closeDialogBtn = page.getByRole("dialog").getByRole("button").nth(1);
+    await expect(closeDialogBtn).toBeVisible();
+    await closeDialogBtn.click();
+    await expect(favoriteTitle).not.toBeVisible();
+
+    await expect(layerTagPlayground).toBeVisible();
+    await expect(layerTagKindergarten).toBeVisible();
   });
 });
