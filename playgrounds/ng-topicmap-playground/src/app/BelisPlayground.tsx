ec73a682450452bf8768bb43f83ccc455b4c5fd6
Thorsten Hell <thorsten.hell@cismet.de>
2026-01-24 10:57:44 +0100

    update ng-topicmap-playground to use CarmaMap from core

--- playgrounds/ng-topicmap-playground/src/app/BelisPlayground.tsx
+++ playgrounds/ng-topicmap-playground/src/app/BelisPlayground.tsx
@@ -1,4 +1,4 @@
-import { CarmaMap } from "@carma-appframeworks/portals";
+import { CarmaMap } from "@carma-mapping/core";
 import { CustomCard } from "./CustomCard";
 import { BelisSwitch } from "@carma-appframeworks/belis";
 
