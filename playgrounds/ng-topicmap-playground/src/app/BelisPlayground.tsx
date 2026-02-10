e17513d089702532c48e4ce753050dae56577a1b
Thorsten Hell <thorsten.hell@cismet.de>
2026-02-10 14:04:09 +0100

    add commented-out working LibreLayers for testing

--- playgrounds/ng-topicmap-playground/src/app/BelisPlayground.tsx
+++ playgrounds/ng-topicmap-playground/src/app/BelisPlayground.tsx
@@ -610,3 +610,109 @@ const BelisPlayground = () => {
 };
 
 export default BelisPlayground;
+
+//Working LibreLayers
+
+// libreLayers={[
+//         // --- Background layers for testing ---
+//         // RVR
+//         {
+//           type: "wmts",
+//           url: "https://geodaten.metropoleruhr.de/spw2/service",
+//           layers: "spw2_light",
+//           version: "1.3.0",
+//           transparent: true,
+//           format: "image/png",
+//           tileSize: 512,
+//           maxZoom: 26,
+//         },
+// Liegenschaftskarte (grau)
+// {
+//   type: "wmts",
+//   url: "http://rpr.s10222.wuppertal-intra.de/forwardingTo/s10221/7098/alkis/services",
+//   //url: "http://s10221.wuppertal-intra.de:7098/alkis/services",
+//   layers: "alkomgw",
+//   styles: "default",
+//   version: "1.1.1",
+//   tileSize: 256,
+//   maxZoom: 26,
+//   transparent: true,
+//   format: "image/png",
+//   opacity: 0.5,
+// },
+// // Liegenschaftskarte (bunt)
+// {
+//   type: "wmts",
+//   url: "http://rpr.s10222.wuppertal-intra.de/forwardingTo/s10221/7098/alkis/services",
+//   //url: "http://s10221.wuppertal-intra.de:7098/alkis/services",
+//   layers: "alkomf",
+//   styles: "default",
+//   version: "1.1.1",
+//   tileSize: 256,
+//   transparent: true,
+//   format: "image/png",
+//   opacity: 0.5,
+// },
+// // True Orthofoto
+// {
+//   type: "wms",
+//   url: "https://geo.udsp.wuppertal.de/geoserver-cloud/ows",
+//   layers: "GIS-102:trueortho2024",
+//   tileSize: 256,
+//   transparent: true,
+//   maxZoom: 26,
+//   format: "image/png",
+// },
+// Luftbildkarte (SPW2 light Grundriss)
+// {
+//   type: "wmts",
+//   url: "https://geodaten.metropoleruhr.de/spw2/service",
+//   layers: "spw2_light_grundriss",
+//   version: "1.3.0",
+//   transparent: true,
+//   format: "image/png",
+//   maxZoom: 26,
+// },
+// // Luftbildkarte (True Ortho underlay)
+// {
+//   type: "wms",
+//   url: "https://geo.udsp.wuppertal.de/geoserver-cloud/ows",
+//   layers: "GIS-102:trueortho2024",
+//   tileSize: 256,
+//   transparent: true,
+//   maxZoom: 26,
+//   format: "image/png",
+// },
+// // Luftbildkarte (DOP Overlay)
+// {
+//   type: "wmts",
+//   url: "https://geodaten.metropoleruhr.de/dop/dop_overlay?language=ger",
+//   layers: "dop_overlay",
+//   version: "1.3.0",
+//   format: "image/png",
+//   transparent: true,
+//   maxZoom: 26,
+// },
+// Stadtplan (grau)
+// {
+//   type: "vector",
+//   name: "Stadtplan grau",
+//   style:
+//     "https://sgx.geodatenzentrum.de/gdz_basemapde_vektor/styles/bm_web_gry.json",
+//   opacity: 0.5,
+// },
+// Stadtplan (bunt)
+// {
+//   type: "vector",
+//   name: "Stadtplan bunt",
+//   style:
+//     "https://sgx.geodatenzentrum.de/gdz_basemapde_vektor/styles/bm_web_top.json",
+//   opacity: 0.5,
+// },
+//   {
+//     type: "vector",
+//     name: "Leuchten",
+//     style: "https://tiles.cismet.de/belis/style.json",
+//     opacity: 1,
+//   },
+// ]}
