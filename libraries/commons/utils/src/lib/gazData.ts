2f45df561bd09a50e07b1547b4510f763def5b83
David Glogaza <david.glogaza40@googlemail.com>
2026-03-06 13:14:27 +0100

    move gazetteer hit trigger from cismap to carma & add padding property

--- libraries/commons/utils/src/lib/gazData.ts
+++ libraries/commons/utils/src/lib/gazData.ts
@@ -1,5 +1,11 @@
+import proj4 from "proj4";
+import objectAssign from "object-assign";
+import bboxCreator from "@turf/bbox";
+import * as turfHelpers from "@turf/helpers";
 import { ENDPOINT } from "@carma-commons/resources";
+import { proj4crs25832def } from "@carma-commons/geo/proj";
 import { md5FetchText } from "./fetching/fetching";
+import { convertBBox2Bounds } from "./proj4helpers";
 
 export type GazDataSourceConfig = {
   topic: ENDPOINT;
@@ -161,4 +167,104 @@ export const getGazData = async (
 
   setGazData?.(gazData);
   return gazData;
+};
+
+export const builtInGazetteerHitTrigger = ({
+  hit,
+  leafletElement,
+  referenceSystem,
+  referenceSystemDefinition = proj4crs25832def,
+  setGazetteerHit,
+  setOverlayFeature,
+  furtherGazeteerHitTrigger,
+  suppressMarker = false,
+  padding,
+}: {
+  hit;
+  leafletElement;
+  referenceSystem;
+  referenceSystemDefinition?: string;
+  setGazetteerHit;
+  setOverlayFeature;
+  furtherGazeteerHitTrigger?;
+  suppressMarker?: boolean;
+  padding?: [number, number];
+}) => {
+  if (
+    hit !== undefined &&
+    hit.length !== undefined &&
+    hit.length > 0 &&
+    hit[0].x !== undefined &&
+    hit[0].y !== undefined
+  ) {
+    let logGazetteerHit = new URLSearchParams(window.location.href).get(
+      "logGazetteerHits"
+    );
+    if (logGazetteerHit !== null) {
+      let url = window.location.href.split("?")[0];
+
+      // console.log(url + '?gazHit=' + window.btoa(JSON.stringify(hit[0])));
+    }
+
+    const pos = proj4(
+      referenceSystemDefinition || proj4crs25832def,
+      "EPSG:4326",
+      [hit[0].x, hit[0].y]
+    );
+    //console.log(pos)
+    leafletElement.panTo([pos[1], pos[0]], {
+      animate: false,
+    });
+
+    let hitObject = objectAssign({}, hit[0]);
+
+    //Change the Zoomlevel of the map
+    if (hitObject.more.zl) {
+      leafletElement.setZoom(hitObject.more.zl, {
+        animate: false,
+      });
+
+      if (suppressMarker === false) {
+        //show marker
+        setGazetteerHit(hitObject);
+        setOverlayFeature(null);
+      }
+    } else if (hitObject.more.g) {
+      var feature = turfHelpers.feature(hitObject.more.g);
+      if (!feature.crs) {
+        console.log("xxx no crs therefore context based crs", referenceSystem);
+
+        const refSys =
+          referenceSystem !== undefined
+            ? referenceSystem.code.split("EPSG:")[1]
+            : "25832";
+
+        feature.crs = {
+          type: "name",
+          properties: {
+            name: "urn:ogc:def:crs:EPSG::" + refSys,
+          },
+        };
+      }
+      console.log("xxx no crs therefore context based crs. feature:", feature);
+
+      var bb = bboxCreator(feature);
+      if (suppressMarker === false) {
+        setGazetteerHit(null);
+        setOverlayFeature(feature);
+      }
+
+      leafletElement.fitBounds(
+        convertBBox2Bounds(bb, proj4(referenceSystemDefinition || proj4crs25832def)),
+        padding ? { padding } : undefined
+      );
+    }
+    setTimeout(() => {
+      if (furtherGazeteerHitTrigger !== undefined) {
+        furtherGazeteerHitTrigger(hit);
+      }
+    }, 200);
+  } else {
+    //console.log(hit);
+  }
 };
