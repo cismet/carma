a68398a8d838f2c5b441d43ca1f426520064948a
David Glogaza <david.glogaza40@googlemail.com>
2026-03-17 19:49:43 +0100

    hide filter ui when switching to 3d mode

--- apps/geoportal/src/app/components/layers/GeoportalLayerButton.tsx
+++ apps/geoportal/src/app/components/layers/GeoportalLayerButton.tsx
@@ -133,6 +133,12 @@ const GeoportalLayerButton = ({
   const map = routedMapRef?.leafletMap?.leafletElement as L.Map;
 
   useEffect(() => {
+    if (hide && activeInteractionLayerID === id) {
+      dispatch(setActiveInteractionLayerID(null));
+    }
+  }, [hide, activeInteractionLayerID, id, dispatch]);
+
+  useEffect(() => {
     if (!inView && selectedLayerIndex === index) {
       document.getElementById(`layer-${id}`).scrollIntoView();
     }
@@ -223,7 +229,7 @@ const GeoportalLayerButton = ({
         {!background && (
           <>
             <span className="text-base ml-1">{title}</span>
-            {layer.filterConfig && (
+            {(layer.filterConfig || id.toLowerCase().includes("poi")) && (
               <button
                 id={`filterLayerButton-${id}`}
                 className={cn("px-1.5 flex items-center justify-center")}
