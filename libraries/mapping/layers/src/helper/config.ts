796260ceb5432658a5a415228fa21b722f01ec3b
David Glogaza <david.glogaza40@googlemail.com>
2026-04-27 20:35:13 +0200

    add missing letter

--- libraries/mapping/layers/src/helper/config.ts
+++ libraries/mapping/layers/src/helper/config.ts
@@ -682,7 +682,7 @@ export const topicMapsConfig: Config = {
         "Öffentlichkeitsarbeit",
         "Stadterneuerung",
         "Planungsmaßnahme",
-        "Kommunalpoliti",
+        "Kommunalpolitik",
       ],
       type: "link",
       thumbnail:
