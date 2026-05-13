cf49d69dbac47983354ffe786bfa9f9ce9729af9
David Glogaza <david.glogaza40@googlemail.com>
2026-05-13 15:32:03 +0200

    change libremap feature info cursor

--- apps/geoportal/src/app/hooks/useFeatureInfoModeCursorStyle.ts
+++ apps/geoportal/src/app/hooks/useFeatureInfoModeCursorStyle.ts
@@ -1,16 +1,19 @@
 import { useEffect } from "react";
 import { useSelector } from "react-redux";
+import type maplibregl from "maplibre-gl";
 import { getUIMode, UIMode } from "../store/slices/ui";
 
 export const useFeatureInfoModeCursorStyle = (
-  topicMapElementId: string = "routedMap"
+  topicMapElementId: string = "routedMap",
+  libreMap?: maplibregl.Map | null
 ) => {
   const uiMode = useSelector(getUIMode);
   const isModeFeatureInfo = uiMode === UIMode.FEATURE_INFO;
   useEffect(() => {
-    const mapElement = document.getElementById(topicMapElementId);
+    const mapElement =
+      document.getElementById(topicMapElementId) || libreMap?.getCanvas();
     if (mapElement) {
       mapElement.style.cursor = isModeFeatureInfo ? "crosshair" : "pointer";
     }
-  }, [isModeFeatureInfo, topicMapElementId]);
+  }, [isModeFeatureInfo, topicMapElementId, libreMap]);
 };
