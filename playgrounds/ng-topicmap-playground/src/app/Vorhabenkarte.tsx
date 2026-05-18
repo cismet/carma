4d2cdcbaf7f1d9e22c5e8d4e2035a4bcfff01048
Thorsten Hell <thorsten.hell@cismet.de>
2026-05-18 17:53:53 +0200

    #4000 add Vorhabenkarte page to ng-topicmap-playground (vector style)
    First-step scaffold using the published vorhabenkarte vector style URL.
    No clustering. Clustering toggle in DefaultSettingsPanel stays visible
    but is a no-op against the vector layer.

---
+++ playgrounds/ng-topicmap-playground/src/app/Vorhabenkarte.tsx
@@ -0,0 +1,61 @@
+import {
+  SelectionProvider,
+  ProgressIndicator,
+  useProgress,
+  GazDataProvider,
+} from "@carma-appframeworks/portals";
+import { SandboxedEvalProvider } from "@carma-commons/sandbox-eval";
+import { CarmaMap } from "@carma-mapping/core";
+import { LibreContextProvider } from "@carma-mapping/engines/maplibre";
+import TopicMapContextProvider from "react-cismap/contexts/TopicMapContextProvider";
+import { defaultGazDataConfig } from "@carma-commons/resources";
+import {
+  backgroundModes,
+  backgroundConfigurations,
+} from "./backgroundConfig";
+import Menu from "./Menu";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-bootstrap-typeahead/css/Typeahead.css";
+import "react-cismap/topicMaps.css";
+import "leaflet/dist/leaflet.css";
+
+const VORHABENKARTE_STYLE_URL =
+  "https://tiles.cismet.de/vorhabenkarte/style.json";
+
+export function Vorhabenkarte() {
+  const { progress, showProgress, handleProgressUpdate } = useProgress();
+
+  return (
+    <TopicMapContextProvider
+      appKey="ng-topicmap-playground-vorhabenkarte"
+      infoBoxPixelWidth={350}
+      backgroundModes={backgroundModes}
+      backgroundConfigurations={backgroundConfigurations}
+    >
+      <SandboxedEvalProvider>
+        <GazDataProvider config={defaultGazDataConfig}>
+          <SelectionProvider>
+            <LibreContextProvider>
+              <ProgressIndicator progress={progress} show={showProgress} />
+              <CarmaMap
+                appKey="ng-topicmap-playground-vorhabenkarte"
+                mapEngine="maplibre"
+                exposeMapToWindow
+                overrideGlyphs="https://tiles.cismet.de/fonts/{fontstack}/{range}.pbf"
+                onProgressUpdate={handleProgressUpdate}
+                libreLayers={[
+                  {
+                    type: "vector",
+                    name: "Vorhaben",
+                    style: VORHABENKARTE_STYLE_URL,
+                  },
+                ]}
+                modalMenu={<Menu />}
+              />
+            </LibreContextProvider>
+          </SelectionProvider>
+        </GazDataProvider>
+      </SandboxedEvalProvider>
+    </TopicMapContextProvider>
+  );
+}
