e564e3d6e5f9563be5261036583721c232e2e1c9
PavelOlkhovoi <p-olhovoi@bk.ru>
2026-06-15 09:29:19 +0200

    #693 add env to rest service

--- apps/belis/desktop/src/constants/belis.ts
+++ apps/belis/desktop/src/constants/belis.ts
@@ -1,7 +1,8 @@
 import queries from "./queries";
 import { gql } from "graphql-request";
+export const REST_SERVICE =
+  import.meta.env.VITE_REST_SERVICE || "https://belis-cloud-api.cismet.de";
 
-export const REST_SERVICE = "https://belis-cloud-api.cismet.de";
 export const DOMAIN = "BELIS2";
 export const ENDPOINT = REST_SERVICE + `/graphql/` + DOMAIN + "/execute";
 export const SAVE_ENDPOINT =
