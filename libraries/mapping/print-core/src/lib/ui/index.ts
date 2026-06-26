6560f2884395d5903e2b2b9292ce08368036a8d2
PavelOlkhovoi <p-olhovoi@bk.ru>
2026-06-26 14:02:43 +0200

    #713 add print error ui

--- libraries/mapping/print-core/src/lib/ui/index.ts
+++ libraries/mapping/print-core/src/lib/ui/index.ts
@@ -5,3 +5,4 @@ export { PrintSettings } from "./PrintSettings";
 export type { PrintSettingsProps } from "./PrintSettings";
 export { PrintPreviewControls } from "./PrintPreviewControls";
 export type { PrintPreviewControlsProps } from "./PrintPreviewControls";
+export { showPrintErrorToast } from "./showPrintErrorToast";
