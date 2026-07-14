25a4e34928bf87d9da3efb6c205ae1a5ccf7db7b
therter <thorsten.herter@cismet.de>
2026-07-14 11:14:57 +0200

    rasterfari-tileserver-wms-companion: increase the limit of post requests

--- services/rasterfari-tileserver-wms-companion/src/main.ts
+++ services/rasterfari-tileserver-wms-companion/src/main.ts
@@ -7,8 +7,8 @@ const host = process.env.HOST ?? "localhost";
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 
 // Default route
