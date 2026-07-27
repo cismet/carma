38a091f14f2c740cece697a57f83e02e2827d995
David Glogaza <david.glogaza40@googlemail.com>
2026-07-27 09:41:26 +0200

    add gazetteer namespace to carma api

--- libraries/carma-api/src/index.ts
+++ libraries/carma-api/src/index.ts
@@ -1,8 +1,9 @@
 /// <reference path="./global.d.ts" />
+import { gazetteer } from "./lib/gazetteer";
 import { mapping, mapping2D, mapping3D } from "./lib/mapping";
 import { ui } from "./lib/ui";
 
-export const carma = { mapping, mapping2D, mapping3D, ui } as const;
+export const carma = { gazetteer, mapping, mapping2D, mapping3D, ui } as const;
 
 export { registerMapping } from "./lib/mapping";
 export type {
@@ -18,3 +19,12 @@ export type {
 
 export { registerUi } from "./lib/ui";
 export type { UiAdapter, UiFacade } from "./lib/ui";
+
+export { registerGazetteer } from "./lib/gazetteer";
+export type {
+  GazetteerAdapter,
+  GazetteerContribution,
+  GazetteerFacade,
+  GazetteerMode,
+  GazetteerSource,
+} from "./lib/gazetteer";
