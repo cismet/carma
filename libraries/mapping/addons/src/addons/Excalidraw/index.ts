6dd81961a4bcfac616aac2ab38fd2df0402bc063
PavelOlkhovoi <p-olhovoi@bk.ru>
2026-08-31 09:06:59 +0200

    #777 synch excalidraw zoom with map

--- libraries/mapping/addons/src/addons/Excalidraw/index.ts
+++ libraries/mapping/addons/src/addons/Excalidraw/index.ts
@@ -1,6 +1,7 @@
 export { ExcalidrawOverlay } from "./ExcalidrawOverlay";
 export { ExcalidrawControl } from "./ExcalidrawControl";
 export { useExcalidrawActions } from "./excalidraw-actions";
+export { useMapSceneSync } from "./map-scene-sync";
 export type {
   ExcalidrawControlConfig,
   ExcalidrawOverlayConfig,
