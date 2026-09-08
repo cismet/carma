f6267113dea427c37b8dbc497e3ff8240f466734
David Glogaza <david.glogaza40@googlemail.com>
2026-09-08 15:03:08 +0200

    change default values

--- libraries/mapping/addons/src/addons/Routing/config.ts
+++ libraries/mapping/addons/src/addons/Routing/config.ts
@@ -1,15 +1,15 @@
 export type RoutingConfig = {
-  /** the zoom the map goes to at the start of the route; default 17 */
+  /** the zoom the map goes to at the start of the route; default 18 */
   zoom?: number;
   /** camera tilt in degrees; default 30, 0 is flat */
   pitch?: number;
-  /** how far along the route the bearing looks, in meters; default 100 */
+  /** how far along the route the bearing looks, in meters; default 10 */
   lookAheadMeters?: number;
   /** how long the camera takes to get there, in ms; default 1200 */
   duration?: number;
 };
 
-export const DEFAULT_ZOOM = 17;
+export const DEFAULT_ZOOM = 18;
 export const DEFAULT_PITCH = 30;
-export const DEFAULT_LOOK_AHEAD_METERS = 100;
+export const DEFAULT_LOOK_AHEAD_METERS = 10;
 export const DEFAULT_DURATION = 1200;
